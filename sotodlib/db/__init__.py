77628ad793ee9d787947c22df064d64d5ca0680b
Theodore Kisner <mail@theodorekisner.com>
2018-10-15 22:20:17 -0700

    More stubs

---
+++ sotodlib/db/__init__.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2018 Simons Observatory.
+# Full license can be found in the top level "LICENSE" file.
+"""Database Access.
+
+This module contains higher-level functions for creation and I/O on various
+relevant databases.
+
+"""
