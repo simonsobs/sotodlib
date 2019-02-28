a5a149da0cce398caa3ffd0824b4f0b8e7fb06c1
Theodore Kisner <tskisner.public@gmail.com>
2019-02-28 00:40:38 -0800

    Individual wafer layout working.  Still some fixes to rotations needed when laying out tubes.  Visualization working ok for now.

--- tests/test_hardware.py
+++ tests/test_hardware.py
@@ -13,7 +13,10 @@ from ._helpers import create_outdir
 
 from sotodlib.hardware.config import get_example
 
-from sotodlib.hardware.sim import (sim_wafer_detectors,)
+from sotodlib.hardware.sim import (sim_wafer_detectors,
+                                   sim_telescope_detectors)
+
+from sotodlib.hardware.vis import plot_detectors
 
 
 class HardwareTest(TestCase):
@@ -39,16 +42,45 @@ class HardwareTest(TestCase):
         self.assertTrue(conf == check)
         return
 
-    def test_sim_wafer(self):
+    # def test_sim_wafer(self):
+    #     conf = get_example()
+    #     for tele, teleprops in conf["telescopes"].items():
+    #         platescale = teleprops["platescale"]
+    #         plotdim = 150.0 * platescale
+    #         fwhm = teleprops["fwhm"]
+    #         for tube in teleprops["tubes"]:
+    #             tubeprops = conf["tubes"][tube]
+    #             for wafer in tubeprops["wafers"]:
+    #                 wprops = conf["wafers"][wafer]
+    #                 print("telescope {}, tube {}, wafer {}, platescale = {},"
+    #                       " fwhm = {}".format(tele, tube, wafer, platescale,
+    #                       fwhm[wprops["bands"][0]]), flush=True)
+    #                 outpath = os.path.join(self.outdir,
+    #                                        "wafer_{}.toml".format(wafer))
+    #                 dets = sim_wafer_detectors(conf, wafer, platescale, fwhm)
+    #                 # with open(outpath, "w") as f:
+    #                 #     toml.dump(dets, f)
+    #                 outpath = os.path.join(self.outdir,
+    #                                        "wafer_{}.pdf".format(wafer))
+    #                 plot_detectors(dets, plotdim, plotdim, outpath,
+    #                                labels=True)
+    #     return
+
+    def test_sim_telescope(self):
         conf = get_example()
         for tele, teleprops in conf["telescopes"].items():
             platescale = teleprops["platescale"]
-            for tube in teleprops["tubes"]:
-                tubeprops = conf["tubes"][tube]
-                for wafer in tubeprops["wafers"]:
-                    outpath = os.path.join(self.outdir,
-                                           "wafer_{}.toml".format(wafer))
-                    dets = sim_wafer_detectors(conf, wafer, platescale)
-                    with open(outpath, "w") as f:
-                        toml.dump(dets, f)
+            plotdim = None
+            if tele[0] == "S":
+                plotdim = 400.0 * platescale
+            else:
+                plotdim = 900.0 * platescale
+            dets = sim_telescope_detectors(conf, tele)
+            outpath = os.path.join(self.outdir,
+                                   "telescope_{}.toml".format(tele))
+            with open(outpath, "w") as f:
+                toml.dump(dets, f)
+            outpath = os.path.join(self.outdir,
+                                   "telescope_{}.pdf".format(tele))
+            plot_detectors(dets, plotdim, plotdim, outpath, labels=False)
         return
