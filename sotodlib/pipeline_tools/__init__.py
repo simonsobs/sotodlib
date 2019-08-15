9e4875b30ef8a08ca694f8790e7c4f909e05f0d5
Reijo Keskitalo <reijo.keskitalo@gmail.com>
2019-08-15 18:17:02 -0400

    move the observation loading into pipeline_tools

--- sotodlib/pipeline_tools/__init__.py
+++ sotodlib/pipeline_tools/__init__.py
@@ -5,5 +5,5 @@ from .atm import scale_atmosphere_by_bandpass
 from .export import add_export_args, export_TOD
 from .hardware import add_hw_args, load_focalplanes
 from .noise import add_so_noise_args, get_analytic_noise, get_elevation_noise
-from .observation import create_observations
+from .observation import create_observations, load_observations, add_import_args
 from .pysm import add_pysm_args, simulate_sky_signal
