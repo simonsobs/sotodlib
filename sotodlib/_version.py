fec16a8ff01a585de3638fab707eaa862cb438bb
Matthew Hasselfield <mhasself@users.noreply.github.com>
2019-09-30 10:49:17 -0400

    Fix versioneer configuration.
    Use tags of the form vX.Y.Z.  Set the docs version code through
    __version__.

--- sotodlib/_version.py
+++ sotodlib/_version.py
@@ -41,7 +41,7 @@ def get_config():
     cfg = VersioneerConfig()
     cfg.VCS = "git"
     cfg.style = "pep440"
-    cfg.tag_prefix = "sotodlib-"
+    cfg.tag_prefix = "v"
     cfg.parentdir_prefix = "sotodlib-"
     cfg.versionfile_source = "sotodlib/_version.py"
     cfg.verbose = False
