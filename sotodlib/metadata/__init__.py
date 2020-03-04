365780f6648594ca6b81746a56640c04ee5f231c
Matthew Hasselfield <mhasself@users.noreply.github.com>
2020-03-04 13:46:24 -0500

    metadata module: for now this just imports everything from sotoddb

---
+++ sotodlib/metadata/__init__.py
@@ -0,0 +1,8 @@
+# The metadata engine.  Supports a number of classes for holding (and
+# reading and writing) fairly generic metadata.
+#
+# Currently we just import all from the sotoddb library -- this latter
+# should be merged into sotodlib soon.
+#
+
+from sotoddb import *
