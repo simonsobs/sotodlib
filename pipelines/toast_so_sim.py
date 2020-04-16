46f1d4087303cd4abe9cf45bcc3de0768b17e05e
Theodore Kisner <tskisner.public@gmail.com>
2020-04-16 15:15:16 -0700

    Fix imports in pipelines

--- pipelines/toast_so_sim.py
+++ pipelines/toast_so_sim.py
@@ -40,13 +40,11 @@ from toast.timing import dump as dump_timing
 
 import toast.pipeline_tools as toast_tools
 
-import sotodlib.pipeline_tools as so_tools
+import sotodlib.utils.pipeline_tools as so_tools
 
 import sotodlib.observe
 
 import numpy as np
-
-import sotodlib.hardware
 
 import warnings
 
