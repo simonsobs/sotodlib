0086aa1ac1ed795501288919e07675162d189781
Matthew Hasselfield <mhasself@users.noreply.github.com>
2020-04-27 13:51:09 -0400

    Adjust imports of sotoddb classes into new structure
    This imports ResultSet and the metadata *Db directly to core.metadata,
    renaming the *DB classes as *Db.  The more stuff-on-disk code in
    sotodlib.simple is imported as io.metadata.simple.  This has been
    tested on simons1 with the SO sim and ACT Uranus demo code.

--- sotodlib/core/metadata.py
+++ sotodlib/core/metadata.py
@@ -3,4 +3,8 @@
 """Metadata containers.
 """
 
-from sotoddb import resultset
+# Import classes from sotoddb, with a rename DB -> Db.  These will
+# live in sotodlib soon.
+from sotoddb import ResultSet, DetDB as DetDb, ObsDB as ObsDb, ObsFileDB as ObsFileDb
+from sotoddb import ManifestDB as ManifestDb, ManifestScheme
+from sotoddb import SuperLoader
