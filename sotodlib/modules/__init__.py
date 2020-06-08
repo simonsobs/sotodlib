9a7afa125fa8510dda75a607bf14faf5f81a8390
Reijo Keskitalo <reijo.keskitalo@gmail.com>
2020-06-08 11:04:20 -0700

    Time constant (#52)
    * Initial implementation of the operator and pipeline tools. Thanks to Katie Harrington for the code.
    
    * unit test now passes
    
    * fix name errors
    
    * add missing import
    
    * add unit test file
    
    * Add units [s] to time constant doc strings
    
    * Add parameters to leave a time constant residual
    
    * fix time constant unit test
    
    * Improve unit tests
    
    * move time_constant.py
    
    * fix import; add __init__.py
    
    * more import fixes
    
    * Fully functional time constant operator
    
    * fix issue in pipeline_tools; correct the order of time constant and noise simulation
    
    * remove obsolete directory

---
+++ sotodlib/modules/__init__.py
@@ -0,0 +1,7 @@
+# Copyright (c) 2020 Simons Observatory.
+# Full license can be found in the top level "LICENSE" file.
+"""Simons Observatory processing modules.
+
+"""
+
+from .time_constant import OpTimeConst
