943d39bd0aa806720d8ffa06b6e4d1af28dd2938
Reijo Keskitalo <reijo.keskitalo@gmail.com>
2021-02-02 09:22:47 -0800

    Initial implementation (#101)

--- sotodlib/toast/pipeline_tools/__init__.py
+++ sotodlib/toast/pipeline_tools/__init__.py
@@ -18,3 +18,4 @@ from .demodulation import add_demodulation_args, demodulate
 from .h_n import add_h_n_args, compute_h_n
 from .crosslinking import add_crosslinking_args, compute_crosslinking
 from .corotator import rotate_focalplane
+from .sim_hwpss import add_sim_hwpss_args, simulate_hwpss
