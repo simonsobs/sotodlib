84b24f937968aa4cdca4fb410709d8d53a36614e
Jason Leung <leung@astro.utoronto.ca>
2021-04-20 16:20:00 -0400

    Create Bookbinder object and rewrite main loop
    Bookbinder object buffers both HK and SMuRF frames, removing the discontinuity
    when loading new HK frames. Rewrite main loop to accommodate new buffering
    strategy.
    
    Functionality unchanged from previous commit.

--- main.py
+++ main.py
@@ -7,58 +7,88 @@ import numpy as np
 def locate_sign_changes(t):
     return np.where(np.sign(t[:-1]) != np.sign(t[1:]))[0] + 1
 
-class _ScanDataBundle(object):
-    def __init__(self, flush_time):
+class _HKBlockBundle(object):
+    def __init__(self):
         self.times = []
-        self.flush_time = flush_time
+        self.data = []
+
+    def add(self, b):
+        self.times.extend([int(t) for t in b.times])
+        self.data.extend(b['Azimuth_current_velocity'])
+
+    def ready(self):
+        return len(locate_sign_changes(self.data)) > 0
+
+    def rebundle(self, flush_time):
+        if len(self.times) == 0:
+            return None
+
+        self.times = [t for t in self.times if t >= flush_time]
+        self.data = self.data[-len(self.times):]
+
+class _ScanDataBundle(object):
+    def __init__(self):
+        self.times = []
 
     def add(self, b):
         self.times.extend(b.times())
 
-    def ready(self):
+    def ready(self, flush_time):
         """
         Returns True if the current frame has crossed the flush_time
         """
-        return len(self.times) > 0 and self.times[-1].time >= self.flush_time
+        return len(self.times) > 0 and self.times[-1].time >= flush_time
 
-    def rebundle(self):
+    def rebundle(self, flush_time):
         if len(self.times) == 0:
             return None
         # For now, just return the times as output
-        output = [t for t in self.times if t.time < self.flush_time]
-        self.times = [t for t in self.times if t.time >= self.flush_time]
+        output = [t for t in self.times if t.time < flush_time]
+        self.times = [t for t in self.times if t.time >= flush_time]
 
         return output
 
-class DataReframer(object):
+class Bookbinder(object):
     def __init__(self):
-        self.bundle = None
+        self.hkbundle = None
+        self.sdbundle = None
         self.flush_time = None
 
     def ready(self):
-        return self.bundle.ready() if (self.bundle is not None) else False
+        """
+        Check if criterion passed in HK (for now, sign change in Az scan velocity data)
+        """
+        return self.hkbundle.ready() if (self.hkbundle is not None) else False
 
     def __call__(self, f):
         """
         Process a frame
         """
         if f.type == core.G3FrameType.EndProcessing:
-            return self.bundle.rebundle() + [f]
+            return self.sdbundle.rebundle(self.flush_time) + [f]
 
-        if f.type != core.G3FrameType.Scan:
+        if f.type != core.G3FrameType.Housekeeping and f.type != core.G3FrameType.Scan:
             return f
 
-        if self.bundle is None:
-            self.bundle = _ScanDataBundle(self.flush_time)
+        if f.type == core.G3FrameType.Housekeeping:
+            if self.hkbundle is None:
+                self.hkbundle = _HKBlockBundle()
 
-        output = []
+            self.hkbundle.add(f['blocks'][0])   # 0th block for now
 
-        self.bundle.add(f['data'])
+        if f.type == core.G3FrameType.Scan:
+            if self.sdbundle is None:
+                self.sdbundle = _ScanDataBundle()
 
-        if self.bundle.ready():
-            output = self.bundle.rebundle()
+            output = []
 
-        return output
+            self.sdbundle.add(f['data'])
+
+            if self.sdbundle.ready(self.flush_time):
+                self.hkbundle.rebundle(self.flush_time)
+                output = self.sdbundle.rebundle(self.flush_time)
+
+            return output
 
 if __name__ == '__main__':
     import argparse
@@ -70,20 +100,38 @@ if __name__ == '__main__':
 
     scanframes = [f for f in core.G3File(args.g3file) if f.type == core.G3FrameType.Scan]
 
-    rf = DataReframer()
+    B = Bookbinder()
 
     hkframes = [h for h in core.G3File(args.hkfile)]
     acu_summary = [h for h in hkframes if h['hkagg_type'] == 2 and h['block_names'][0] == 'ACU_summary_output']
 
-    for s in acu_summary:
-        sc = locate_sign_changes(s['blocks'][0]['Azimuth_current_velocity'])
-        tc = [int(s['blocks'][0]['ctime'][int(i)]*1e8) for i in sc]
+    """
+    Main loop
 
-        for t in tc:
-            out = []
-            rf.flush_time = t
+    Strategy:
+    1. Add HK frames until sign change detected - get list of (timestamps of) sign changes
+    2. While len(sign_changes) > 0, pop the first sign change, add SMuRF frames until that time;
+       emit new frame; truncate HK and SMuRF frames
+    3. Repeat Step 2 until len(sign_changes) == 0, then go back to Step 1
+    """
+    while len(acu_summary) > 0:
+        while not B.ready():
+            # If there are no more HK frames, terminate program by ending loop
+            if len(acu_summary) == 0:
+                break
+            B(acu_summary.pop(0))
 
-            if rf.bundle is not None:
-                rf.bundle.flush_time = t
-            while len(scanframes) > 0 and len(out) == 0:
-                out = rf.bundle.rebundle() if rf.ready() else rf(scanframes.pop(0))
+        sc = locate_sign_changes(B.hkbundle.data)
+        tc = [B.hkbundle.times[i] for i in sc]
+        while len(tc) > 0:
+            B.flush_time = tc.pop(0)
+
+            while B.sdbundle is None or not B.sdbundle.ready(B.flush_time):
+                # If there are no more SMuRF frames, output remaining SMuRF data
+                # and terminate program by ending loop
+                if len(scanframes) == 0:
+                    output = B.sdbundle.rebundle(B.flush_time)
+                    tc = []
+                    acu_summary = []
+                    break
+                output = B(scanframes.pop(0))
