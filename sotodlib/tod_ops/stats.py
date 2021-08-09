e944ff0dcc0482b4e64dbbea2ddd84823ee8e868
Saianeesh Keshav Haridas <accounts@saianeesh.com>
2021-08-09 13:21:02 -0400

    Added option to append resampled timestamps and data to aman in average_to

--- sotodlib/tod_ops/stats.py
+++ sotodlib/tod_ops/stats.py
@@ -3,7 +3,7 @@ Useful tools for statistical analyses
 """
 from scipy.stats import skewnorm, binned_statistic
 import numpy as np
-
+from .. import core
 
 def gauss(x, mean, sigma, A):
     """
@@ -50,7 +50,7 @@ def skewgauss(x, mean, sigma, A, sk):
     return A * skewnorm.pdf((x-mean)/sigma, sk)
 
 
-def average_to(aman, DT, signal=None, timestamps=None, tmin=None, tmax=None):
+def average_to(aman, DT, signal=None, timestamps=None, tmin=None, tmax=None, append_aman=False):
     """
 	Bins and averages input signal and timestamps to a new (regular) sample rate.
 	This is most useful for downsampling (paricularly non-regularly sapmled) data.
@@ -68,12 +68,16 @@ def average_to(aman, DT, signal=None, timestamps=None, tmin=None, tmax=None):
         tmin: desired start time for resampled data. If None, uses beginning of timestamps.
 
         tmax: desired stop time for resampled data. If None, uses end of timestamps.
+
+        append_aman: return AxisManager with resampled time and data appended to the provided aman. If aman=None this returns None.
             
     Returns:
     
 		t: resampled timestamps
 
-		d: resampled data -- will have nans in bins where there was no input data.
+		d: resampled data -- will have nans in bins where there was no input data
+		
+                aman: aman containing resampled timestamps and data. The new sample axis will be called avgDT_samps, the resampled timestamps will be called avgDT_timestamps, and the resampled signal will be called avgDT_signal (where DT is the provided time seperation of the new sample rate). This is only returned if append_aman is True.
     """
     if signal is None:
         signal = aman.signal
@@ -88,5 +92,14 @@ def average_to(aman, DT, signal=None, timestamps=None, tmin=None, tmax=None):
     bins = np.arange(tmin-DT/2,tmax+DT/2,DT)
     d, bins, _ = binned_statistic(timestamps, signal, bins=bins)
     t = (bins[1:]+bins[:-1])/2
+
+    if append_aman:
+        if aman is None:
+            return t, d, None
+        aman = core.AxisManager(aman, core.OffsetAxis(f'avg{DT}_samps', len(t)))
+
+        aman.wrap(f'avg{DT}_timestamps', t, [(0, f'avg{DT}_samps')])
+        aman.wrap(f'avg{DT}_signal', d, [(0, 'dets'), (1, f'avg{DT}_samps')])
+        return t, d, aman
 
     return t, d
