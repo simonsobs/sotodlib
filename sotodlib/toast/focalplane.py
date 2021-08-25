e638f0c0f59cf2bd2c63553cbc3cc853c4c7bba3
Reijo Keskitalo <reijo.keskitalo@gmail.com>
2021-08-25 09:19:51 -0700

    WIP

---
+++ sotodlib/toast/focalplane.py
@@ -0,0 +1,138 @@
+# Copyright (c) 2019-2021 Simons Observatory.
+# Full license can be found in the top level "LICENSE" file.
+
+import os
+import sys
+
+import astropy.units as u
+from toast.instrument import Focalplane
+from toast.utils import Logger
+
+from ..sim_hardware import get_example, sim_telescope_detectors
+
+
+FOCALPLANE_RADII = {
+    "LAT" : 3.6 * u.deg,
+    "SAT1" : 17.8 * u.deg,
+    "SAT2" : 17.8 * u.deg,
+    "SAT3" : 17.8 * u.deg,
+    "SAT4" : 17.2 * u.deg,
+}
+
+
+def get_telescope(telescope, wafer_slots, tube_slots):
+    """ Determine which telescope matches the detector selections
+    """
+    hwexample = get_example()
+    if wafer_slots is not None:
+        wafer_slots = wafer_slots.split(",")
+        wafer_map = hwexample.wafer_map()
+        tube_slots = [wafer_map["tube_slots"][ws] for ws in wafer_slots]
+    else:
+        tube_slots = tube_slots.split(",")
+    for tube_slot in tube_slots:
+        for telescope_name, telescope_data in hwexample.data[
+            "telescopes"
+        ].items():
+            if tube_slot in telescope_data["tube_slots"]:
+                if telescope is None:
+                    telescope = telescope_name
+                elif telescope != telescope_name:
+                    raise RuntimeError(
+                        f"Tubes '{tube_slots}' span more than one telescope"
+                    )
+        if telescope is None:
+            raise RuntimeError(
+                f"Failed to match tube_slot = '{tube_slot}' with a telescope"
+            )
+    return telescope
+
+
+class SOFocalplane(Focalplane):
+    """ SO Focalplane class
+    """
+
+    def __init__(self, hwfile=None, telescope=None, sample_rate=10 * u.Hz, bands=None, wafer_slots=None, tube_slots=None, thinfp=None):
+        if thinfp is not None:
+            raise RuntimeError("Focalplane thinning not implemented")
+        log = Logger.get()
+        telescope = get_telescope(telescope, wafer_slots, tube_slots)
+        if hwfile is not None:
+            log.info(f"Loading hardware configuration from {hwfile}...")
+            hw = Hardware(args.hardware)
+        elif telescope in ["LAT", "SAT1", "SAT2", "SAT3"]:
+            log.info("Simulating default hardware configuration")
+            hw = get_example()
+            hw.data["detectors"] = sim_telescope_detectors(hw, telescope)
+        else:
+            raise RuntimeError("Must provide a path to file or a valid telescope name")
+
+        field_of_view = FOCALPLANE_RADII[telescope]
+        match = {"band": bands.replace(",", "|")}
+        tube_slots = None
+        if wafer_slots is not None:
+            match["wafer_slot"]  = args.wafer_slots.split(",")
+        elif tube_slots is not None:
+            tube_slots = args.tube_slots.split(",")
+        # If one provides both telescopes and tube_slots, the tube_slots matching *either*
+        # will be concatenated
+        #hw = hw.select(telescopes=[telescope.name], tube_slots=tube_slots, match=match)
+        hw = hw.select(tube_slots=tube_slots, match=match)
+
+        def get_par(key, default):
+            if key in det_data:
+                return det_data[key]
+            else:
+                return default
+
+        (
+            names, quats, bands, wafer_slots, tube_slots, nets, fknees, fmins,
+            alphas, As, Cs, bandcenters, bandwidths
+         ) = (
+             [], [], [], [], [], [], [], [], [], [], [], [], []
+         )
+        for det_name, det_data in hw.data["detectors"].items():
+            names.append(det_name)
+            quats.append(det_data["quat"])
+            band = det_data["band"]
+            band_data = hw.data["bands"][band]
+            bands.append(band)
+            wafer_slot = det_data["wafer_slot"]
+            wafer_slots.append(wafer_slot)
+            # Determine which tube_slot has this wafer
+            for tube_slot, tube_data in hw.data["tube_slots"].items():
+                if wafer_slot in tube_data["wafer_slots"]:
+                    break
+            else:
+                raise RuntimeError(f"{wafer_slot} is not in any tube_slot")
+            tube_slots.append(tube_slot)
+            # Extract parameters from detector data object, add units
+            nets.append(get_par("NET", band_data.net) * u.uK)
+            fknee.append(get_par("fknee", band_data.fknee)) * u.mHz
+            fmins.append(get_par("fmin", band_data.fmin)) * u.mHz
+            #alphas.append(get_par("alpha", band.alpha))
+            alphas.append(1)  # hardwire a sensible number. 3.5 is not realistic.
+            As.append(get_par("A", band_data.A))
+            Cs.append(get_par("C", band_data.C))
+            # bandpass
+            lower = get_par("low", band.lower) * u.GHz
+            #center = get_par("center", band.center) * u.GHz
+            upper = get_par("high", band.upper) * u.GHz
+            bandcenters.append(0.5 * (self.lower + self.upper))
+            bandwidths.append(self.upper - self.lower)
+
+        detdata = QTable(
+            [
+                names, quats, bands, wafer_slots, tube_slots, nets, fknees,
+                fmins, alphas, As, Cs, bandcenters, bandwidths,
+            ],
+            names=[
+                "name", "quat", "wafer_slot", "tube_slot", "NET", "fknee",
+                "fmin", "alpha", "A", "C", "bandcenter", "bandwidth",
+            ])
+        
+        super.__init__(
+            detector_data=detdata,
+            field_of_view=field_of_view,
+            sample_rate=sample_rate,
+        )
