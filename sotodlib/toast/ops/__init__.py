3e63b832b35a383c5e4617fd2b1fc0e477a3f16d
Reijo Keskitalo <reijo.keskitalo@gmail.com>
2021-08-30 14:01:36 -0700

    Add missing file

---
+++ sotodlib/toast/ops/__init__.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2020-2021 Simons Observatory.
+# Full license can be found in the top level "LICENSE" file.
+"""Simons Observatory processing modules.
+
+"""
+
+from .corotator import CoRotator
+from .sim_sso import SimSSO
