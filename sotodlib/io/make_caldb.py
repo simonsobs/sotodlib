d2637420652a5352e1e461e14eb286038eba5938
Katie Harrington <kmharrin@umich.edu>
2022-03-08 23:29:36 +0000

    h5 saving for sample caldb maker

--- sotodlib/io/make_caldb.py
+++ sotodlib/io/make_caldb.py
@@ -1,9 +1,11 @@
 import os
-import datetime as dt
+import h5py
 import logging
+import numpy as np
+import datetime as dt
 
 import sotodlib
-from sotodlib.core import Context, metadata
+from sotodlib.core import Context, metadata, axisman_io
 import sotodlib.io.metadata as io_meta
 
 from sotodlib.core.metadata import ManifestDb, ManifestScheme
@@ -51,7 +53,7 @@ class _MakeCalDb:
         field_names : string, list of strings, or None
             names to be used in the result set creation. Must match length of 
             results returned from the calc function. If none, name will be used
-        manifest_args : dict or none. Information to add to the Manifest information for
+        manifest_args : dict or none. Information to add to the Manifest Database for
             each observation.
         """
         self.name = name
@@ -73,11 +75,17 @@ class _MakeCalDb:
         else:
             if prefix is None:
                 prefix = os.path.split(db_file)[0]
-            
+        
+        if manifest_args is None:
+            manifest_args = {}
+        self.manifest_args = manifest_args
+        
         if not os.path.exists(db_file):
             scheme = ManifestScheme()
             scheme.add_exact_match('obs:obs_id')
             scheme.add_data_field('dataset')   
+            for k in self.manifest_args:
+                scheme.add_data_field(k)
             self.db = ManifestDb(scheme=scheme)
         else:
             self.db = ManifestDb.from_file(db_file)
@@ -102,9 +110,7 @@ class _MakeCalDb:
         else:
             self.field_names = field_names
         
-        if manifest_args is None:
-            manifest_args = {}
-        self.manifest_args = manifest_args
+        
         
     def __repr__(self):
         return f"CalcCalDb {self.name}"
@@ -116,12 +122,16 @@ class _MakeCalDb:
         Arguments
         ----------
         obs_id : obs_id
-        samples : a range of samples to run and return these calculations
+        samples : a list of sample ranges to run and return these calculations
+            Ex: [(0, 2000), (2000, 4000), ...]
         
         Returns :
         -----------
         ids : [n_dets]
-        result : [n_dets, len(samples)]
+        samples : None or samples as described in argument
+            used when sample range determined within function
+        result : [n_dets, len(self.field_list), ...]
+            exact result formatting is different for different CalDb Makers
         """
         raise NotImplementedError("calc function must be defined by child classes")
     
@@ -139,7 +149,10 @@ class _MakeCalDb:
         if not os.path.exists(self.h5prefix):
             os.makedirs(self.h5prefix)
         
-        attrs = {'sotodlib.version':sotodlib.__version__}
+        attrs = {
+            'sotodlib.version':sotodlib.__version__,
+            'save_time': dt.datetime.now().strftime('%Y-%m-%d %H:%M'),
+        }
         attrs.update(save_attrs)
         
         dataset = f"{obs_id}"
@@ -167,12 +180,21 @@ class _MakeCalDb:
             self.db.to_file(self.db_file)
         
 class Make_ObsCalDb(_MakeCalDb):
-    
+    """This CalDb maker is designed for data that is some N numbers per detector per 
+    observation. These N numbers should all apply for the entire observation. An 
+    example of this could be a 1/f noise calculator that returns white noise, 
+    spectral index, and knee frequency per detector per observation.
+    """
     def __init__(self, name, context, **kwargs): 
         super().__init__(name, context, **kwargs)
     
     def write_h5_data(self, h5_file, dataset, samples, 
                       ids, result, attrs, overwrite=False ):
+        
+        if len(samples) != 1:
+            raise ValueError("Expect len(samples)==1 where samples=[(0,obs_n_samps)]"
+                            f"received {samples}")
+        attrs['n_samps'] = samples[0][1]
         
         rs = metadata.ResultSet(keys=['dets:readout_id', *self.field_names])
         
@@ -191,4 +213,75 @@ class Make_ObsCalDb(_MakeCalDb):
     def calc(self, obs_id, samples=None):
         if samples is not None:
             raise ValueError(f"{type(self).__name__} does not accept samples argument")
+        
+        flist = self.context.obsfiledb.get_files(obs_id)
+        nsamps = [flist[dset][-1][2] for dset in flist]
+        if not np.all(nsamps != nsamps[0]):
+            raise ValueError("nsamps different for different detsets.")
+        return [(0,nsamps[0])]
+
+class Make_SampleCalDb(_MakeCalDb):
+    """This CalDb maker is designed for data that is indexed along select samples of 
+    an observation. An example of this might be a glitch detection algorithm that returns
+    detected glitches at certain samples and their significance at that sample.
+    """
+    def __init__(self, name, context, **kwargs):
+        super().__init__(name, context, **kwargs)
+        
+    def calc(self, obs_id, samples=None):
+        if samples is not None:
+            raise ValueError(f"{type(self).__name__} does not accept samples argument")
+        
+        flist = self.context.obsfiledb.get_files(obs_id)
+        nsamps = [flist[dset][-1][2] for dset in flist]
+        if not np.all(nsamps != nsamps[0]):
+            raise ValueError("nsamps different for different detsets.")
+        return [(0,nsamps[0])]
             
+    def write_h5_data( self, h5_file, dataset, samples, 
+                      ids, result, attrs, overwrite=False):
+        if len(ids) != len(result):
+            raise ValueError("Expect ids and result to be the same length."
+                             f" Received {len(ids)} and {len(result)} instead")
+        
+        if len(samples) != 1:
+            raise ValueError("Expect len(samples)==1 where samples=[(0,obs_n_samps)]"
+                            f"received {samples}")
+            
+        if isinstance(result, list):
+            for r in result:
+                if len(r) != len(self.field_names):
+                    raise ValueError(f"Expect result[item] to have {len(self.field_names)} items."
+                                    f"Received {len(r)} instead.")
+        elif isinstance(result, RangesMatrix):
+            raise NotImplementedError("I don't know how to save RangesMatrix yet")
+        else:
+            raise ValueError("Result not formatted for hdf5 saving")
+
+        f = h5py.File(h5_file, "a")
+        if dataset in f and not overwrite:
+            raise ValueError(f"{dataset} already saved in {h5_file}")
+        elif dataset in f: 
+            del f[dataset]
+
+        f.create_group(dataset)
+        for k in attrs:
+            f[dataset].attrs[k] = attrs[k]
+        f[dataset].attrs['n_samps'] = samples[0][1]
+        f[dataset].create_dataset('dets:readout_id', data=axisman_io._retype_for_write(ids) )
+
+        for i, field in enumerate(self.field_names):
+            ends = []
+            vals = []
+            for r in result:
+                if len(ends)==0:
+                    ends.append(len(r[i]))
+                else:
+                    ends.append(ends[-1] + len(r[i]))
+                vals.append(r[i])
+            vals = np.hstack(vals)
+            f[dataset].create_group(field)
+            f[dataset][field].create_dataset("ends", data=ends)
+            f[dataset][field].create_dataset("vals", data=vals)
+        
+        f.close()
