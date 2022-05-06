8d6086370d95a6934a2bb7e0c89e792f649caecc
Matthew Hasselfield <mhasself@users.noreply.github.com>
2022-05-06 17:30:41 +0000

    site_pipeline: make_source_flags fixes

--- sotodlib/site_pipeline/make_source_flags.py
+++ sotodlib/site_pipeline/make_source_flags.py
@@ -7,6 +7,7 @@ import yaml
 
 import sotodlib
 from sotodlib import core, coords, site_pipeline
+from . import util
 
 logger = logging.getLogger(__name__)
 
@@ -41,12 +42,11 @@ def main(args=None):
 
     ctx = core.Context(config['context_file'])
 
-    group_by = config['subobs'].get('use', 'detset')
-    if group_by == 'detset':
-        groups = ctx.obsfiledb.get_detsets(config['obs_id'])
-    elif group_by.startswith('dets:'):
+    # This will process data by detset, but will insist that this maps
+    # one-to-one with some detdb field.
+    group_by = config['subobs'].get('group_by', 'dets:detset')
+    if group_by.startswith('dets:'):
         group_by = group_by.split(':',1)[1]
-        groups = ctx.detdb.props(props=group_by).distinct()[group_by]
     else:
         raise ValueError("Can't group by '{group_by}'")
 
@@ -57,26 +57,39 @@ def main(args=None):
         logger.info(f'Creating {config["archive"]["index"]} for the archive index.')
         scheme = core.metadata.ManifestScheme()
         scheme.add_exact_match('obs:obs_id')
-        if group_by != 'detset':
-            scheme.add_exact_match('dets:' + group_by)
+        scheme.add_data_field('dets:' + group_by)
         scheme.add_data_field('dataset')
         db = core.metadata.ManifestDb(config['archive']['index'], scheme=scheme)
 
-    for group in groups:
-        logger.info(f'Loading {config["obs_id"]}:{group_by}={group}')
-        if group_by == 'detset':
-            # Load pointing and dets axis; we don't need signal though.
-            tod = ctx.get_obs(config['obs_id'], detsets=[group],
-                              no_signal=True)
-        else:
-            tod = ctx.get_obs(config['obs_id'], dets={group_by: group},
-                              no_signal=True)
+    # We will loop over detsets here, but insist on a simple
+    # correspondence to the output index values.
+    index_map = {}
+
+    detsets = ctx.obsfiledb.get_detsets(config['obs_id'])
+    for detset in detsets:
+        logger.info(f'Loading {config["obs_id"]}:detset={detset}')
+        # Load pointing and dets axis; we don't need signal though.
+        tod = ctx.get_obs(config['obs_id'], detsets=[detset],
+                          no_signal=True)
+
+        # Boost detdb into array_data.
+        if 'array_data' not in tod:
+            util.promote_array_data(ctx, tod)
+
+        # Figure out the indexing info for this.
+        this_index = tod.array_data[group_by]
+        assert([x == this_index[0] for x in this_index])
+        this_index = this_index[0]
+        if this_index in index_map:
+            raise RuntimeError(f"Some detset already mapped to {this_index}: "
+                               f"{index_map[this_index]}")
+        index_map[this_index] = detset
+        logger.info(f'detset "{detset}" corresponds to {group_by}="{this_index}"')
 
         # Load / compute mask parameters.
         mask_params = config['mask_params']['default']
-        mask_res = site_pipeline.util.parse_angle(
-            site_pipeline.util.lookup_conditional(config['mask_params'], 'res',
-                                                  default=[1, 'arcmin']))
+        mask_res = util.parse_angle(util.lookup_conditional(
+            config['mask_params'], 'res', default=[1, 'arcmin']))
 
         sources = coords.planets.get_nearby_sources(tod)
         flags = None
@@ -102,21 +115,18 @@ def main(args=None):
         logger.info(f'Total mask weight is {weight*100:.2}%.')
 
         # Wrap result into AxisManager for HDF5 off-load.
-        aman = core.AxisManager(tod.dets)
-        aman.wrap('source_flags', flags, [(0, 'dets')])
+        aman = core.AxisManager(tod.dets, tod.samps)
+        aman.wrap('source_flags', flags, [(0, 'dets'), (1, 'samps')])
 
         # Get file + dataset from policy.
-        policy = site_pipeline.util.ArchivePolicy.from_params(config['archive']['policy'])
+        policy = util.ArchivePolicy.from_params(config['archive']['policy'])
         dest_file, dest_dataset = policy.get_dest(config['obs_id'])
-        if group_by == 'detset':
-            dest_dataset += '_' + group
         aman.save(dest_file, dest_dataset, overwrite=True)
 
         # Update the index.
         db_data = {'obs:obs_id': config['obs_id'],
-                   'dataset': dest_dataset}
-        if group_by != 'detset':
-            db_data['dets:'+group_by] = group
+                   'dataset': dest_dataset,
+                   f'dets:{group_by}': this_index}
         db.add_entry(db_data, dest_file, replace=True)
 
     # Return something?
