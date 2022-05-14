9a53822a6ad3553dacacc8ccb900349745df5957
Matthew Hasselfield <mhasself@users.noreply.github.com>
2022-05-14 00:04:16 +0000

    context: bug fixes and cleanup and a new set of tests

--- tests/test_context.py
+++ tests/test_context.py
@@ -8,7 +8,8 @@
 import unittest
 import tempfile
 
-from sotodlib.core import metadata, Context
+from sotodlib import core
+from sotodlib.core import metadata, Context, OBSLOADER_REGISTRY
 from sotodlib.io.metadata import ResultSetHdfLoader, write_dataset, _decode_array
 
 import os
@@ -93,6 +94,179 @@ class ContextTest(unittest.TestCase):
         self.assertEqual(len(req['dets']), 1)
         req = ctx.get_obs('obs0', logic_only=True, dets={'band': 'f090'})
         self.assertEqual(len(req['dets']), len(det_list) // 2)
+
+    def test_100_loads(self):
+        dataset_sim = DatasetSim()
+
+        # Test detector resolutions ...
+        ctx = dataset_sim.get_context()
+        n = len(dataset_sim.dets)
+        for selection, count in [
+                ({'dets:readout_id': ['det05']}, 1),
+                ({'dets:detset': 'neard'}, 4),
+                ({'dets:detset': ['neard']}, 4),
+                ({'dets:detset': ['neard', 'fard']}, 8),
+                ({'dets:detset': ['neard'], 'dets:readout_id': ['det00', 'det05']}, 1),
+                ({'dets:readout_id': ['xx']}, 0),
+                ({'dets:band': ['f090']}, 4),
+                ({'dets:band': ['f090'], 'dets:detset': ['neard']}, 2),
+        ]:
+            selection['obs:obs_id'] = dataset_sim.obss['obs_id'][1]
+            meta = ctx.get_meta(selection)
+            self.assertEqual(meta.dets.count, count, msg=f"{selection}")
+
+        # And without detdb nor metadata
+        ctx = dataset_sim.get_context(with_detdb=False)
+        for selection, count in [
+                ({'dets:detset': ['neard']}, 4),
+        ]:
+            selection['obs:obs_id'] = dataset_sim.obss['obs_id'][1]
+            meta = ctx.get_meta(selection)
+            self.assertEqual(meta.dets.count, count, msg=f"{selection}")
+
+    def test_110_more_loads(self):
+        dataset_sim = DatasetSim()
+        n_det, n_samp = dataset_sim.det_count, dataset_sim.sample_count
+        obs_id = dataset_sim.obss['obs_id'][1]
+
+        # Test some TOD loading.
+        ctx = dataset_sim.get_context()
+        tod = ctx.get_obs(obs_id)
+        self.assertEqual(tod.signal.shape, (n_det, n_samp))
+
+        tod = ctx.get_obs(obs_id + ':f090')
+        self.assertEqual(tod.signal.shape, (n_det//2, n_samp))
+
+        tod = ctx.get_obs(obs_id, free_tags=['f150'])
+        self.assertEqual(tod.signal.shape, (n_det//2, n_samp))
+
+        tod = ctx.get_obs(obs_id, samples=(10, n_samp // 2))
+        self.assertEqual(tod.signal.shape, (n_det, n_samp // 2 - 10))
+
+        # Loading via filename
+        tod = ctx.get_obs(filename='obs_number_11_neard.txt')
+        self.assertEqual(tod.signal.shape, (n_det//2, n_samp))
+
+        # Loading via prepopulated & modified meta
+        meta = ctx.get_meta(obs_id)
+        meta.restrict('dets', meta.dets.vals[:5])
+        tod = ctx.get_obs(meta)
+        self.assertEqual(tod.signal.shape, (5, n_samp))
+
+
+class DatasetSim:
+    """Provide in-RAM Context objects and tod/metadata loader functions
+    for Context behavior testing.
+
+    """
+    def __init__(self):
+        self.dets = metadata.ResultSet(
+            ['readout_id', 'band', 'pol_code', 'x', 'y'],
+            [('det00', 'f090', 'A', 0.0, 0.0),
+             ('det01', 'f090', 'B', 0.0, 0.0),
+             ('det02', 'f150', 'A', 0.0, 0.0),
+             ('det03', 'f150', 'B', 0.0, 0.0),
+             ('det04', 'f090', 'A', 1.0, 0.0),
+             ('det05', 'f090', 'B', 1.0, 0.0),
+             ('det06', 'f150', 'A', 1.0, 0.0),
+             ('det07', 'f150', 'B', 1.0, 0.0)])
+
+        self.obss = metadata.ResultSet(
+            ['obs_id', 'timestamp', 'type', 'target'],
+            [('obs_number_11', 1600010000., 'planet', 'uranus'),
+             ('obs_number_12', 1600020000., 'survey', 'the_cmb'),
+             ('obs_number_13', 1600030000., 'survey', 'the_cmb'),
+            ])
+
+        self.det_count = len(self.dets)
+        self.sample_count = 100
+
+        class _TestML(metadata.LoaderInterface):
+            def from_loadspec(_self, load_params):
+                return self.metadata_loader(load_params)
+
+        OBSLOADER_REGISTRY['unittest_loader'] = self.tod_loader
+        metadata.SuperLoader.register_metadata('unittest_loader', _TestML)
+
+    def get_context(self, with_detdb=True, with_metadata=True):
+        detdb = metadata.DetDb()
+        detdb.create_table('base', ['readout_id string',
+                                    'pol_code string',
+                                    'x float',
+                                    'y float'])
+        save_for_later = ['band']
+        for row in self.dets.subset(keys=[k for k in self.dets.keys
+                                          if k not in save_for_later]):
+            detdb.add_props('base', row['readout_id'], **row)
+
+        obsdb = metadata.ObsDb()
+        obsdb.add_obs_columns([f'{k} string' for k in self.obss.keys
+                               if k not in ['obs_id', 'timestamp']])
+        for row in self.obss:
+            obsdb.update_obs(row['obs_id'], data=row)
+
+        obsfiledb = metadata.ObsFileDb()
+        obsfiledb.add_detset('neard', self.dets['readout_id'][:len(self.dets)//2])
+        obsfiledb.add_detset('fard',  self.dets['readout_id'][len(self.dets)//2:])
+        for obs_id in self.obss['obs_id']:
+            obsfiledb.add_obsfile(f'{obs_id}_neard.txt', obs_id, 'neard',
+                                  0, self.sample_count)
+            obsfiledb.add_obsfile(f'{obs_id}_fard.txt', obs_id, 'fard',
+                                  0, self.sample_count)
+
+        ctx = Context(data=MINIMAL_CONTEXT)
+        ctx.obsdb = obsdb
+        ctx.obsfiledb = obsfiledb
+        if with_detdb:
+            ctx.detdb = detdb
+        ctx.reload(['loader'])
+        ctx.update({
+            'obs_loader_type': 'unittest_loader',
+            'obs_colon_tags': ['band'],
+        })
+
+        # metadata: bands.h5
+        _scheme = metadata.ManifestScheme() \
+                  .add_data_field('loader') \
+                  .add_range_match('obs:timestamp')
+        bands_db = metadata.ManifestDb(scheme=_scheme)
+        bands_db.add_entry({'obs:timestamp': [0, 2e9], 'loader': 'unittest_loader'}, 'bands.h5')
+
+        if with_metadata:
+            ctx['metadata'] = [
+                {'db': bands_db,
+                 'det_info': True,
+                 'dets_key': 'readout_id'},
+            ]
+
+        return ctx
+
+    def metadata_loader(self, kw):
+        # For Superloader.
+        filename = os.path.split(kw['filename'])[1]
+        if filename == 'bands.h5':
+            rs = self.dets.subset(keys=['readout_id', 'band'])
+            rs.keys = ['dets:' + k for k in rs.keys]
+            return rs
+        else:
+            raise ValueError(f'metadata request for "{filename}"')
+
+    def tod_loader(self, obsfiledb, obs_id, dets=None, prefix=None,
+                   samples=None, no_signal=None,
+                   **kwargs):
+        # For Context.get_obs
+        if samples is None:
+            samples = 0, None
+        samples = list(samples)
+        if samples[1] is None:
+            samples[1] = self.sample_count
+        samples = [s if s >= 0 else self.sample_count + s
+                   for s in samples]
+
+        tod = core.AxisManager(core.LabelAxis('dets', dets),
+                               core.OffsetAxis('samps', samples[1] - samples[0], samples[0]))
+        tod.wrap_new('signal', ('dets', 'samps'))
+        return tod
 
 
 if __name__ == '__main__':
