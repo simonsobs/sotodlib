3133a557092a323042b53802385ae9160afaeec1
Jack Lashner <jlashner@gmail.com>
2022-06-13 20:45:56 -0400

    Implements Matthew's suggestions and adds tests

--- sotodlib/core/axisman_util.py
+++ sotodlib/core/axisman_util.py
@@ -6,25 +6,25 @@ import numpy as np
 from sotodlib.core import AxisManager
 
 class RestrictionException(Exception):
-    """Exception for when cannot restrict AxisManger properly"""
+    """Exception for when cannot restrict AxisManager properly"""
 
 def restrict_to_times(am, t0, t1, in_place=False):
     """
     Restricts axis manager to a time range (t0, t1)
     """
-    m = (t0 < am.timestamps) & (am.timestamps < t1)
+    m = (t0 <= am.timestamps) & (am.timestamps < t1)
     if not m.any():
-        raise RestrictionException
+        raise RestrictionException()
     i0, i1 = np.where(m)[0][[0, -1]] + am.samps.offset
-    return am.restrict('samps', (i0, i1), in_place=in_place)
+    return am.restrict('samps', (i0, i1+1), in_place=in_place)
 
 def dict_to_am(d, skip_bad_types=False):
     """
-    Attempts to convert a dictionary into an axis manager. This can be used on
+    Attempts to convert a dictionary into an AxisManager. This can be used on
     dicts containing basic types such as (str, int, float) along with numpy
     arrays. The AxisManager will not have any structure such as "axes", but
     this is useful if you want to nest semi-arbitrary data such as the "meta"
-    dict into an axis manager.
+    dict into an AxisManager.
 
     Args
     -----
@@ -39,7 +39,7 @@ def dict_to_am(d, skip_bad_types=False):
     for k, v in d.items():
         if isinstance(v, dict):
             am.wrap(k, dict_to_am(v))
-        if isinstance(v, allowed_types):
+        elif isinstance(v, allowed_types):
             am.wrap(k, v)
         elif not skip_bad_types:
             raise ValueError(
