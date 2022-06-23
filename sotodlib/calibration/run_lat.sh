10117061f6ecfd9b127fa02369be252747e0b928
Zhilei Xu <astro.zlxu@gmail.com>
2022-06-23 02:39:49 +0000

    Updating the run_lat.sh file.

--- sotodlib/calibration/run_lat.sh
+++ sotodlib/calibration/run_lat.sh
@@ -4,18 +4,19 @@ ntasks=8
 indir=/home/zhileixu/data/simulated_tod/LAT/i6
 outdir=/home/zhileixu/data/fitting_results
 
-mpirun -n ${ntasks} ./calibration_tod_analysis.py \
-        --indir ${indir} \
-        --outdir ${outdir} \
-        --f_format g3 \
-        --tele LAT \
-        --tube i6 \
-        --band f090 \
-        --wafer w13 \
-        --sso_name Saturn \
-        --year 2022 \
-        --month 9 \
-        --day 1 \
-        --highpass \
-        --cutoff 0.2 \
-        
+for start_day in {1..5}; do
+    mpirun -n ${ntasks} ./calibration_tod_analysis.py \
+            --indir ${indir} \
+            --outdir ${outdir} \
+            --f_format g3 \
+            --tele LAT \
+            --tube i6 \
+            --band f090 \
+            --wafer w13 \
+            --sso_name Saturn \
+            --year 2022 \
+            --month 9 \
+            --day $start_day \
+            --highpass \
+            --cutoff 0.2
+done  
