068de3fb1c78b39526a6ec0a4672c5383fe3169e
yukisakurai <ysakurai@s.okayama-u.ac.jp>
2022-09-13 08:31:00 +0000

    update name and docs

--- sotodlib/hwp/hwp.py
+++ sotodlib/hwp/hwp.py
@@ -24,19 +24,29 @@ def func_hwpss_all(x, A1, A2, A3, A4, A5, phi1, phi2, phi3, phi4, phi5):
             A3*np.cos(3*x+phi3) + A4*np.cos(4*x+phi4) + A5*np.cos(5*x+phi5)
 
 
-def extract_hwpss(aman, hwp_angle=None, mode=0, 
-                  name='hwpss_ext', signal='signal',
+def extract_hwpss(aman, signal='signal', name='hwpss_ext',
+                  hwp_angle=None, mode=0, 
                   prefilt_cutoff=0.1, bins=3601):
     """
-    ** HWPSS extraction function **
-    Args ---
-    aman: AxisManager
-    mode: 0 = 2f&4f, 1 = 2f only, 2 = 4f only, 3 = all up to 5f
-    name: aman output name
-    signal: target timestream data
-    Return ---
-    Adding aman.hwpss
+    * Extract HWP synchronous signal from given tod (AxisManager)
+    * Estimated HWPSS will be added to AxisManager as aman['name']
+
+    Args
+    -----
+        aman: AxisManager
+            AxisManager including target timestream data
+        signal: str
+            name of target timestream data in AxisManager
+        name: str
+            output timestram name into AxisManager
+        mode: int 
+            0 = 2f&4f, 1 = 2f only, 2 = 4f only, 3 = all up to 5f
+        prefit_cutoff: float
+            cut off value of pre-lowpass filter
+        bins: int
+            number of bins for hwpss histogram
     """
+    
     filt = tod_ops.filters.high_pass_sine2(cutoff=prefilt_cutoff)
     signal_prefilt = np.array(tod_ops.fourier_filter(
         aman, filt, detrend=None, signal_name=signal))
@@ -68,55 +78,66 @@ def extract_hwpss(aman, hwp_angle=None, mode=0,
         aman[name][i] = func(aman.hwp_angle, *popt)
 
 
-def demod(aman, hwp_angle=None, bpf_width=0.5, lpf_cut=0.5, signal='signal'):
+def demod(aman, signal='signal', name='signal_demod',
+          hwp_angle=None, bpf_width=0.5, lpf_cut=0.5):
     """
-    ** Simple demoduation function **
-    Args ---
+    * Simple demoduation function for AxisManager
+    * Demod timstream will be added to AxisManager as aman['name']
+    
+    Args
+    -----
     aman: AxisManager
-    hwp_angle: HWP reconstructed angle, do not need to input if aman already has this
-    bpf_width: Width of pre-bandpass filter
-    lpf_cut: cut off of low pass filter after applying demod. factor
-    Return ---
-    Adding aman.signal_demod
+        target AxisManager
+    signal: str
+        name of target timestream
+    name: str
+        output timestram name into AxisManager
+    hwp_angle: list or None
+        HWP reconstructed angle, 
+        do not need to input if aman already has this
+    bpf_width: float
+        Width of pre-bandpass filter
+    lpf_cut: float
+        cut off of low pass filter after applying demod. factor
     """
     if hwp_angle is not None:
         if 'hwp_angle' in aman.keys():
             aman.move('hwp_angle', None)
         aman.wrap('hwp_angle', hwp_angle)
-    if 'signal_demod_prelfilt' in aman.keys():
-        aman.move('signal_demod_prelfilt', None)
-    if 'signal_demod_prebfilt' in aman.keys():
-        aman.move('signal_demod_prebfilt', None)
-    if 'signal_demod_wo_lfilt' in aman.keys():
-        aman.move('signal_demod_wo_lfilt', None)
-    if 'signal_demod' in aman.keys():
-        aman.move('signal_demod', None)
+    if name + '_prelfilt' in aman.keys():
+        aman.move(name + '_prelfilt', None)
+    if name + '_prebfilt' in aman.keys():
+        aman.move(name + '_prebfilt', None)
+    if name + '_wo_lfilt' in aman.keys():
+        aman.move(name + '_wo_lfilt', None)
+    if name in aman.keys():
+        aman.move(name, None)
 
     speed = (np.sum(np.diff(aman.hwp_angle) % (2 * np.pi)) /
              (aman.timestamps[-1] - aman.timestamps[0])) / (2 * np.pi)
     prelfilt = tod_ops.filters.low_pass_butter4(fc=4 * speed + bpf_width)
-    aman.wrap('signal_demod_prelfilt', 
+    aman.wrap(name + '_prelfilt', 
               tod_ops.fourier_filter(aman, prelfilt, 
                                      detrend=None, 
                                      signal_name=signal), 
               [(0, 'dets'), (1, 'samps')])
     prehfilt = tod_ops.filters.high_pass_butter4(fc=4 * speed - bpf_width)
-    aman.wrap('signal_demod_prebfilt', 
+    aman.wrap(name + '_prebfilt', 
               tod_ops.fourier_filter(aman, prehfilt, 
                                      detrend=None, 
-                                     signal_name='signal_demod_prelfilt'), 
+                                     signal_name=name + '_prelfilt'), 
               [(0, 'dets'), (1, 'samps')])
 
-    aman.wrap('signal_demod_wo_lfilt', 
-              (aman.signal_demod_prebfilt * np.exp(-1j * (4 * aman.hwp_angle))).real, 
+    aman.wrap(name + '_wo_lfilt', 
+              (aman[name + '_prebfilt'] * np.exp(-1j * (4 * aman.hwp_angle))).real, 
               [(0, 'dets'), (1, 'samps')])
     lfilt = tod_ops.filters.low_pass_butter4(fc=lpf_cut)
-    aman.wrap('signal_demod', 
+    aman.wrap(name,
               tod_ops.fourier_filter(aman, lfilt, 
                                      detrend=None, 
-                                     signal_name='signal_demod_wo_lfilt'), 
+                                     signal_name=name+'_wo_lfilt'), 
               [(0, 'dets'), (1, 'samps')])
 
-    aman.move('signal_demod_prelfilt', None)
-    aman.move('signal_demod_prebfilt', None)
-    aman.move('signal_demod_wo_lfilt', None)
+    aman.move(name + '_prelfilt', None)
+    aman.move(name + '_prebfilt', None)
+    aman.move(name + '_wo_lfilt', None)
