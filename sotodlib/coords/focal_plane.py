f3b8ccdf896b9f65982f9b548e0465de2ad65d77
Saianeesh Keshav Haridas <accounts@saianeesh.com>
2023-01-10 18:57:18 +0000

    Moved code for loading zemax data into its own functions

--- sotodlib/coords/focal_plane.py
+++ sotodlib/coords/focal_plane.py
@@ -7,10 +7,12 @@ Requires zemax to be running or set get_data to be False and
 @author: sdicker
 """
 import numpy as np
+import logging
 from scipy.interpolate import interp2d
 
-LAT_FOV=3.7 # LAT FOV in degrees
+logger = logging.getLogger(__name__)
 
+LAT_FOV=3.7 # LAT FOV in degrees
 '''
 Dictionary of zemax tube layout.
 +ve x is to the right as seen from back of the cryostat *need to check*
@@ -23,25 +25,6 @@ Below config assumes a 30 degree rotation
 '''
 LAT_TUBES={'c':1,'i1':4,'i2':2,'i3':8,'i4':9,'i5':3,'i6':5, 'o1':6,'o2':10,'o3':13,'o4':11,'o5':7,'o6':12}
 
-dat=np.load(dir+id9.split('.')[0]+'_trace_data.npz',allow_pickle=True)
-tube_mapping=dat['tube_mapping'][()]
-LAT=dat['LAT'][()]
-LATR=dat['LATR'][()]
-
-
-gi=np.where(LAT['mask'] != 0.)
-sec2elev = interp2d(LAT['x'][gi],LAT['y'][gi],LAT['elev'][gi],bounds_error=True)
-sec2xel  = interp2d(LAT['x'][gi],LAT['y'][gi],LAT['xel'][gi],bounds_error=True)
-array2secx=[] #set up optics tubes....
-array2secy=[]
-for n in range(len(LATR)):
-    
-    print('working on tube ',n+1)
-    gi=np.where(LATR[n]['mask'] != 0)
-    array2secx.append(
-            interp2d(LATR[n]['array_x'][gi],LATR[n]['array_y'][gi],LATR[n]['sec_x'][gi],bounds_error=True))
-    array2secy.append(
-            interp2d(LATR[n]['array_x'][gi],LATR[n]['array_y'][gi],LATR[n]['sec_y'][gi],bounds_error=True))
 def pix2sky(x,y,tube,rot=0,opt2cryo=0.):
     '''Routine to map pixels from arrays to sky
     x,y = position on focal plane (currently zemax coord)
@@ -61,3 +44,63 @@ def pix2sky(x,y,tube,rot=0,opt2cryo=0.):
     elev=sec2elev(xrot,yrot) # note these are around the telescope boresight
     xel=sec2xel(xrot,yrot)
     return elev,xel
+
+def LAT_optics(zemax_dat):
+    if type(zemax_dat) is str:
+        try: 
+            zemax_dat = np.load(zemax_dat, allow_pickle=True)
+        except Exception as e:
+            logger.error("Can't load data from " + zemax_dat)
+            raise e
+    elif type(zemax_dat) is not dict:
+        logger.error("zemax_dat should either be a path or a dictionary")
+        raise TypeError
+    try:
+        LAT=zemax_dat['LAT'][()]
+    except Exception as e:
+        logger.error("LAT key missing from dictionary")
+        raise e
+    
+    gi=np.where(LAT['mask'] != 0.)
+    sec2elev = interp2d(LAT['x'][gi],LAT['y'][gi],LAT['elev'][gi],bounds_error=True)
+    sec2xel  = interp2d(LAT['x'][gi],LAT['y'][gi],LAT['xel'][gi],bounds_error=True)
+
+    return sec2elev, sec2xel
+
+def LATR_optics(zemax_dat, tube):
+    if type(zemax_dat) is str:
+        try: 
+            zemax_dat = np.load(zemax_dat, allow_pickle=True)
+        except Exception as e:
+            logger.error("Can't load data from " + zemax_dat)
+            raise e
+    elif type(zemax_dat) is not dict:
+        logger.error("zemax_dat should either be a path or a dictionary")
+        raise TypeError
+    try:
+        LATR=zemax_dat['LATR'][()]
+    except Exception as e:
+        logger.error("LATR key missing from dictionary")
+        raise e
+    
+    if type(tube) is str:
+        tube_name = tube
+        try:
+            tube_num = LAT_TUBES[tube]
+        except Exception as e:
+            logger.error("Invalid tube name")
+            raise e
+    elif type(tube) is int:
+        tube_num = tube
+        try:
+            tube_name = list(LAT_TUBES.keys())[tube_num]
+        except Exception as e:
+            logger.error("Invalid tube number")
+            raise e
+
+    logger.info('Working on LAT tube ' + tube_name)
+    gi=np.where(LATR[tube_num]['mask'] != 0)
+    array2secx = interp2d(LATR[tube_num]['array_x'][gi],LATR[tube_num]['array_y'][gi],LATR[tube_num]['sec_x'][gi],bounds_error=True)
+    array2secy = interp2d(LATR[tube_num]['array_x'][gi],LATR[tube_num]['array_y'][gi],LATR[tube_num]['sec_y'][gi],bounds_error=True)
+
+    return array2secx, array2secy
