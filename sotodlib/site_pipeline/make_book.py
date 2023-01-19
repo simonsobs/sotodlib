59fa66b51e0ce4b98f1b8810a5e0ca97107b2f31
Yilun Guan <zoom.aaron@gmail.com>
2023-01-19 05:41:10 +0000

    make_book: adapt to latest imprinter

--- sotodlib/site_pipeline/make_book.py
+++ sotodlib/site_pipeline/make_book.py
@@ -1,11 +1,13 @@
+from typing import Optional
 import typer
+
 from ..io.imprinter import Imprinter
 
 
 def main(
     im_config: str,
-    g3tsmurf_config: str,
     output_root: str,
+    source: Optional[str],
     ):
     """Make books based on imprinter db
     
@@ -13,15 +15,18 @@ def main(
     ----------
     im_config : str
         path to imprinter configuration file
-    g3tsmurf_config : str
-        path to the g3tsmurf db configuration file
     output_root : str
         root path of the books 
+    source: str, optional
+        data source to use, e.g., sat1, latrt, tsat. If None, use all sources
     """
-    imprinter = Imprinter(im_config, g3tsmurf_config)
+    imprinter = Imprinter(im_config, db_args={'connect_args': {'check_same_thread': False}})
     # get unbound books
     unbound_books = imprinter.get_unbound_books()
     failed_books = imprinter.get_failed_books()
+    if source is not None:
+        unbound_books = [book for book in unbound_books if book.tel_tube == source]
+        failed_books = [book for book in failed_books if book.tel_tube == source]
     print(f"Found {len(unbound_books)} unbound books and {len(failed_books)} failed books")
     for book in unbound_books:
         print(f"Binding book {book.bid}")
