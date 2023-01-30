fa3e356b1913a7601a068a3ad27f87da9fdca9c8
Brian Koopman <brian.koopman@yale.edu>
2023-01-30 15:02:40 -0500

    Migrate qds monitor from pwg-scripts structure

--- pwg-qds/setup.py
+++
@@ -1,6 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(name='qds',
-      description='Quality Assurance/Control and Data Selection',
-      packages=find_packages(),
-      version="v0.1.0")
