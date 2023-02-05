7c30b1ed7bfd8135366b283d6fbca0feaa96fa74
sanahabhimani <sanahbhimani@gmail.com>
2023-02-05 01:44:51 +0000

    input logging statements

--- sotodlib/io/hk_utils.py
+++ sotodlib/io/hk_utils.py
@@ -1,10 +1,13 @@
 import numpy as np
 import yaml
 import itertools
+import logging
 
 from so3g.hk import load_range
 from sotodlib import core
 
+logging.basicConfig(level=logging.INFO, format='%(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 
 def _get_swap_dict(d):
     """
@@ -21,7 +24,7 @@ def get_grouped_hkdata(start, stop, config):
     that list of data.
     """
     # call load_range()
-    print("running load_range()")
+    logging.info("running load_range()")
     hkdata = load_range(start, stop, config=config)
 
     # load all fields from config file
@@ -99,7 +102,7 @@ def make_hkaman(grouped_data):
             # TODO: diff of times, if less than say 10%??, treat as cosampled
         except AssertionError:
             # if not cosampled, make aman for each field in device/feed
-            print("{} is not cosampled".format('.'.join(field.split('.')[0:3])))
+            logging.info("{} is not cosampled".format('.'.join(field.split('.')[0:3])))
             for field in group:
                 alias = group[field][0]
                 time = group[field][1]
