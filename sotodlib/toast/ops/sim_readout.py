9783718acf6b1e2bb174d5289ba308cb873ee15b
Reijo Keskitalo <reijo.keskitalo@gmail.com>
2023-02-15 20:56:07 +0000

    Simple implementation of add_glitches

--- sotodlib/toast/ops/sim_readout.py
+++ sotodlib/toast/ops/sim_readout.py
@@ -31,6 +31,8 @@ class SimReadout(Operator):
 
     API = Int(0, help="Internal interface version for this operator")
 
+    realization = Int(0, help="The noise realization index")
+
     times = Unicode(defaults.times, help="Observation shared key for timestamps")
 
     shared_flags = Unicode(
@@ -48,32 +50,65 @@ class SimReadout(Operator):
         help="Observation detdata key for simulated signal",
     )
 
+    glitch_rate = Quantity(1.0 * u.Hz, help="Glitch occurrence rate")
+
+    glitch_amplitude_center = Quantity(
+        1 * u.K, help="Center of Gaussian distribution for glitch amplitude"
+    )
+
+    glitch_amplitude_sigma = Quantity(
+        1 * u.K, help="Width of Gaussian distribution for glitch amplitude"
+    )
+
+    simulate_glitches = Bool(True, help="Enable glitch simulation")
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
     @function_timer
-    def _add_glitches(self, times, focalplane, signal, dets):
+    def _add_glitches(self, obs_id, times, focalplane, signal, dets):
         """Simulate glitches"""
+        if not self.simulate_glitches:
+            return
+        fsample = focalplane.sample_rate
+        events_per_sample = self.glitch_rate.to_value(u.Hz) / fsample.to_value(u.Hz)
+        nsample = times.size
         for det in dets:
+            det_id = focalplane[det]["uid"]
             sig = signal[det]
+            np.random.seed(
+                int(obs_id + det_id  + self.realization) % 2**32
+            )
+            event_counts = np.random.poisson(events_per_sample, nsample)
+            event_indices = np.argwhere(event_counts).ravel()
+            nindex = event_indices.size
+            # Drawing amplitudes for every affected sample is not the
+            # same as drawing amplitudes for every event but it is fast.
+            # If we routinely have more than one event occurring during
+            # the same time stamp we are in trouble
+            amplitudes = self.glitch_amplitude_center \
+                         + np.random.randn(nindex) * self.glitch_amplitude_sigma
+            events = amplitudes.to_value(signal.units) \
+                     * event_counts[event_indices]
+            sig[event_indices] += events
         return
 
     @function_timer
-    def _add_jumps(self, times, focalplane, signal, dets):
+    def _add_jumps(self, obs_id, times, focalplane, signal, dets):
         """Simulate baseline jumps"""
         for det in dets:
             sig = signal[det]
         return
 
     @function_timer
-    def _fail_bolometers(self, times, focalplane, signal, dets):
+    def _fail_bolometers(self, obs_id, times, focalplane, signal, dets):
         """Simulate bolometers that fail to bias or otherwise are unusable"""
         for det in dets:
             sig = signal[det]
         return
 
     @function_timer
-    def _misidentify_channels(self, times, focalplane, signal, dets):
+    def _misidentify_channels(self, obs_id, times, focalplane, signal, dets):
         """Swap detector data between two confused readout channels"""
         for det in dets:
             sig = signal[det]
@@ -89,6 +124,7 @@ class SimReadout(Operator):
                 raise RuntimeError(f"You must set `{trait}` before running SimReadout")
 
         for obs in data.obs:
+            obs_id = obs.uid
             times = obs.shared[self.times].data
             dets = obs.select_local_detectors(detectors)
             obs.detdata.ensure(self.det_data, detectors=dets, create_units=u.K)
@@ -98,10 +134,10 @@ class SimReadout(Operator):
             focalplane = obs.telescope.focalplane
             signal = obs.detdata[self.det_data]
     
-            self._add_glitches(times, focalplane, signal, dets)
-            self._add_jumps(times, focalplane, signal, dets)
-            self._fail_bolometers(times, focalplane, signal, dets)
-            self._misidentify_channels(times, focalplane, signal, dets)
+            self._add_glitches(obs_id, times, focalplane, signal, dets)
+            self._add_jumps(obs_id, times, focalplane, signal, dets)
+            self._fail_bolometers(obs_id, times, focalplane, signal, dets)
+            self._misidentify_channels(obs_id, times, focalplane, signal, dets)
             
         return
 
