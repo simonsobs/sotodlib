29cae43d65f30195a51cd912226f4fd6f5035dca
tterasaki <tomoki.terasaki@phys.s.u-tokyo.ac.jp>
2023-05-29 01:35:00 +0000

    make it pep8-like

--- sotodlib/coords/sat_mapmake.py
+++ sotodlib/coords/sat_mapmake.py
@@ -4,12 +4,13 @@ from pixell import enmap
 
 from .. import core, tod_ops, coords
 
+
 def make_map(tod,
-            wcs_kernel = None,
-            res = 0.1*coords.DEG,
-            dsT = None, demodQ = None, demodU = None,
-            cuts = None,
-            det_weights = None, det_weights_demod = None):
+             wcs_kernel=None,
+             res=0.1 * coords.DEG,
+             dsT=None, demodQ=None, demodU=None,
+             cuts=None,
+             det_weights=None, det_weights_demod=None):
     """
     Generates maps of temperature and polarization from a TOD.
 
@@ -48,15 +49,16 @@ def make_map(tod,
         The map of inverse variance weights used in the map-making process.
     """
     if wcs_kernel is None:
-        wcs_kernel = coords.get_wcs_kernel('car', 0, 0, res)    
+        wcs_kernel = coords.get_wcs_kernel('car', 0, 0, res)
     if dsT is None:
         dsT = tod['dsT']
     if demodQ is None:
         demodQ = tod['demodQ']
     if demodU is None:
         demodU = tod['demodU']
-    
-    PQU = coords.P.for_tod(tod=tod, wcs_kernel=wcs_kernel, cuts=cuts, comps='QU')
+
+    PQU = coords.P.for_tod(
+        tod=tod, wcs_kernel=wcs_kernel, cuts=cuts, comps='QU')
 
     if det_weights is None:
         if det_weights_demod is None:
@@ -65,37 +67,46 @@ def make_map(tod,
     else:
         if det_weights_demod is None:
             det_weights_demod = det_weights / 2.
-    
+
     # T map and weight
-    mT_weighted = PQU.to_map(tod=tod, signal=dsT, comps='T', det_weights=det_weights)
+    mT_weighted = PQU.to_map(
+        tod=tod, signal=dsT, comps='T', det_weights=det_weights)
     wT = PQU.to_weights(tod, signal=dsT, comps='T', det_weights=det_weights)
-    
+
     # Q/U maps and weights
-    mQ_weighted = PQU.to_map(tod=tod, signal=demodQ, det_weights=det_weights_demod)
-    mU_weighted = PQU.to_map(tod=tod, signal=demodU, det_weights=det_weights_demod)
+    mQ_weighted = PQU.to_map(tod=tod, signal=demodQ,
+                             det_weights=det_weights_demod)
+    mU_weighted = PQU.to_map(tod=tod, signal=demodU,
+                             det_weights=det_weights_demod)
     mQU_weighted = PQU.zeros()
-    
-    #### CAUTION: Here the definition of mQU_weighted uses a wrong way of definition, as toast simulation defines that in the wrong way.
-    mQU_weighted[0][:] = mQ_weighted[0] - mU_weighted[1] # (= Q_{flipped detector coord}*cos(2 theta_pa) - U_{flipped detector coord}*sin(2 theta_pa) )
-    mQU_weighted[1][:] = mQ_weighted[1] + mU_weighted[0] # (= Q_{flipped detector coord}*sin(2 theta_pa) + U_{flipped detector coord}*cos(2 theta_pa) )
+
+    # CAUTION: Here the definition of mQU_weighted uses a wrong way of definition, as toast simulation defines that in the wrong way.
+    # (= Q_{flipped detector coord}*cos(2 theta_pa) - U_{flipped detector coord}*sin(2 theta_pa) )
+    mQU_weighted[0][:] = mQ_weighted[0] - mU_weighted[1]
+    # (= Q_{flipped detector coord}*sin(2 theta_pa) + U_{flipped detector coord}*cos(2 theta_pa) )
+    mQU_weighted[1][:] = mQ_weighted[1] + mU_weighted[0]
     #### In field, you should use instead ####
     # mQU_weighted[0][:] = mQ_weighted[0] + mU_weighted[1] # (= Q_{flipped detector coord}*cos(2 theta_pa) + U_{flipped detector coord}*sin(2 theta_pa) )
-    # mQU_weighted[1][:] = -mQ_weighted[1] + mU_weighted[0] # (= -Q_{flipped detector coord}*sin(2 theta_pa) + U_{flipped detector coord}*cos(2 theta_pa) )
-    wQU = PQU.to_weights(tod, signal=demodQ, comps='T', det_weights=det_weights_demod)
-    
+    # mQU_weighted[1][:] = -mQ_weighted[1] + mU_weighted[0] # (= -Q_{flipped
+    # detector coord}*sin(2 theta_pa) + U_{flipped detector coord}*cos(2
+    # theta_pa) )
+    wQU = PQU.to_weights(tod, signal=demodQ, comps='T',
+                         det_weights=det_weights_demod)
+
     # combine mT_weighted and mQU_weighted into mTQU_weighted
     mTQU_weighted = PQU.zeros(super_shape=3)
     mTQU_weighted[0] = mT_weighted
     mTQU_weighted[1] = mQU_weighted[0]
     mTQU_weighted[2] = mQU_weighted[1]
-    
+
     # combine wT and wQU into wTQU
     wTQU = enmap.zeros((3, 3) + PQU.geom.shape, wcs=PQU.geom.wcs)
     wTQU[0][0] = wT
     wTQU[1][1] = wQU
     wTQU[2][2] = wQU
-    
+
     # remove weights
-    mTQU = PQU.remove_weights(signal_map=mTQU_weighted, weights_map=wTQU, comps='TQU')
-    
+    mTQU = PQU.remove_weights(signal_map=mTQU_weighted,
+                              weights_map=wTQU, comps='TQU')
+
     return mTQU, mTQU_weighted, wTQU
