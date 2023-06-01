ef1da60186d85784427d29769df6553c9e17a748
yukisakurai <ysakurai@s.okayama-u.ac.jp>
2023-06-01 05:13:07 +0000

    added main functionality

--- sotodlib/site_pipeline/make_hwp_solutions.py
+++ sotodlib/site_pipeline/make_hwp_solutions.py
@@ -3,75 +3,126 @@ Script to make hwp angle of CalDB.
 This script will run:
 1. automatically if data are acquired as preliminary version.
 2. New versions of the metadata can be created as the analysis evolves.
-The output format is sqlite or HDF5.
 """
+import sys
 import os
 import argparse
 import logging
 import yaml
-
 import datetime as dt
+import scipy 
+
+## Temporary
+sys.path.insert(0, "/mnt/so1/users/ysakurai/sotodlib_dev/make-hwp-solutions/sotodlib/")
+
+import sotodlib
+from sotodlib import core
 from sotodlib.hwp.g3thwp import G3tHWP
 
 from sotodlib.site_pipeline import util
 logger = util.init_logger(__name__, 'make-hwp-solutinos: ')
 
-def get_parser():
 
-    parser = argparse.ArgumentParser(
-        description='Output HWP angle as versioned metadata to load using CalDB')
-    parser.add_argument(
-        '-c', '--config-file', default=None, type=str, required=True,
-        help="Configuration file for running make_hwp_solutions")
-    parser.add_argument(
-        '-d', '--data-dir', action='store', default=None, type=str,
-        help='input data directory, overwrite config data_dir')
+def get_parser(parser=None):
+    if parser is None:
+        parser = argparse.ArgumentParser()
+    parser.add_argument('--context',required=True, help=
+                        "Path to context yaml file to define observation for which to generate hwp angle.")
+    parser.add_argument('--config-file', required=True, help=
+                        "Path to HWP configuration yaml file.")
     parser.add_argument(
         '-o', '--output-dir', action='store', default=None, type=str,
         help='output data directory, overwrite config output_dir')
-    parser.add_argument('--file', default=None, action='append',
-        help="Force processing of a specific file, overriding the "
-             "standard selection process.  The file must be in the "
-             "usual data tree, though.  You may specify either the file "
-             "basename (1234567890.g3) or the full path.")
     parser.add_argument("--verbose", default=2, type=int,
                         help="increase output verbosity. \
                         0: Error, 1: Warning, 2: Info(default), 3: Debug")
 
     return parser
 
-if __name__ == '__main__':
-
-    parser = get_parser()
-    args = parser.parse_args()
-
-    logger.info("Starting make_hwp_solutions")
-
-    # Load output files from update_hwp_angle.py
-    configs = yaml.safe_load(open(args.config_file, "r"))
-    if args.data_dir is None:
-        args.data_dir = configs["data_dir"]
-
-    # Specify output directory
+def main(context=None, config_file=None, output_dir=None, verbose=None):
+    
+    print(context, config_file)
+    configs = yaml.safe_load(open(config_file, "r"))
     if args.output_dir is None:
         args.output_dir = configs["output_dir"]
+    
+    logger.info("Starting make_hwp_solutions")
 
-    if not os.path.exists(args.output_dir):
-        logger.info(f"Making output directory {args.output_dir}")
-        os.mkdir(args.output_dir)
+    # Specify output directory
+    if output_dir is None:
+        output_dir = configs["output_dir"]
+
+    if not os.path.exists(output_dir):
+        logger.info(f"Making output directory {output_dir}")
+        os.mkdir(output_dir)
     
     # Set verbose
-    if args.verbose == 0:
+    if verbose == 0:
         logger.setLevel(logging.ERROR)
-    elif args.verbose == 1:
+    elif verbose == 1:
         logger.setLevel(logging.WARNING)
-    elif args.verbose == 2:
+    elif verbose == 2:
         logger.setLevel(logging.INFO)
-    elif args.verbose == 3:
+    elif verbose == 3:
         logger.setLevel(logging.DEBUG)
+        
+    ctx = core.Context(context)
 
-    if args.file is not None and len(files) != len(args.file):
-        logger.warning(f"Matched only {len(files)} of the {len(args.file)} "
-                       "files provided with --file.")
+    scheme = core.metadata.ManifestScheme()
+    scheme.add_exact_match('obs:obs_id')
+    scheme.add_data_field('dataset')
+    man_db = core.metadata.ManifestDb(scheme=scheme)
+    
+    # Get file + dataset from policy.
+    # policy = util.ArchivePolicy.from_params(config['archive']['policy'])
+    # dest_file, dest_dataset = policy.get_dest(obs_id)
+    # Use 'output_dir' argument for now    
+    man_db_filename = os.path.join(output_dir, 'hwp_angle.sqlite')
+    output_filename = os.path.join(output_dir, 'hwp_angle.h5')
+    
+    obs = ctx.obsdb.get()[:1]
+    for obs_id in obs['obs_id']:
+        print(obs_id)
+        tod = ctx.get_obs(obs_id, no_signal=True)
+        # Wrap result into AxisManager for HDF5 off-load.
+        aman = core.AxisManager(tod.dets, tod.samps)
+        aman.wrap_new('hwp_angle', shape=('samps', ))
+        aman.wrap_new('hwp_angle_eval', shape=('samps', ))
+        
+        #### Angle calculation ####
+        start = int(tod.timestamps[0])
+        end = int(tod.timestamps[-1])
+        g3thwp = G3tHWP(config_file)
+        data = g3thwp.load_data(start, end)
 
-    ########## Put main process here ###########
+        if len(data)==0:
+            logger.info(f"Found no HWP data in {obs_id}")
+            continue
+
+        logger.debug("analyze")
+        solved = g3thwp.analyze(data)
+        print(solved['angle'])
+        aman.hwp_angle = g3thwp.interp_smurf(solved,tod.timestamps)
+        print(aman.hwp_angle)
+        
+        # template subtracted angle --- need to fix bug inside
+        #g3thwp.eval_angle(solved)
+        #aman.hwp_angle_eval = g3thwp.interp_smurf(solved,tod.timestamps)
+        
+        del g3thwp
+        #### End of angle calculation ####
+
+        h5_address = obs_id
+        aman.save(output_filename, h5_address, overwrite=True)
+    
+        # Add an entry to the database
+        man_db.add_entry({'obs:obs_id': obs_id, 'dataset': h5_address}, filename=output_filename)
+
+        # Commit the ManifestDb to file.
+        man_db.to_file(man_db_filename)
+    
+    return
+    
+    
+if __name__ == '__main__':
+    util.main_launcher(main, get_parser)
