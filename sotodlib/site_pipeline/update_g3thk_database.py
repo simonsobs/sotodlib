118c0f3f06230cea34bd5d647dffd3f3c0f76cc7
Katie Harrington <katie.megan.harrington@gmail.com>
2023-06-29 14:36:01 +0000

    bug fixes on updates

--- sotodlib/site_pipeline/update_g3thk_database.py
+++ sotodlib/site_pipeline/update_g3thk_database.py
@@ -11,7 +11,6 @@ import argparse
 import logging
 from sqlalchemy import not_, or_, and_, desc
 
-from sotodlib.site_pipeline.monitor import Monitor
 from sotodlib.io.g3thk_db import G3tHk, HKFiles, logger as default_logger
 
 
@@ -35,23 +34,14 @@ def main(config=None, from_scratch=False, verbosity=2, logger=None):
 
     if from_scratch or HK.session.query(HKFiles).count()==0:
         logger.info("Building Database from Scratch, May take awhile")
-        min_time = dt.datetime.utcfromtimestamp(int(1.6e9))
+        min_time = int(1.6e9)
     else:
         ## start at the last file in the database
         last_file = HK.session.query(HKFiles)
         last_file = last_file.order_by(desc(HKFiles.global_start_time)).first()
         min_time = last_file.global_start_time - 10
 
-    monitor = None
-    if "monitor" in cfgs:
-        logger.info("Will send monitor information to Influx")
-        try:
-            monitor = Monitor.from_configs(cfgs["monitor"]["connect_configs"])
-        except Exception as e:
-            logger.error(f"Monitor connectioned failed {e}")
-            monitor = None
-        
-    HK.add_hkfiles(min_ctime=min_ctime, show_pb=show_pb)
+    HK.add_hkfiles(min_ctime=min_time, show_pb=show_pb)
 
 def get_parser(parser=None):
     if parser is None:
