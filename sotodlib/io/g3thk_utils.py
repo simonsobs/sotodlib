a5a82a052583150bcc36618a7371d32afbfd1479
Katie Harrington <kmharrin@compute21.simonsobs.org>
2023-08-07 06:51:05 +0000

    actually make nice arrays

--- sotodlib/io/g3thk_utils.py
+++ sotodlib/io/g3thk_utils.py
@@ -34,7 +34,9 @@ def pysmurf_monitor_control_list(agent, start=None, stop=None, HK=None):
         if HK is None:
             raise ValueError("need database to search if agent is instance id")
         agent_list = HK.get_db_agents(agent, start, stop)
-        return np.unique([pysmurf_monitor_control_list(agent) for agent in agent_list])
+        return np.unique(
+                np.concatenate[pysmurf_monitor_control_list(agent) for agent in agent_list])
+        )
     stream_ids = []
     for field in agent.fields:
         if not "_meta" in field.field:
