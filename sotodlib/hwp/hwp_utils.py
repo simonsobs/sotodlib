33168dfcc95f275cdc83f0bdce82f676d497dad3
tterasaki <tomoki.terasaki@phys.s.u-tokyo.ac.jp>
2023-08-08 06:19:02 +0000

    add PSD plotting function

--- sotodlib/hwp/hwp_utils.py
+++ sotodlib/hwp/hwp_utils.py
@@ -42,7 +42,48 @@ def plot_hwpss_fit_status(tod, hwpss_stats, plot_dets=None, plot_num_dets=3,
         plt.savefig(os.path.join(save_path, save_ts+'_'+save_name))
     return fig, ax
 
+def plot_preprocess_PSDs(aman, det=None, psd_before=None, psd_after=None, psd_dsT=None, psd_demodQ=None, psd_demodU=None,
+                        take_square_root=True, amplitude_unit='pA',
+                        save_plot=False, save_path='./', save_name='preprocess_PSDs.png'):
+    if psd_before is None: psd_before = aman.psd
+    if psd_after is None: psd_after = aman.psd_hwpss_remove
+    if psd_dsT is None: psd_dsT = aman.psd_dsT
+    if psd_demodQ is None: psd_demodQ = aman.psd_demodQ
+    if psd_demodU is None: psd_demodU = aman.psd_demodU
+    
+    psd_dict = {
+    'before': psd_before,
+    'after': psd_after,
+    'dsT': psd_dsT,
+    'demodQ': psd_demodQ,
+    'demodU': psd_demodU,
+           }
 
+    if take_square_root:
+        power = 0.5
+        ylabel = f'PSD [{amplitude_unit}/sqrt(Hz)]'
+    else:
+        power = 1
+        ylabel = f'PSD [{amplitude_unit}^2/Hz]'
+
+    if det is None:
+        det = aman.dets.vals[0]
+        
+    det_idx = np.where(aman.dets.vals == det)[0][0]
+    fig, ax = plt.subplots(1, 1, figsize=(7, 5))
+    for i, (psd_name, psd) in enumerate(psd_dict.items()):
+        ax.loglog(psd.freqs, psd.Pxx[det_idx]**power, label=psd_name, alpha=0.3)
+        if i == 0:
+            ax.set_ylim(np.nanmin(psd.Pxx[det_idx]**power), np.nanmax(psd.Pxx[det_idx]**power))
+
+    ax.legend()
+    ax.set_xlabel('freq [Hz]')
+    ax.set_ylabel(ylabel)
+    ax.set_title(f'Obs_timestamp:{aman.timestamps[0]:.0f}\ndet:{det}')
+    fig.tight_layout()
     
+    save_ts = str(int(time.time()))
+    if save_plot:
+        plt.savefig(os.path.join(save_path, save_ts+'_'+save_name))
     
-    
+    return fig, ax
