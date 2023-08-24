e69079a1d980be8bfd1e6b73c65122460c824b21
Katie Harrington <kharrington@anl.gov>
2023-08-24 19:24:10 +0000

    make book changes to remove source and fix re-try logic

--- sotodlib/site_pipeline/make_book.py
+++ sotodlib/site_pipeline/make_book.py
@@ -2,10 +2,10 @@ from typing import Optional
 import traceback
 import argparse
 
-from ..io.imprinter import Imprinter
+from sotodlib.io.imprinter import Imprinter
 
 
-def main(config: str, output_root: str, source: Optional[str], logger=None):
+def main(config: str, output_root: str, logger=None):
     """Make books based on imprinter db
     
     Parameters
@@ -14,17 +14,14 @@ def main(config: str, output_root: str, source: Optional[str], logger=None):
         path to imprinter configuration file
     output_root : str
         root path of the books 
-    source: str, optional
-        data source to use, e.g., sat1, latrt, tsat. If None, use all sources
     """
     imprinter = Imprinter(config, db_args={'connect_args': {'check_same_thread': False}})
     # get unbound books
     unbound_books = imprinter.get_unbound_books()
-    failed_books = imprinter.get_failed_books()
-    if source is not None:
-        unbound_books = [book for book in unbound_books if book.tel_tube == source]
-        failed_books = [book for book in failed_books if book.tel_tube == source]
-    print(f"Found {len(unbound_books)} unbound books and {len(failed_books)} failed books")
+    already_failed_books = imprinter.get_failed_books()
+    
+    print(f"Found {len(unbound_books)} unbound books and "
+          f"{len(already_failed_books)} failed books")
     for book in unbound_books:
         print(f"Binding book {book.bid}")
         try:
@@ -33,8 +30,12 @@ def main(config: str, output_root: str, source: Optional[str], logger=None):
             print(f"Error binding book {book.bid}: {e}")
             print(traceback.format_exc())
 
-    print("Retrying previously failed books") 
+    print("Retrying failed books") 
+    failed_books = imprinter.get_failed_books()
     for book in failed_books:
+        if book in already_failed_books:
+            print(f"Book {book.bid} has already failed twice, not re-trying")
+            continue
         print(f"Binding book {book.bid}")
         try:
             imprinter.bind_book(book, output_root=output_root)
@@ -50,7 +51,6 @@ def get_parser(parser=None):
         parser = argparse.ArgumentParser()
     parser.add_argument('config', type=str, help="Path to imprinter configuration file")
     parser.add_argument('output_root', type=str, help="Root path of the books")
-    parser.add_argument('--source', type=str, help="Data source to use, e.g., sat1, latrt, tsat. If None, use all sources")
     return parser
 
 
