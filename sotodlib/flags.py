3d01d4fe18a9f310feaf6b2caf80c1b92ee1987a
Atsuto Takeuchi <atsuto.takeuchi@phys.s.u-tokyo.ac.jp>
2023-10-28 04:18:43 +0000

    script modofication based on comments

--- sotodlib/flags.py
+++ sotodlib/flags.py
@@ -1,5 +1,6 @@
 import numpy as np
 import scipy.stats as stats
+from scipy.signal import find_peaks
 
 ## "temporary" fix to deal with scipy>1.8 changing the sparse setup
 try:
@@ -15,16 +16,13 @@ from .tod_ops import fourier_filter
 
 
 def get_turnaround_flags_by_dazdt(
-    tod, qlim=5.,az=None, merge=False, overwrite=False, name="turnarounds_by_dazdt"
+    aman, t_buffer=1.8 ,az=None, merge=True, overwrite=False, name="turnarounds_dazdt"
 ):
 
     """
     Args:
-        tod: AxisManager object
-        qlimp: percentage threshold applied to positive daz/dt
-        qlimn: percentage threshold applied to negative daz/dt
-        az: The azimuth signal to look for turnarounds. If None it defaults to
-            tod.boresight.az
+        aman: AxisManager object
+        t_buffer: buffer length to avoid the flactuation of daz [sec]
         merge: If true, merge into tod.flags
         overwrite: If true and merge is True, write over existing flag
         name: name of flag when merged into tod.flags
@@ -32,33 +30,205 @@ def get_turnaround_flags_by_dazdt(
     Returns:
         flag: Ranges object of turn-arounds
     """
-
-    daz = np.copy(np.diff(tod.boresight.az))
+    
+    # Get daz and time 
+    if az == None : 
+        az = np.copy(aman.boresight.az)
+    daz = np.diff(az)
     daz = np.append(daz,daz[-1])
-    # rough selection of stationary part
-    lo,hi = np.min(daz)*(100-5)*0.01,np.max(daz)*(100-5)*0.01
+    time = aman.timestamps - aman.timestamps[0]
 
-    mean_hi = np.mean(daz[daz>hi])
-    mean_lo = np.mean(daz[daz<lo])
+    # derive approx daz
+    approx_daz = np.mean(daz[daz>np.percentile(daz, 95)])
 
-    hi = mean_hi*(100.-qlim)*0.01
-    lo = mean_lo*(100.-qlim)*0.01
-    # to avoid oscillation of daz
-    #lo = mean_lo+2.*(mean_lo-np.min(daz[daz<lo]))
+    # derive approx aprox daz/dt and rough number of samples in one_scan period 
+    dt = np.mean(np.diff(aman.timestamps))
+    approx_dazdt = approx_daz / dt
+    approx_samps_onescan = int(np.ptp(az) / approx_dazdt / dt)
 
-    m = np.logical_and(daz > lo, daz < hi)
+    # Make a step-function like matched filter
+    kernel_size = 400
+    kernel = np.zeros(kernel_size)
+    # normalize with approx_daz so that peaks height be ~1
+    kernel[:kernel_size//2] = 1/approx_daz
+    kernel[kernel_size//2:] = - 1/approx_daz
+    kernel /= kernel_size
 
-    flag = Ranges.from_bitmask(m)
+    # convolve signal with the kernel
+    matched = np.convolve(np.diff(aman.boresight.az), kernel, mode='same')
+    matched[:kernel_size] = 0
+    matched[-kernel_size:] = 0 
+
+    # find peaks in matched daz, height=0.1, distance=approx_samps_onescan//3 would be robust enough
+    peaks, properties = find_peaks(matched, height=0.1, distance=approx_samps_onescan//3)
+
+    # turn around flag 
+    turnaround_mask = np.zeros_like(aman.timestamps,dtype=bool)
+
+    ### flagging
+    
+    mean_length = np.mean(np.diff(peaks))
+    prev = 0    
+    nbuffer = int(t_buffer/dt) # A finite buffer is needed to avoid the fluctuation of daz/dt
+
+    # Check the beggining part 
+    first_is_positive = False
+    if peaks[0] < mean_length//2 : 
+        first_is_positive = True
+
+    # search for edges of square waves looking at the peaks
+    for p in peaks : 
+        start = prev
+        end = p
+        length = p-prev
+        middle = start + length//2
+        prev = p
+
+        turnaround_mask[middle-nbuffer:middle+nbuffer] = True
+        turnaround_mask[prev-nbuffer:prev+nbuffer] = True
+        
+    # we don't know the end of the last square wave, so use the mean of wave width
+    start = prev
+    end = prev + mean_length
+    middle = start + mean_length//2
+    
+    turnaround_mask[int(middle-nbuffer):int(middle+nbuffer)] = True
+    turnaround_mask[int(prev-nbuffer):int(prev+nbuffer)] = True    
+    
+    # treatment of the begining
+    if first_is_positive : 
+        turnaround_mask[:peaks[0]+nbuffer] = True
+
+    else : 
+        turnaround_mask[:int(peaks[0]-mean_length//2)] = True
+        
+    ta_flag = Ranges.from_bitmask(turnaround_mask)
 
     if merge:
-        if name in tod.flags and not overwrite:
-            raise ValueError("Flag name {} already exists in tod.flags".format(name))
-        elif name in tod.flags:
-            tod.flags[name] = flag
+        if name in aman.flags and not overwrite:
+            raise ValueError("Flag name {} already exists in aman.flags".format(name))
+        elif name in aman.flags:
+            aman.flags[name] = ta_flag
         else:
-            tod.flags.wrap(name, flag)
-    return flag
+            aman.flags.wrap(name, ta_flag)
+    return ta_flag
+    
 
+def get_leftright_flags_by_dazdt(
+    aman, t_buffer=1.8, w_turnaround=True, az=None, merge=True, overwrite=False, name=["left", "right"]
+):
+
+    """
+    Args:
+        aman: AxisManager object
+        t_buffer: buffer length to avoid the flactuation of daz [sec]
+        merge: If true, merge into tod.flags
+        overwrite: If true and merge is True, write over existing flag
+        name: list of names of flags when merged into tod.flags
+
+    Returns:
+        flag: Ranges object of left/right scan
+    """
+    
+    # Get daz and time 
+    if az == None : 
+        az = np.copy(aman.boresight.az)
+    daz = np.diff(az)
+    daz = np.append(daz,daz[-1])
+    time = aman.timestamps - aman.timestamps[0]
+
+    # derive approx daz
+    approx_daz = np.mean(daz[daz>np.percentile(daz, 95)])
+
+    # derive approx aprox daz/dt and rough number of samples in one_scan period 
+    dt = np.mean(np.diff(aman.timestamps))
+    approx_dazdt = approx_daz / dt
+    approx_samps_onescan = int(np.ptp(az) / approx_dazdt / dt)
+
+    # Make a step-function like matched filter
+    kernel_size = 400
+    kernel = np.zeros(kernel_size)
+    # normalize with approx_daz so that peaks height be ~1
+    kernel[:kernel_size//2] = 1/approx_daz
+    kernel[kernel_size//2:] = - 1/approx_daz
+    kernel /= kernel_size
+
+    # convolve signal with the kernel
+    matched = np.convolve(np.diff(aman.boresight.az), kernel, mode='same')
+    matched[:kernel_size] = 0
+    matched[-kernel_size:] = 0 
+
+    # find peaks in matched daz, height=0.1, distance=approx_samps_onescan//3 would be robust enough
+    peaks, properties = find_peaks(matched, height=0.1, distance=approx_samps_onescan//3)
+
+    # flag that involves turnaround part 
+    left_ent = (daz > 0.)
+    right_ent = (daz <= 0.)
+    
+    # flag that does not involve turnaround part
+    left = np.zeros_like(aman.timestamps,dtype=bool)
+    right = np.zeros_like(aman.timestamps,dtype=bool)
+
+    ### flagging
+    
+    mean_length = np.mean(np.diff(peaks))
+    prev = 0    
+    nbuffer = int(t_buffer/dt) # A finite buffer is needed to avoid the fluctuation of daz/dt
+
+    # Check the beggining part 
+    first_is_positive = False
+    if peaks[0] < mean_length//2 : 
+        first_is_positive = True
+
+    # search for edges of square waves looking at the peaks
+    for p in peaks : 
+        start = prev
+        end = p
+        length = p-prev
+        middle = start + length//2
+        prev = p
+
+        left[start+nbuffer:middle-nbuffer] = True
+        right[middle+nbuffer:end-nbuffer] = True
+        
+    # we don't know the end of the last square wave, so use the mean of wave width
+    start = prev
+    end = prev + mean_length
+    middle = start + mean_length//2
+    
+    left[int(start+nbuffer):int(middle-nbuffer)] = True
+    right[int(middle+nbuffer):int(end-nbuffer)] = True
+    
+    # treatment of the begining
+    if first_is_positive : 
+        left[:peaks[0]+nbuffer] = False
+        right[:peaks[0]+nbuffer] = False
+    else : 
+        right[int(peaks[0]-mean_length//2):peaks[0]-nbuffer] = True
+        left[:peaks[0]+nbuffer] = False
+        
+    if w_turnaround : 
+        left_flag = Ranges.from_bitmask(left_ent)
+        right_flag = Ranges.from_bitmask(right_ent)        
+    else : 
+        left_flag = Ranges.from_bitmask(left)
+        right_flag = Ranges.from_bitmask(right)
+        
+    if merge:
+        if (name[0] in aman.flags or name[1] in aman.flags) and not overwrite:
+            raise ValueError("Flag name {} already exists in aman.flags".format(name))
+        else : 
+            if name[0] in aman.flags:
+                aman.flags[name[0]] = left_flag
+            else:            
+                aman.flags.wrap(name[0], left_flag)
+            if name[1] in aman.flags:
+                aman.flags[name[1]] = right_flag
+            else:
+                aman.flags.wrap(name[1], right_flag)
+
+    return left_flag, right_flag
+        
 
 
 def get_turnaround_flags(
--- sotodlib/tod_ops/sub_polyf.py
+++ sotodlib/tod_ops/sub_polyf.py
@@ -1,86 +1,9 @@
 import numpy as np 
-import matplotlib.pyplot as plt 
 
 from sotodlib import flags
 
 
 
-def get_turnaround_flags_by_dazdt(
-    tod, qlim=5.,az=None
-):
-
-    """
-    Args:
-        tod: AxisManager object
-        qlimp: percentage threshold applied to positive daz/dt
-        qlimn: percentage threshold applied to negative daz/dt
-        az: The azimuth signal to look for turnarounds. If None it defaults to
-            tod.boresight.az
-
-    Returns:
-        flag: boolean array of turn-arounds
-    """
-    
-    daz = np.copy(np.diff(tod.boresight.az))
-    daz = np.append(daz,daz[-1])
-    # rough selection of stationary part
-    lo,hi = np.min(daz)*(100-5)*0.01,np.max(daz)*(100-5)*0.01
-    
-    mean_hi = np.mean(daz[daz>hi])
-    mean_lo = np.mean(daz[daz<lo])
-    
-    hi = mean_hi*(100.-qlim)*0.01
-    lo = mean_lo*(100.-qlim)*0.01
-    # to avoid oscillation of daz
-    #lo = mean_lo+2.*(mean_lo-np.min(daz[daz<lo]))
-    
-    
-    m = np.logical_and(daz > lo, daz < hi)
-    
-    return m
-
-
-# Copy of flags.get_turnaround_flags, editted to return boolean numpy array
-def get_turnaround_flags( # Copy of flags.get_turnaround_flags, editted to return boolean numpy array
-    tod, qlim=1, az=None
-):
-    """Flag the scan turnaround times.
-
-    Args:
-        tod: AxisManager object
-        qlim: percentile used to find turnaround
-        az: The azimuth signal to look for turnarounds. If None it defaults to
-            tod.boresight.az
-
-    Returns:
-        flag: Ranges object of turn-arounds
-    """
-    
-    if az is None:
-        az = tod.boresight.az
-    lo, hi = np.percentile(az, [qlim, 100 - qlim])
-    m = np.logical_or(az < lo, az > hi)
-
-    return m
-
-
-def get_ta_list(ta_flag):
-    
-    # starts and ends of st. parts
-    num1,num2 = 5000,7000
-    ones = np.where(ta_flag==False)[0]
-    diff = np.diff(ones)
-    
-    starts = np.insert(ones[np.where(diff != 1)[0]], 0, ones[0])
-    ends = np.append(ones[np.where(diff != 1)[0] - 1], ones[-1])
-    indices = list(zip(starts, ends))
-    indices = [(start, end) for start, end in indices if (end - start + 1 >= num1) and (end - start + 1 <= num2)]
-
-    return indices 
-
-
-
-# subscan ranges are obtained based on the sign of az speed 
 def get_leftright_array(
     tod, az=None
 ):
@@ -101,37 +24,50 @@ def get_leftright_array(
     ranges = np.split(negative_indices, np.where(np.diff(negative_indices) != 1)[0]+1)
     index_ranges_n = [(rng[0], rng[-1]) for rng in ranges]
     
+
     index_ranges_p.extend(index_ranges_n)
     
     return index_ranges_p
- 
-    
-def subscan_polyfilter_onlystat(aman, degree, signal='signal', default_flag=False, wrap_flag=True) : 
 
+
+
+def get_range_list(ta_flag,_min=0):
+    
+    # starts and ends of non-turn-around parts
+    ones = np.where(ta_flag==False)[0]
+    diff = np.diff(ones)
+    
+    starts = np.insert(ones[np.where(diff != 1)[0]], 0, ones[0])
+    ends = np.append(ones[np.where(diff != 1)[0] - 1], ones[-1])
+    indices = list(zip(starts, ends))
+    indices = [(start, end) for start, end in indices if (end - start + 1 >= _min)]
+
+    return indices
+
+
+def subscan_polyfilter_tamasked(aman, degree, signal='signal', default_flag=False, wrap_flag=True) : 
+    
     """
-    subscan polyfilter for only stationary part.
+    subscan polyfilter for only non turn-around part.
     
     Args:
         aman: AxisManager object
         degree: Degree of polynomial
-        default_flag: If True, turnaround part is looked for using 'get_turnaround_flags()'
-                        Otherwise, turnaround part is looked for by looking at daz/dt (get_turnaround_flags_by_dazdt())
-        wrap_flag: This function can make the jumps in tod at each turnaround part.
-                        If wrap_flag is True, a mask that veto turnaround part will be wrapped in aman. 
+        default_flag: If True, turnaround part is looked for using original method.
+                        Otherwise, turnaround part is looked for by looking at daz/dt
 
     Returns:
         -
     """
-   
-    if default_flag : ta_flag = get_turnaround_flags(aman,qlim=5)
-    else : ta_flag = get_turnaround_flags_by_dazdt(aman,qlim=5)
-    ta_list = get_ta_list(ta_flag)
+    
+    if default_flag : ta_flag = flags.get_turnaround_flags(aman,merge=False)
+    else : ta_flag = flags.get_turnaround_flags_by_dazdt(aman, merge=False)
+    ta_list = get_range_list(ta_flag.mask())
     
     time = aman.timestamps
     time = time - time[0]
-    new_mask = np.zeros_like(time,dtype=bool)
     
-    for i_det in range(aman[signal]shape[0]) : 
+    for i_det in range(aman[signal].shape[0]) : 
         for ta in ta_list :
             start = ta[0]
             end = ta[1]
@@ -140,21 +76,17 @@ def subscan_polyfilter_onlystat(aman, degree, signal='signal', default_flag=Fals
             
             pars = np.polyfit(time[start:end+1]-t_mean,aman[signal][i_det,start:end+1],deg=degree)
             f = np.poly1d(pars)
-            aman[signal][i_det,start:end+1] = aman[signal][i_det,start:end+1] - f(time[start:end+1]-t_mean)
-            new_mask[start:end+1] = True
-        ]
-    if wrap_flag : 
-        print("non-turn-around part is wrapped.")
-        aman.wrap('stationary_part', new_mask, [(0, 'samps')])
+            
+            aman.signal[i_det,start:end+1] = aman[signal][i_det,start:end+1] - f(time[start:end+1]-t_mean)
 
-    
+        
 
 def subscan_polyfilter(aman, degree, signal='signal') :
-
+    
     """
     subscan polyfilter
-    In this function, subscan is defined looking at the sign of scan speed.
-
+    In this function, subscan is defined by the sign of scan speed. 
+    
     Args:
         aman: AxisManager object
         degree: Degree of polynomial
@@ -162,29 +94,29 @@ def subscan_polyfilter(aman, degree, signal='signal') :
 
     Returns:
         -
-    """
-
-    ### Get left scan ranges and right scan ranges
-    subscans = get_leftright_array(aman)
-
+    """   
+    
     time = aman.timestamps
     time = time - time[0]
-
-    for i_det in range(aman[signal].shape[0]) :
-        for subscan in subscans :
+    
+    ranges_list = flags.get_leftright_array(aman)
+    
+    for i_det in range(aman[signal].shape[0]) :  
+        for subscan in ranges_list :
             start = subscan[0]
             end = subscan[1]
 
             ### if the number of points is small, just subtract the mean
-            if end - start < degree :
+            if end - start < degree : 
                 aman[signal][i_det,start:end+1] = aman[signal][i_det,start:end+1] - np.mean(aman[signal][i_det,start:end+1])
                 continue
 
-            ### mean of time is subtracted to make fit easier,
+            ### mean of time is subtracted to make fit easier, 
             ### then polynomial fit is applied.
             t_mean = np.mean(time[start:end+1])
             pars = np.polyfit(time[start:end+1]-t_mean,aman[signal][i_det,start:end+1],deg=degree)
             f = np.poly1d(pars)
-
+        
             aman[signal][i_det,start:end+1] = aman[signal][i_det,start:end+1] - f(time[start:end+1]-t_mean)
 
+    
