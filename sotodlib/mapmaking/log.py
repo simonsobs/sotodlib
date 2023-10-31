b5d2e919fb38258f03c33798262a2e5ddb641050
tanaybhandarkar <tanaybhandarkar@gmail.com>
2023-10-31 13:26:10 +0000

    Adding a banner w/ source information in bench and log

--- sotodlib/mapmaking/log.py
+++ sotodlib/mapmaking/log.py
@@ -1,3 +1,7 @@
+# This code has been ported over from enlib: 
+# https://github.com/amaurea/enlib
+# Ported over on 10/31/2023
+
 """This module provids a simple logging/output interface."""
 import time
 from logging import *
