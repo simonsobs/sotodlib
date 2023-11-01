2b4ecd0f750905294df978431d4a6834497e7a6c
Theodore Kisner <tskisner@users.noreply.github.com>
2023-11-01 11:28:56 -0700

    Reorganize toast workflow tools (#583)
    * Refactor the monolithic toast workflows into high-level functions
    that do one or more closely related operations:
    
    * Each each high-level operation has a "setup" function which
      prepares the given task for configuration.
    
    * A separate "setup_job" function parses config files, user options,
      commandline options, etc and configures all the low-level operators.
    
    * These workflow tools can be used both interactively in a notebook
      or in a main() script and can be mixed with other operators and
      code.
    
    * Documentation updated to reflect these changes.
    
    * Move global realization index into simulted observing.  Add verbose option to setup_job.
    
    * Get HWP status from the data rather than the sim_ground operator.
    
    * Move timing and mem count into a workflow decorator
    
    * Rename data characterization workflow file.
    
    * Move script entry points into a dedicated submodule
    
    * Adjust entrypoints in setup.py
    
    * Remove the legacy top-level workflows directory.  Everything is an entrypoint.
    
    * Fix stale entry
    
    * Do not run unimplemented function stub.
    
    * Raise an exception if a toast workflow attempts to run the ML mapmaker without the group size set to one.

---
+++ sotodlib/toast/workflows/proc_noise_est.py
@@ -0,0 +1,87 @@
+# Copyright (c) 2023-2023 Simons Observatory.
+# Full license can be found in the top level "LICENSE" file.
+"""Noise estimation for mapmaking.
+"""
+
+import numpy as np
+from astropy import units as u
+import toast
+import toast.ops
+
+from .. import ops as so_ops
+from .job import workflow_timer
+
+
+def setup_noise_estimation(operators):
+    """Add commandline args and operators for estimating detector noise.
+
+    Args:
+        operators (list):  The list of operators to extend.
+
+    Returns:
+        None
+
+    """
+    operators.append(
+        toast.ops.NoiseEstim(
+            name="noise_estim",
+            out_model="noise_estim",
+            lagmax=1024,
+            nbin_psd=64,
+            nsum=1,
+            naverage=64,
+            enabled=False,
+        )
+    )
+    operators.append(
+        toast.ops.FitNoiseModel(
+            name="noise_estim_fit",
+        )
+    )
+
+
+@workflow_timer
+def noise_estimation(job, otherargs, runargs, data):
+    """Run noise estimation and create a best-fit 1/f model.
+
+    Args:
+        job (namespace):  The configured operators and templates for this job.
+        otherargs (namespace):  Other commandline arguments.
+        runargs (namespace):  Job related runtime parameters.
+        data (Data):  The data container.
+
+    Returns:
+        None
+
+    """
+    log = toast.utils.Logger.get()
+    timer = toast.timing.Timer()
+    timer.start()
+
+    # Configured operators for this job
+    job_ops = job.operators
+
+    # If any operators are disabled, just return
+    all_enabled = job_ops.noise_estim.enabled and job_ops.noise_estim_fit.enabled
+    if not all_enabled:
+        log.info_rank("Noise estimation disabled", comm=data.comm.comm_world)
+        return
+
+    # Estimate noise.
+    log.info_rank(
+        "  Building noise estimate...", comm=data.comm.comm_world
+    )
+    job_ops.noise_estim.apply(data)
+    log.info_rank(
+        "  Finished noise estimate in", comm=data.comm.comm_world, timer=timer
+    )
+
+    # Create a fit to this noise model
+    job_ops.noise_estim_fit.noise_model = job_ops.noise_estim.out_model
+    log.info_rank(
+        "  Running fit to noise estimate...", comm=data.comm.comm_world
+    )
+    job_ops.noise_estim_fit.apply(data)
+    log.info_rank(
+        "  Fit 1/f noise model in", comm=data.comm.comm_world, timer=timer
+    )
