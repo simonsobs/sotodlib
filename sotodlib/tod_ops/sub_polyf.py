0ae380e57d4cea7ee8ab4ff13d261ae92d365937
Tomoki Terasaki <tterasaki@compute11.simonsobs.org>
2023-11-07 09:42:36 +0000

    improved readability in sub_polyf and added functions in preprocess

--- sotodlib/tod_ops/sub_polyf.py
+++ sotodlib/tod_ops/sub_polyf.py
@@ -1,114 +1,86 @@
 import numpy as np 
-
 from sotodlib import flags
 
-
-def subscan_polyfilter(aman, degree, signal='signal', subscan_def=None, mask=None) : 
-    
+def subscan_polyfilter(aman, degree, signal='signal', exclude_turnarounds=True, mask=None):
     """
+    Apply polynomial filtering to subscan segments in a data array.
+
+    This function applies polynomial filtering to subscan segments within aman[`signal`] for each detector.
+    Subscan segments are defined based on the presence of flags such as 'left_scan' and 'right_scan'. Polynomial filtering
+    is used to remove low-degree polynomial trends within each subscan segment.
+
+    Parameters:
+    - aman (object): AxisManager object
+    - degree (int): The degree of the polynomial to be removed.
+    - signal (str, optional): The name of the signal in 'aman' to which polynomial filtering is applied. Default is 'signal'.
+    - exclude_turnarounds (bool, optional): If True, turnarounds are excluded from subscan identification. Default is True.
+    - mask (str or RangesMatrix, optional): A mask used to select specific data points for filtering. Default is None.
+        If None, no mask is applied. If the mask is given in str, aman.flags[`mask`] is used as mask. Arbitrary mask can be 
+        specified in the style of RangesMatrix.
+    """
+    if exclude_turnarounds:
+        if ("left_scan" not in aman.flags) or ("turnarounds" not in aman.flags):
+            raise ValueError('Flag turnarounds,left_scan, and right_scan by `sotodlib.flags.get_turnaround_flags`')
+        valid_scan = np.logical_and(np.logical_or(aman.flags["left_scan"].mask(), aman.flags["right_scan"].mask()),
+                                    ~aman.flags["turnarounds"].mask())
+        subscan_indices = _get_subscan_range_index(valid_scan)
+    else:
+        if ("left_scan" not in aman.flags):
+            raise ValueError('Flag left_scan and right_scan by `sotodlib.flags.get_turnaround_flags`')
+        subscan_indices_l = _get_subscan_range_index(aman.flags["left_scan"].mask())
+        subscan_indices_r = _get_subscan_range_index(aman.flags["right_scan"].mask())
+        subscan_indices = np.vstack([subscan_idx_l, subscan_idx_r])
+        subscan_indices= subscan_indices[np.argsort(subscan_indices[:, 0])]
     
-    Args:
-        aman: AxisManager object
-        degree: Degree of polynomial
-        signal: the name of the signal to filtered. defaults is 'signal'.
-        subscan_def: the name of Ranges object with which subscan ranges 
-                     are defined. (False ranges will be filtered)
-                     if None, left/right scan flag will be used.
-        mask: Ranges/RangesMatrix object which will be 
-                    rejected from polyfiltering ranges.
+    if mask is None:
+        mask_array = np.zeros(aman.samps.count, dtype=bool)
+    elif type(mask) is str:
+        mask_array = aman.flags[mask].mask()
+    else:
+        mask_array = mask.mask()
+    is_matrix = len(mask_array.shape) > 1
+    
+    t = aman.timestamps - aman.timestamps[0]
+    for i_det in range(aman.dets.count):
+        if is_matrix:
+            each_det_mask = mask_array[i_det]
+        else:
+            each_det_mask = mask_array
+
+        for start, end in subscan_indices:
+            if np.count_nonzero(~each_det_mask[start:end+1]) < degree:
+                # If degree of freedom is lower than zero, just subtract mean
+                aman[signal][i_det, start:end+1] -= np.mean(aman[signal][i_det, start:end+1])
+            else:
+                t_mean = np.mean(t[start:end+1])
+                pars = np.ma.polyfit(
+                        np.ma.array(t[start:end+1]-t_mean, mask=each_det_mask[start:end+1]),
+                        np.ma.array(aman[signal][i_det,start:end+1], mask=each_det_mask[start:end+1]),
+                        deg=degree)
+
+                aman[signal][i_det,start:end+1] -= np.polyval(pars, t[start:end+1]-t_mean)
+                
+                
+def _get_subscan_range_index(scan_flag,_min=0):
+    """
+    Get the indices of subscans in a binary flag array.
+    This function identifies subscan ranges within a binary flag array, where subscans are defined as consecutive
+    sequences of 'True' values in the input 'scan_flag' array.
+
+    Parameters:
+    - scan_flag (numpy.ndarray): A 1-dimensional binary flag array indicating the presence of subscans.
+    - _min (int, optional): The minimum length of a subscan range to consider. Default is 0.
+
     Returns:
-        -
+    - numpy.ndarray: A 2-column array containing the start and end indices of subscan ranges in 'scan_flag' where
+      the length of each subscan is greater than or equal to '_min'.
     """
-    
-    # Case with right/left scan flag 
-    if subscan_def == None : 
-        # Check if aman has left/right_flag 
-        if  ("left_scan" in aman.flags) and ("right_scan" in aman.flags) :
-            pass
-        else :
-            flags.get_turnaround_flags(aman, merge=False)
-            
-        ta_list_l = _get_subscan_range_index(aman.flags["left_scan"].mask())
-        ta_list_r = _get_subscan_range_index(aman.flags["right_scan"].mask())
-        
-        # First part can be overlapped. Delete overlapped part.
-        if ta_list_l[0][0] == 0 and ta_list_r[0][0] == 0 :
-            if ta_list_l[0][1] > ta_list_r[0][1] : 
-                del ta_list_r[0]
-            else : 
-                del ta_list_l[0]
-        
-        ta_list_l.extend(ta_list_r)
-        ta_list = ta_list_l
-            
-    # Case with given definition 
-    else : 
-        ta_flag = subscan_def 
-        ta_list = _get_subscan_range_index(ta_flag.mask())
-
-    time = aman.timestamps
-    time = time - time[0]
-    
-    # Get boolean array. 
-    is_matrix = False
-    if not mask is None : 
-        masked_flag = mask.mask()
-        if len(masked_flag.shape) > 1 : is_matrix = True
-        else : masked_flag = masked_flag.reshape([1,len(masked_flag)])
-        
-        assert aman.signal.shape == masked_flag.shape, "Mask shape is not consistent with signal shape."
-        
-    else : 
-        masked_flag = np.zeros([1,len(time)],dtype=bool)
-    
-    for i_det in range(aman[signal].shape[0]) : 
-        
-        # In case Ranges object is given, re-shape and read data of index 0.
-        # If RangesMatrix object is given, read data of index i_det
-        if is_matrix == False : 
-            read = 0
-        else : 
-            read = i_det
-        
-        for ta in ta_list :
-            start = ta[0]
-            end = ta[1]
-            
-            if np.any(masked_flag[read, start:end+1]) : 
-                if np.all(masked_flag[read, start:end+1] == True): continue
-                    
-                t_mean = np.mean(time[start:end+1])
-                pars,_,_,_,_ = np.ma.polyfit(
-                    np.ma.array(time[start:end+1]-t_mean, mask=masked_flag[read, start:end+1]),
-                    np.ma.array(aman[signal][i_det,start:end+1], mask=masked_flag[read, start:end+1]),
-                    deg=degree,
-                    full=True,
-                )         
-                
-                aman.signal[i_det,start:end+1] = aman.signal[i_det,start:end+1] - np.polyval(pars, time[start:end+1]-t_mean)
-                
-            else : 
-                t_mean = np.mean(time[start:end+1])
-                pars = np.polyfit(time[start:end+1]-t_mean,aman[signal][i_det,start:end+1],deg=degree)
-                f = np.poly1d(pars)        
-                
-                aman.signal[i_det,start:end+1] = aman[signal][i_det,start:end+1] - f(time[start:end+1]-t_mean) 
-                
-
-
-def _get_subscan_range_index(ta_flag,_min=0):
-    
-    # starts and ends of st. parts
-    ones = np.where(ta_flag==False)[0]
+    ones = np.where(scan_flag)[0]
     diff = np.diff(ones)
     
     starts = np.insert(ones[np.where(diff != 1)[0]+1], 0, ones[0])
     ends = np.append(ones[np.where(diff != 1)[0] ], ones[-1])
     indices = list(zip(starts, ends))
-    #indices = [(start, end) for start, end in indices if (end - start + 1 >= _min)]
-    indices = [(start, end) for start, end in indices if (end - start + 1 >= _min)]
-
-    return indices 
+    indices = np.array([(start, end) for start, end in indices if (end - start + 1 >= _min)])
+    return indices
  
-    
-
