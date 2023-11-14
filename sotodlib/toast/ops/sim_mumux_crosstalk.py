04398dca1dcf2ab52010ecfa73140a623fc7a956
keskitalo <reijo.keskitalo@gmail.com>
2023-11-13 19:05:22 -0800

    Implementing SQUID phase calculation

--- sotodlib/toast/ops/sim_mumux_crosstalk.py
+++ sotodlib/toast/ops/sim_mumux_crosstalk.py
@@ -22,6 +22,7 @@ from scipy.interpolate import RectBivariateSpline, splrep, splev
 from toast.timing import function_timer
 
 from toast import qarray as qa
+import toast.rng
 
 from toast.data import Data
 
@@ -35,6 +36,72 @@ from toast.observation import default_values as defaults
 
 
 XAXIS, YAXIS, ZAXIS = np.eye(3)
+
+# https://github.com/simonsobs/bolocalc-so-model/blob/master/V3r7/V3r7_Baseline
+# Total optical power in pW
+P_OPT = {
+    "SAT_f030" : 0.158,
+    "SAT_f040" : 0.267,
+    "SAT_f090" : 1.51,
+    "SAT_f150" : 2.69,
+    "SAT_f230" : 7.27,
+    "SAT_f290" : 11.18,
+    "LAT_f030" : 0.21,
+    "LAT_f040" : 1.02,
+    "LAT_f090" : 1.04,
+    "LAT_f150" : 2.23,
+    "LAT_f230" : 7.50,
+    "LAT_f290" : 12.48,
+}
+# Optical power due to standard atmosphere
+P_ATM = {
+    "SAT_f030" : 0.059685257,
+    "SAT_f040" : 0.618673466,
+    "SAT_f090" : 0.811076040,
+    "SAT_f150" : 1.343902747,
+    "SAT_f230" : 4.118517930,
+    "SAT_f290" : 7.056159517,
+    "LAT_f030" : 0.042984911,
+    "LAT_f040" : 0.495809196,
+    "LAT_f090" : 0.509838815,
+    "LAT_f150" : 1.000122526,
+    "LAT_f230" : 3.359030038,
+    "LAT_f290" : 6.284548710,
+}
+# Optical effiency between the detector and the atmosphere
+ETA_ATM = {
+    "SAT_f030" : 0.160,
+    "SAT_f040" : 0.278,
+    "SAT_f090" : 0.206,
+    "SAT_f150" : 0.267,
+    "SAT_f230" : 0.310,
+    "SAT_f290" : 0.344,
+    "LAT_f030" : 0.115,
+    "LAT_f040" : 0.216,
+    "LAT_f090" : 0.130,
+    "LAT_f150" : 0.199,
+    "LAT_f230" : 0.252,
+    "LAT_f290" : 0.305,
+}
+# Saturation power [pW]
+P_SAT = {
+    "SAT_f030" : 1.08,
+    "SAT_f040" : 4.62,
+    "SAT_f090" : 3.42,
+    "SAT_f150" : 9.37,
+    "SAT_f230" : 29.4,
+    "SAT_f290" : 31.8,
+    "LAT_f030" : 1.08,
+    "LAT_f040" : 4.62,
+    "LAT_f090" : 3.42,
+    "LAT_f150" : 9.37,
+    "LAT_f230" : 29.4,
+    "LAT_f290" : 31.8,
+}
+# Bolometer Resistance [Ohm]
+R_BOLO = 0.008
+# Readout noise fraction
+R_FRAC = 0.5
 
 
 @trait_docs
@@ -59,6 +126,30 @@ class SimMuMUXCrosstalk(Operator):
         help="Observation detdata key for simulated signal",
     )
 
+    det_flags = Unicode(
+        defaults.det_flags,
+        allow_none=True,
+        help="Observation detdata key for flags to use",
+    )
+
+    det_flag_mask = Int(
+        defaults.det_mask_invalid,
+        help="Bit mask value for optional detector flagging",
+    )
+
+    shared_flags = Unicode(
+        defaults.shared_flags,
+        allow_none=True,
+        help="Observation shared key for telescope flags to use",
+    )
+
+    shared_flag_mask = Int(
+        defaults.shared_mask_invalid,
+        help="Bit mask value for optional telescope flagging",
+    )
+
+    realization = Int(0, help="Realization ID")
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
@@ -66,17 +157,23 @@ class SimMuMUXCrosstalk(Operator):
         """ Return the crosstalk strength parameter for a pair of detectors
         """
         # FIXME: implement _get_chi()
+        # We probably want to draw a table of chi somewhere
         if det_target == det_source:
             chi = 0
         else:
             chi = 1e-3
         return chi
 
-    def _temperature_to_squid_phase(self, input_signal):
+    def _temperature_to_squid_phase(
+            self, input_signal, detector, Phi0, dPhi0dT
+    ):
         """ Translate temperature-valued signal into SQUID phase
         """
         # FIXME: implement _temperature_to_squid_phase()
         output_signal = input_signal.copy()
+        # Need a mechanism for retrieving phi0 and dPhi0dT
+        # We probably want to draw a random table of Phi0 (at T0) somewhere
+        output_signal = Phi0[detector] + input_signal * dPhi0dT[detector]
         return output_signal
 
     def _rf_phase_to_temperature(self, input_signal):
@@ -93,6 +190,64 @@ class SimMuMUXCrosstalk(Operator):
         """
         # FIXME: double-check this
         return chi * np.sin(source_squid_phase - target_squid_phase)
+
+    def _draw_Phi0(self, obs, detectors, vmin=0.3, vmax=1.3):
+        """ Draw initial SQUID phases from a flat distribution
+        """
+        Phi0 = {}
+        for det in detectors:
+            # randomize Phi0 in a reproducible manner
+            counter1 = obs.session.uid
+            counter2 = self.realization
+            key1 = focalplane[det]["uid"]
+            key2 = 234561
+
+            x = toast.rng.random(
+                1,
+                sampler="uniform_01",
+                key=(key1, key2),
+                counter=(counter1, counter2),
+            )[0]
+            v = vmin + x * (vmax - vmin)
+            Phi0[det] = v
+
+        return Phi0
+
+    def _evaluate_dPhi0dT(self, obs, signal, detectors, rows, Phi0):
+        """ Estimate how the SQUID phase in each detector changes
+        with the sky temperature
+        """
+        focalplane = obs.telescope.focalplane
+        bandpass = focalplane.bandpass
+
+        if self.shared_flags is not None:
+            common_good = (
+                obs.shared[self.shared_flags].data & self.shared_flag_mask
+            ) == 0
+        else:
+            common_good = np.ones(obs.n_local_samples, dtype=bool)
+
+        dPhi0dT = {}
+        for row, det in zip(rows, detectors):
+            band = focalplane[det]["band"]
+            cfreq = bandpass.center_frequency(det) * 1e-9  # GHz
+            det_flags = obs.detdata[self.det_flags][det]
+            good = np.logical_and(
+                common_good, (det_flags & self.det_flag_mask) == 0
+            )
+            median_signal = np.median(signal[row][good])
+            P_opt = P_OPT[band] * 1e-12  # W
+            P_atm_ref = P_ATM[band] * 1e-12  # W
+            efficiency = ETA_ATM[band]
+            P_sat = P_SAT[band] * 1e-12  # W
+            P_atm = efficiency * bandpass.optical_loading(median_signal)  # W
+            P_opt += P_atm - P_atm_ref
+            dPdT = bandpass.kcmb2w(det)  # K_CMB -> W
+            dIdP = 1 / np.sqrt((P_sat - P_opt) * R_FRAC * R_BOLO)  # W -> A
+            dPhi0dI = Phi0[det] / 9e-6  # A -> [rad]
+            dPhi0dT[det] = dPdT * dIdP * dPhi0dI  # K_CMB -> [rad]
+
+        return dPhi0dT
 
     @function_timer
     def _exec(self, data, detectors=None, **kwargs):
@@ -134,20 +289,23 @@ class SimMuMUXCrosstalk(Operator):
             input_data = det_data.data.copy() * det_scale
             output_data = det_data.data  # just a reference
 
+            Phi0 = self._draw_Phi0(obs, detectors)
+            dPhi0dT = self._evaluate_dPhi0dT(obs, input_data, detectors, rows, Phi0)
+
             # For each detector-detector pair:
             #     Get crosstalk strength, chi
             #     Generate output data by mixing input data
             for row_target, det_target in zip(rows, detectors):
                 crosstalk = np.zeros_like(input_data[row_target])
                 target_squid_phase = self._temperature_to_squid_phase(
-                    input_data[row_target]
+                    input_data[row_target], det_target, Phi0[det], dPhi0dT[det]
                 )
                 for row_source, det_source in zip(rows, detectors):
                     chi = self._get_chi(det_target, det_source)
                     if chi == 0:
                         continue
                     source_squid_phase = self._temperature_to_squid_phase(
-                        input_data[row_source]
+                        input_data[row_source], det_source, Phi0[det], dPhi0dT[det]
                     )
                     crosstalk += chi * np.sin(
                         source_squid_phase - target_squid_phase
