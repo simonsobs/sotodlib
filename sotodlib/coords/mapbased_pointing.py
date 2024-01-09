ed4090c7a516614ae5d9dd92cd7ed4492289ac80
Tomoki Terasaki <tterasaki@compute11.simonsobs.org>
2024-01-09 02:38:21 +0000

    some modification for boresight rotation

--- sotodlib/coords/mapbased_pointing.py
+++ sotodlib/coords/mapbased_pointing.py
@@ -1,6 +1,9 @@
 import os
+from tqdm import tqdm
 import numpy as np
 from scipy import interpolate
+from scipy.optimize import curve_fit
+
 from sotodlib import coords
 
 from sotodlib.core import metadata
@@ -17,7 +20,7 @@ def get_planet_trajectry(tod, planet, _split=20, return_model=False):
     planet_az_sparse = np.zeros_like(timestamps_sparse)
     planet_el_sparse = np.zeros_like(timestamps_sparse)
     for i, timestamp in enumerate(timestamps_sparse):
-        az, el, _ = coords.planets.get_source_azel('planet', timestamp)
+        az, el, _ = coords.planets.get_source_azel(planet, timestamp)
         planet_az_sparse[i] = az
         planet_el_sparse[i] = el
     planet_az_func = interpolate.interp1d(timestamps_sparse, planet_az_sparse, kind="quadratic", fill_value='extrapolate')
@@ -30,48 +33,62 @@ def get_planet_trajectry(tod, planet, _split=20, return_model=False):
         q_planet = quat.rotation_lonlat(planet_az, planet_el)
         return q_planet
 
-def get_det_centered_sight(tod, planet, q_planet=None, q_bs=None, q_dets=None,):
+def get_det_centered_sight(tod, planet, q_planet=None, q_bs=None, q_det=None, 
+                           det=None):
     if q_planet is None:
         q_planet = get_planet_trajectry(tod, planet)
     if q_bs is None:
         q_bs = quat.rotation_lonlat(tod.boresight.az, tod.boresight.el)
-    if q_dets is None:
-        q_dets = quat.rotation_xieta(tod.focal_plane.xi, tod.focal_plane.eta)
+    if q_det is None:
+        di = np.where(tod.dets.vals == det)[0][0]
+        xi = tod.focal_plane.xi[di]
+        eta = tod.focal_plane.eta[di]
+        q_det = quat.rotation_xieta(-xi, eta)
+    
     z_to_x = quat.rotation_lonlat(0, 0)
-    sight = z_to_x * ~(q_bs * q_dets) * q_planet    
+    sight = z_to_x * ~(q_bs * q_det) * q_planet
     return sight
 
-def get_planet_centered_P(tod, q_planet=None, q_bs=None, q_dets=None,):
-    if q_planet is None:
-        q_planet = get_planet_trajectry(tod)
-    if q_bs is None:
-        q_bs = quat.rotation_lonlat(tod.boresight.az, tod.boresight.el)
-    if q_dets is None:
-        q_dets = quat.rotation_xieta(tod.focal_plane.xi, tod.focal_plane.eta)    
-    z_to_x = quat.rotation_lonlat(0, 0)
-    sight = z_to_x * ~q_planet * q_bs * q_dets
-    return sight
-
-def make_det_centered_maps(tod, hdf_path, cuts=None, wcs_kernel=None, res=0.1*coords.DEG, signal='signal'):
+def make_det_centered_maps(tod, planet, hdf_path, have_same_xieta=True, cuts=None,
+                           wcs_kernel=None, res=0.1*coords.DEG, signal='signal'):
     if wcs_kernel is None:
         wcs_kernel = coords.get_wcs_kernel('car', 0, 0, res)
-        
-    sight_bs_centered_planet = get_det_centered_sight(tod, q_dets=quat.rotation_xieta(0, 0))
-    for di, det in enumerate(tod.dets.vals):
-        tod_single = tod.restrict('dets', tod.dets.vals[di:di+1], in_place=False)
+    
+    q_planet = get_planet_trajectry(tod, planet)
+    q_bs = quat.rotation_lonlat(tod.boresight.az, tod.boresight.el)
+    if have_same_xieta:
+        xi_det, eta_det = tod.focal_plane.xi[0], tod.focal_plane.eta[0]
+        q_det = quat.rotation_xieta(-xi_det, eta_det)
+        tod_single = tod.restrict('dets', tod.dets.vals[:1], in_place=False)
+        sight = get_det_centered_sight(tod_single, planet, q_planet=q_planet, q_bs=q_bs, q_det=q_det)
+        tod_single.focal_plane.xi *= 0.
+        tod_single.focal_plane.eta *= 0.
+        tod_single.boresight.roll *= 0.
         if cuts is None:
             cuts_single = None
         else:
-            cuts_single = cuts[di]
-        
-        xi0, eta0 = tod_single.focal_plane.xi[0], tod_single.focal_plane.eta[0]
-        sight = sight_bs_centered_planet * quat.rotation_xieta(xi0, eta0)
+            cuts_single = cuts[0]
         P = coords.P.for_tod(tod=tod_single, wcs_kernel=wcs_kernel, comps='T', cuts=cuts_single, sight=sight)
+            
+    for di, det in enumerate(tqdm(tod.dets.vals)):
+        tod_single = tod.restrict('dets', tod.dets.vals[di:di+1], in_place=False)
+        
+        if not have_same_xieta:
+            xi_det, eta_det = tod_single.focal_plane.xi[0], tod_single.focal_plane.eta[0]
+            q_det = quat.rotation_xieta(-xi_det, eta_det)
+            sight = get_det_centered_sight(tod_single, planet, q_planet=q_planet, q_bs=q_bs, q_det=q_det)
+            tod_single.focal_plane.xi *= 0.
+            tod_single.focal_plane.eta *= 0.
+            if cuts is None:
+                cuts_single = None
+            else:
+                cuts_single = cuts[di]
+            P = coords.P.for_tod(tod=tod_single, wcs_kernel=wcs_kernel, comps='T', cuts=cuts_single, sight=sight)
         
         mT_weighted = P.to_map(tod=tod_single, signal=signal, comps='T', det_weights=None)
         wT = P.to_weights(tod_single, signal=signal, comps='T', det_weights=None)
         mT = P.remove_weights(signal_map=mT_weighted, weights_map=wT, comps='T')[0]
-        enmap.write_hdf(hdf_path, mT, address=det, extra={'xi0': xi0, 'eta0': eta0})    
+        enmap.write_hdf(hdf_path, mT, address=det, extra={'xi0': xi_det, 'eta0': eta_det})
     return
 
 def detect_peak_xieta(mT, filter_size=None):
@@ -82,8 +99,11 @@ def detect_peak_xieta(mT, filter_size=None):
     peak_i = int(np.median(peak_i))
     peak_j = int(np.median(peak_j))
     dec_grid, ra_grid = mT.posmap()
-    xi, eta = radec2xieta(ra_grid[peak_i][peak_j], dec_grid[peak_i][peak_j])
-    return xi, eta, peak_i, peak_j
+    
+    ra_peak = ra_grid[peak_i][peak_j]
+    dec_peak = dec_grid[peak_i][peak_j]
+    xi_peak, eta_peak = radec2xieta(ra_peak, dec_peak)
+    return xi_peak, eta_peak, ra_peak, dec_peak, peak_i, peak_j
 
 def get_center_of_mass(x, y, z, 
                        circle_mask={'x0':0, 'y0':0, 'r_circle':3.0*coords.DEG},
@@ -144,14 +164,17 @@ def radec2xieta(ra, dec, ra0=0, dec0=0):
     xi, eta, _ = quat.decompose_xieta(~q0 * q)
     return xi, eta
 
-def map_to_xieta(mT, edge_avoidance=1*coords.DEG, edge_check='nan',
-                 r_tune_circle=1.0*coords.DEG, q_tune=50):
-    xi_peak, eta_peak, peak_i, peak_j = detect_peak_xieta(mT)
+def _gauss1d(x, peak, sigma, base):
+    return peak * np.exp( - x**2 / (2*sigma**2)) + base
     
+def map_to_xieta(mT, edge_avoidance=1.0*coords.DEG, edge_check='nan',
+                 r_tune_circle=1.0*coords.DEG, q_tune=50, 
+                 R2_threshold=0.5, r_fit_circle=3.0*coords.DEG, beam_sigma_init=0.5*coords.DEG, ):
+    xi_peak, eta_peak, ra_peak, dec_peak, peak_i, peak_j = detect_peak_xieta(mT)
     edge_map = get_edgemap(mT, edge_avoidance=edge_avoidance, edge_check=edge_check)
-    valid = not edge_map[peak_i, peak_j]
+    edge_valid = not edge_map[peak_i, peak_j]
     
-    if valid:
+    if edge_valid:
         dec_flat, ra_flat = mT.posmap()
         dec_flat = dec_flat.flatten()
         ra_flat = ra_flat.flatten()
@@ -161,34 +184,70 @@ def map_to_xieta(mT, edge_avoidance=1*coords.DEG, edge_check='nan',
         percentile_mask = {'q': q_tune}
         xi_peak, eta_peak = get_center_of_mass(xi_flat, eta_flat, mT.flatten(), 
                                               circle_mask=circle_mask, percentile_mask=percentile_mask)
-        xi_det = -xi_peak
-        eta_det = eta_peak
+        
+        # check R2(=coefficient of determination)
+        r = np.sqrt((xi_flat - xi_peak)**2 + (eta_flat - eta_peak)**2)
+        z = mT.flatten()
+        mask_fit = np.logical_and(~np.isnan(z), r<r_fit_circle)
+        _r = r[mask_fit]
+        _z = z[mask_fit]
+        
+        if _r.shape[0] == 0:
+            xi_det = np.nan
+            eta_det = np.nan
+        else:
+            popt, pcov = curve_fit(_gauss1d, _r, _z,
+                                   p0=[np.ptp(_z), beam_sigma_init, np.percentile(_z, 1)],
+                                   bounds = ((-np.inf, beam_sigma_init/5, -np.inf),
+                                              (np.inf, beam_sigma_init*5, np.inf),),
+                                   max_nfev = 1000000
+                                  )
+            R2 = 1 - np.sum((_z - _gauss1d(_r, *popt))**2)/np.sum((_z - np.mean(_z))**2) # R2(=coefficient of determination)
+            if R2 >= R2_threshold:
+                xi_det = -xi_peak
+                eta_det = eta_peak
+            else:
+                xi_det = np.nan
+                eta_det = np.nan
     else:
         xi_det = np.nan
         eta_det = np.nan
-    return xi_det, eta_det
+    return xi_det, eta_det    
 
-def get_xieta_from_maps(map_hdf_file, r_tune_circle=1.0*coords.DEG, q_tune=50,
-                        save=False, output_file=None):
+def get_xieta_from_maps(map_hdf_file, 
+                        edge_avoidance=1.0*coords.DEG, edge_check='nan',
+                        r_tune_circle=1.0*coords.DEG, q_tune=50,
+                        save=False, output_dir=None, filename=None):
     _input_file = h5py.File(map_hdf_file, 'r')
     
     with _input_file as ifile:
         keys = list(ifile.keys())
         dets = [key for key in keys if isinstance(ifile[key], h5py.Group)]
         xieta_dict = {}
-        for det in dets:
+        for di, det in enumerate(tqdm(dets)):
             mT = enmap.read_hdf(ifile[det])
             mT[mT==0] = np.nan
-            xi, eta = map_to_xieta(mT)
+            xi, eta = map_to_xieta(mT, edge_avoidance=edge_avoidance, edge_check=edge_check,
+                                    r_tune_circle=r_tune_circle, q_tune=q_tune)
             xi0 = float(ifile[det]['xi0'][...])
             eta0 = float(ifile[det]['eta0'][...])
             xi, eta = _add_xieta((xi0, eta0), (xi, eta))
             xieta_dict[det] = {'xi':xi, 'eta':eta}
     if save:
-        fp = metadata.ResultSet(keys=['dets:readout_id', 'xi', 'eta', 'gamma'])
+        if output_dir is None:
+            output_dir = os.path.join(os.getcwd(), 'pointing_results')
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+        if filename is None:
+            filename = 'focalplane_' + os.path.splitext(os.path.basename(map_hdf_file))[0] + '.hdf'
+        output_file = os.path.join(output_dir, filename)
+        
+        focalplane = metadata.ResultSet(keys=['dets:readout_id', 'band', 'channel', 'xi0', 'eta0', 'gamma'])
         for det in dets:
-            fp.rows.append((det, xieta_dict[det]['xi'], xieta_dict[det]['eta'], 0.))
-        write_dataset(fp, output_file, 'focalplane', overwrite=True)
+            band = int(det.split('_')[-2])
+            channel = int(det.split('_')[-1])
+            focalplane.rows.append((det, band, channel, xieta_dict[det]['xi'], xieta_dict[det]['eta'], 0.))
+        write_dataset(focalplane, output_file, 'focalplane', overwrite=True)
     return xieta_dict
 
 def _add_xieta(xieta1, xieta2):
@@ -201,4 +260,3 @@ def _add_xieta(xieta1, xieta2):
     q_add = q2*q1
     xi_add, eta_add, _ = quat.decompose_xieta(q_add)
     return xi_add, eta_add
-    
