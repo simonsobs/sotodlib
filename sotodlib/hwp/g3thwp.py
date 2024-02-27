d015066ca38a16da85df65687f5fd4651dbb44e9
Kyohei YAMADA <38639108+ykyohei@users.noreply.github.com>
2024-02-26 20:36:27 -0500

    Compress h5 output of axisman.save (#730)
    * debug hwp direction correction
    
    * compress h5 by gzip
    
    * Revert "debug hwp direction correction"
    
    This reverts commit 2aaeb11a6b81da10192eea8782f98a83636d9828.
    
    * add compression as argument
    
    * Set default compression to None
    
    * fix and add test

--- sotodlib/hwp/g3thwp.py
+++ sotodlib/hwp/g3thwp.py
@@ -1074,7 +1074,7 @@ class G3tHWP():
         aman.primary_encoder = primary_encoder
         aman.version = highest_version
 
-        aman.save(output, h5_address, overwrite=True)
+        aman.save(output, h5_address, overwrite=True, compression='gzip')
 
     def _hwp_angle_calculator(
             self,
