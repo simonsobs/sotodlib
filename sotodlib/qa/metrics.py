477279150b51072c64664b2f18a0ba97ebb1de17
Tristan Pinsonneault-Marotte <tristpinsm@gmail.com>
2024-02-28 11:15:07 -0800

    Add QA metrics for HWP solution

--- sotodlib/qa/metrics.py
+++ sotodlib/qa/metrics.py
@@ -1,3 +1,6 @@
+import re
+import numpy as np
+
 from ..core import metadata
 
 
@@ -139,3 +142,186 @@ class PreprocessQA(QAMetric):
         # load manifest and read available observations
         man_db = metadata.ManifestDb.from_file(man_file[0])
         return [o[0] for o in man_db.get_entries(["\"obs:obs_id\""]).asarray()]
+
+
+class HWPSolQA(QAMetric):
+    """ Base class for metrics derived from HWP angle solutions. Subclasses should
+    implement the `_process` method. Some quantities are derived twice, once for each
+    encoder, and these will require and `encoder` parameter to be provided to select
+    which one to produce a metric for. This is indicated by setting the `_needs_encoder`
+    attribute to `True`.
+    """
+
+    _influx_meas = "hwp_solution"
+    _needs_encoder = False  # set this flag if encoder needs to be specified
+
+    def __init__(self, context, monitor, encoder=None, **kwargs):
+        super().__init__(context, monitor, **kwargs)
+
+        self._encoder = encoder
+        self._tags = {}
+        if encoder is not None:
+            if str(encoder) not in ["1", "2"]:
+                raise Exception(f"Invalid value {encoder} for encoder parameter.")
+            self._tags = {"encoder": self._encoder}
+        elif self._needs_encoder:
+            raise Exception("This metric needs an encoder to be specified on creation.")
+
+    def _get_available_obs(self):
+        # find preprocess manifest file
+        man_file = [p["db"] for p in self.context["metadata"] if p.get("name", "") == "hwp_solution"]
+        if len(man_file) == 0:
+            raise Exception(f"No hwp_solution metadata block in context {self.context.filename}.")
+
+        # load manifest and read available observations
+        man_db = metadata.ManifestDb.from_file(man_file[0])
+        obs_re = re.compile("^obs_*.")
+        return [o[0] for o in man_db.get_entries(["\"obs:obs_id\""]).asarray() if obs_re.match(o[0])]
+
+
+class HWPSolSuccess(HWPSolQA):
+    """ Records success of the HWP angle solution calculation, for each encode."""
+
+    _influx_field = "logger"
+    _needs_encoder = True
+
+    def _process(self, meta):
+        success = [meta.hwp_solution[f"logger_{self._encoder}"] == "Angle calculation succeeded"]
+        obs_time = [meta.obs_info.timestamp]
+        return {
+            "field": self._influx_field,
+            "values": success,
+            "timestamps": obs_time,
+            "tags": [self._tags],
+        }
+
+
+class HWPSolPrimaryEncoder(HWPSolQA):
+    """ The primary encoder used for the HWP angle calculation."""
+
+    _influx_field = "primary_encoder"
+
+    def _process(self, meta):
+        # no tags for this metric
+        return {
+            "field": self._influx_field,
+            "values": [meta.hwp_solution["primary_encoder"]],
+            "timestamps": [meta.obs_info.timestamp],
+            "tags": [self._tags],
+        }
+
+
+class HWPSolVersion(HWPSolQA):
+    """ The version of the solution used for the HWP angle calculation."""
+
+    _influx_field = "version"
+
+    def _process(self, meta):
+        # no tags for this metric
+        return {
+            "field": self._influx_field,
+            "values": [meta.hwp_solution["version"]],
+            "timestamps": [meta.obs_info.timestamp],
+            "tags": [self._tags],
+        }
+
+
+class HWPSolOffcenter(HWPSolQA):
+    """ Calculated offcentering of HWP angle solution."""
+
+    _influx_field = "offcenter"
+
+    def _process(self, meta):
+        # no tags for this metric
+        return {
+            "field": self._influx_field,
+            "values": [meta.hwp_solution["offcenter"][0]],
+            "timestamps": [meta.obs_info.timestamp],
+            "tags": [self._tags],
+        }
+
+
+class HWPSolOffcenterErr(HWPSolQA):
+    """ Standard error on the offcentering of HWP angle solution."""
+
+    _influx_field = "offcenter_err"
+
+    def _process(self, meta):
+        # no tags for this metric
+        return {
+            "field": self._influx_field,
+            "values": [meta.hwp_solution["offcenter"][1]],
+            "timestamps": [meta.obs_info.timestamp],
+            "tags": [self._tags],
+        }
+
+
+class HWPSolNumSamples(HWPSolQA):
+    """ The total number of encoder samples."""
+
+    _influx_field = "num_samples"
+    _needs_encoder = True
+
+    def _process(self, meta):
+        flag_key = f"filled_flag_{self._encoder}"
+        frac = [meta.hwp_solution[flag_key].size]
+        obs_time = [meta.obs_info.timestamp]
+        return {
+            "field": self._influx_field,
+            "values": frac,
+            "timestamps": obs_time,
+            "tags": [self._tags],
+        }
+
+
+class HWPSolNumFlagged(HWPSolQA):
+    """ The number of encoder samples that were flagged."""
+
+    _influx_field = "num_flagged"
+    _needs_encoder = True
+
+    def _process(self, meta):
+        flag_key = f"filled_flag_{self._encoder}"
+        frac = [meta.hwp_solution[flag_key].sum()]
+        obs_time = [meta.obs_info.timestamp]
+        return {
+            "field": self._influx_field,
+            "values": frac,
+            "timestamps": obs_time,
+            "tags": [self._tags],
+        }
+
+
+class HWPSolMeanRate(HWPSolQA):
+    """ The mean calculated HWP angle rate, for each encoder."""
+
+    _influx_field = "mean_rate"
+    _needs_encoder = True
+
+    def _process(self, meta):
+        good_samp = ~meta.hwp_solution[f"filled_flag_{self._encoder}"]
+        nsamp = good_samp.sum()
+        rate = np.nan if nsamp == 0 else (meta.hwp_solution[f"hwp_rate_{self._encoder}"] * good_samp).sum() / nsamp
+        obs_time = [meta.obs_info.timestamp]
+        return {
+            "field": self._influx_field,
+            "values": [rate],
+            "timestamps": obs_time,
+            "tags": [self._tags],
+        }
+
+
+class HWPSolMeanTemplate(HWPSolQA):
+    """ The mean of the calculated template magnitude."""
+
+    _influx_field = "mean_template"
+    _needs_encoder = True
+
+    def _process(self, meta):
+        obs_time = [meta.obs_info.timestamp]
+        return {
+            "field": self._influx_field,
+            "values": [np.mean(np.abs(meta.hwp_solution[f"template_{self._encoder}"]))],
+            "timestamps": obs_time,
+            "tags": [self._tags],
+        }
