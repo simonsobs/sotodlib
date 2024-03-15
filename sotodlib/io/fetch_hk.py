ddb3deafc7d62c75ed1a3842b6e609175bf67faf
Sanah Bhimani <sanahbhimani@gmail.com>
2024-03-15 17:31:33 +0000

    update to include HK books

--- sotodlib/io/fetch_hk.py
+++ sotodlib/io/fetch_hk.py
@@ -8,17 +8,31 @@ import logging
 hk_logger = logging.getLogger(__name__)
 hk_logger.setLevel(logging.INFO)
 
-def fetch_hk(start, stop, data_dir, fields=None):
+def fetch_hk(start, stop, data_dir, fields=None, folder_patterns=None):
     hk_data = {}
     bases = []
 
     start_ctime = start #- 3600
     stop_ctime = stop #+ 3600
+    
+    if folder_patterns is None:
+        folder_patterns = ['{folder}', 'hk_{folder}_*']
+    for folder in range( int(start_ctime/1e5), int(stop_ctime/1e5)+1):
+        bases = []
+        for pattern in folder_patterns:
+            extended_pattern = pattern.format(folder=folder)
 
-    for folder in range(int(start_ctime/1e5), int(stop_ctime/1e5)+1):
-        base = data_dir+'/'+str(folder)
-        if not os.path.exists(base):
-            hk_logger.debug(f'{base} does not exist, skipping')
+            base = glob.glob(os.path.join(data_dir, extended_pattern))
+            bases.extend(base)
+        
+        if len(bases) > 1:
+            bases.sort
+            base = bases[0]
+            hk_logger.warn(f"Multiple base folders were found for {folder}. The first one, alphabetically, is selected: {base}")
+        elif len(bases) == 1:
+            base = bases[0]
+        elif len(bases) == 0:
+            hk_logger.debug(f"No base folder found for {folder}, skipping")
             continue
 
         for file in sorted(os.listdir(base)):
