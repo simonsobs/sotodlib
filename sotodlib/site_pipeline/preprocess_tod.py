f805529680de5f873549541ef00100d9c1a20123
Max Silva-Feaver <maximiliano.silva-feaver@yale.edu>
2024-04-01 17:13:39 +0000

    Handle run failure with traceback for now.

--- sotodlib/site_pipeline/preprocess_tod.py
+++ sotodlib/site_pipeline/preprocess_tod.py
@@ -2,6 +2,7 @@ import os
 import yaml
 import numpy as np
 import argparse
+import traceback
 
 from sotodlib import core
 import sotodlib.site_pipeline.util as sp_util
@@ -264,8 +265,14 @@ def main(
     logger.info(f"Beginning to run preprocessing on {len(run_list)} observations")
     for obs, groups in run_list:
         logger.info(f"Processing obs_id: {obs_id}")
-        preprocess_tod(obs["obs_id"], configs, overwrite=overwrite,
-                       group_list=groups, logger=logger)
+        try:
+            preprocess_tod(obs["obs_id"], configs, overwrite=overwrite,
+                           group_list=groups, logger=logger)
+        except Exception as e:
+            logger.info(f"{type(e)}: {e}")
+            logger.info(''.join(traceback.format_tb(e.__traceback__)))
+            logger.info(f'Skiping obs:{obs["obs_id"]} and moving to the next')
+            continue
             
 
 if __name__ == '__main__':
