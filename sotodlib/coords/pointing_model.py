fc6a9e49a981ccb2836db7682f1798a9d15a5426
Elle Shaw <eshaw@login.simonsobs.org>
2024-04-05 17:51:47 +0000

    Update apply_pointing_model to overwrite and return tod.boresight

--- sotodlib/coords/pointing_model.py
+++ sotodlib/coords/pointing_model.py
@@ -7,7 +7,7 @@ DEG = np.pi / 180
 
 def apply_pointing_model(tod):
     """
-    Returns an updated boresight AxisManager after applying pointing model parameters.
+    Updates and returns the tod.boresight AxisManager after applying pointing model parameters.
 
     Args:
         tod: AxisManager (must contain 'ancil' field)
@@ -25,12 +25,16 @@ def apply_pointing_model(tod):
         # Returns in Radians
         _boresight.wrap("az", _ancil.az_enc * DEG, [(0, "samps")])
         _boresight.wrap("el", _ancil.el_enc * DEG, [(0, "samps")])
-        _boresight.wrap("roll", (_ancil.el_enc - 60 - _ancil.corotator_enc) * DEG, [(0, "samps")])
+        _boresight.wrap(
+            "roll", (_ancil.el_enc - 60 - _ancil.corotator_enc) * DEG, [(0, "samps")]
+        )
 
     elif "sat" in telescope:
         # Returns in radians
         _boresight.wrap("az", _ancil.az_enc * DEG, [(0, "samps")])
         _boresight.wrap("el", _ancil.el_enc * DEG, [(0, "samps")])
         _boresight.wrap("roll", -1 * _ancil.boresight_enc * DEG, [(0, "samps")])
+
+    tod.boresight = _boresight
 
     return _boresight
