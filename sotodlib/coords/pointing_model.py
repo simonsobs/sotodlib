Fix boresight Roll definition in load_book and the return value of apply_pointing_model The sign error in boresight.roll for SATP platforms was corrected in io.load_book, and corotation_enc was changed to corotator_enc for LAT. coords.apply_pointing_model was updated to return the boresight AxisManager.

sotodlib/coords/pointing_model.py
@@ -6,30 +6,31 @@ DEG = np.pi / 180
 
 
 def apply_pointing_model(tod):
-    """Redefines tod.boresight using pointing model parameters.
+    """
+    Returns an updated boresight AxisManager after applying pointing model parameters.
 
     Args:
-        tod: AxisManager
+        tod: AxisManager (must contain 'ancil' field)
 
     Returns:
-        tod.boresight will be updated.
+        _boresight : AxisManager containing new boresight data.
     """
 
-    _a = _valid_arg("ancil", src=tod)
+    _ancil = _valid_arg("ancil", src=tod)
     telescope = _valid_arg("obs_info", src=tod)["telescope"]
     # Start with a fresh axismanager
-    _b = core.AxisManager(core.OffsetAxis("samps", len(tod.timestamps)))
+    _boresight = core.AxisManager(core.OffsetAxis("samps", len(tod.timestamps)))
 
     if "lat" in telescope:
         # Returns in Radians
-        _b.wrap("az", _a.az_enc * DEG, [(0, "samps")])
-        _b.wrap("el", _a.el_enc * DEG, [(0, "samps")])
-        _b.wrap("roll", (_a.el_enc - 60 - _a.corotator_enc) * DEG, [(0, "samps")])
+        _boresight.wrap("az", _ancil.az_enc * DEG, [(0, "samps")])
+        _boresight.wrap("el", _ancil.el_enc * DEG, [(0, "samps")])
+        _boresight.wrap("roll", (_ancil.el_enc - 60 - _ancil.corotator_enc) * DEG, [(0, "samps")])
 
     elif "sat" in telescope:
         # Returns in radians
-        _b.wrap("az", _a.az_enc * DEG, [(0, "samps")])
-        _b.wrap("el", _a.el_enc * DEG, [(0, "samps")])
-        _b.wrap("roll", -1 * _a.boresight_enc * DEG, [(0, "samps")])
+        _boresight.wrap("az", _ancil.az_enc * DEG, [(0, "samps")])
+        _boresight.wrap("el", _ancil.el_enc * DEG, [(0, "samps")])
+        _boresight.wrap("roll", -1 * _ancil.boresight_enc * DEG, [(0, "samps")])
 
-    tod["boresight"] = _b
+    return _boresight