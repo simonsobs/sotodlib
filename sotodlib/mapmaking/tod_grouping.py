e04197c3dfe1966099001c2de62ab925f9ba2a36
chervias <aconcagua.chc@gmail.com>
2024-04-12 09:51:36 -0700

    glitch_flags cuts implemented in demod mapmaking, also freq parameter to map a specific band

--- sotodlib/mapmaking/tod_grouping.py
+++ sotodlib/mapmaking/tod_grouping.py
@@ -97,7 +97,7 @@ def split_periods(periods, maxdur):
     t2     = np.minimum(periods[group,0]+(sub+1)*maxdur, periods[group,1])
     return np.array([t1,t2]).T
 
-def build_period_obslists(obs_info, periods, context, nset=None, wafer=None):
+def build_period_obslists(obs_info, periods, context, nset=None, wafer=None, freq=None):
     """For each period for each detset-band, make a list of (id,detset,band)
     for the ids that fall inside that period. Returns a dictionary
     that maps (pid,deset,band) to those lists. pid is here the index into
@@ -113,7 +113,10 @@ def build_period_obslists(obs_info, periods, context, nset=None, wafer=None):
         else:
             meta = context.get_meta(row.obs_id)
             wafer_list = np.unique(meta.det_info.wafer_slot)
-        band_list = ['f090', 'f150']
+        if freq is not None:
+            band_list = [freq]
+        else:
+            band_list = ['f090','f150']
         for detset in wafer_list[:nset]:
             for band in band_list:
                 key = (pids[i], detset, band)
@@ -121,7 +124,7 @@ def build_period_obslists(obs_info, periods, context, nset=None, wafer=None):
                 obslists[key].append((row.obs_id, detset, band, i))
     return obslists
 
-def build_obslists(context, query, mode=None, nset=None, wafer=None, ntod=None, tods=None, fixed_time=None, mindur=None, ):
+def build_obslists(context, query, mode=None, nset=None, wafer=None, freq=None, ntod=None, tods=None, fixed_time=None, mindur=None, ):
     """ 
     Return an obslists dictionary (described in build_period_obslists), along with all ancillary data necessary for the mapmaker
     
@@ -184,6 +187,6 @@ def build_obslists(context, query, mode=None, nset=None, wafer=None, ntod=None, 
         sys.exit(1)
     
     # We will make one map per period-detset-band
-    obslists = build_period_obslists(obs_infos, periods, context, nset=nset, wafer=wafer)
+    obslists = build_period_obslists(obs_infos, periods, context, nset=nset, wafer=wafer, freq=freq)
     obskeys  = sorted(obslists.keys())
     return obslists, obskeys, periods, obs_infos
