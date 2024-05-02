328c5561aaf382c26f659c3c02fe621ae8d2a9f0
Jack Lashner <jlashner@gmail.com>
2024-05-02 11:32:22 -0400

    adds hkdb update script to site_pipline

---
+++ sotodlib/site_pipeline/update_hkdb.py
@@ -0,0 +1,12 @@
+"""
+Script for calling hkdb update function with a configuration file
+"""
+from sotodlib.io import hkdb 
+
+def main(cfg_file: str):
+    """Updates hkdb index databases"""
+    hkdb.update_index_all(cfg_file)
+
+if __name__ == "__main__":
+    import sys
+    main(sys.argv[1])
