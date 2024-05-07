cdbabb6358cc7146d20618fab416511abee642a8
tterasaki <tomoki.terasaki@phys.s.u-tokyo.ac.jp>
2024-05-07 10:50:45 +0000

    fixed if

--- sotodlib/tod_ops/filters.py
+++ sotodlib/tod_ops/filters.py
@@ -487,7 +487,7 @@ def iir_filter(freqs, tod, b=None, a=None, fscale=1., iir_params=None,
             iir_params = 'iir_params'
         if isinstance(iir_params, str):
             iir_params = tod[iir_params]
-        if isinstance(iir_params, core.AxisManager):
+        if 'a' not in list(iir_params._fields.keys()):
             iir_params = iir_params[list(iir_params._fields)[1]]
         try:
             a = iir_params['a']
