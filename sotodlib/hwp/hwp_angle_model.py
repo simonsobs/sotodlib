9402a41801f76dca41433aa88e3e4520718acdd3
ykyohei <38639108+ykyohei@users.noreply.github.com>
2024-05-10 20:56:12 +0000

    address suggestions

--- sotodlib/hwp/hwp_angle_model.py
+++ sotodlib/hwp/hwp_angle_model.py
@@ -1,64 +1,69 @@
 import numpy as np
 
 
-def apply_hwp_angle_model(tod, hwp_solution_attr='hwp_solution', hwp_angle_model_attr='hwp_angle_model'):
+def apply_hwp_angle_model(tod, on_sign_ambiguous='fail'):
     """
-    Updates and returns the tod AxisManager after applying hwp angle model parameters.
+    Returns the tod AxisManager after applying hwp angle model parameters.
     hwp_angle_model correct the sign and offset of hwp_angle.
 
     hwp_angle_corrected = sign * hwp_angle + offset
 
     Args:
         tod: AxisManager
+        on_sign_ambiguous: Tolerance options for sign ambiguous
+            fail: raise an error if there is any sign ambiguous
+            pid: raise an error if pid sign does not exist
+            offcenter: raise an error if offcenter sign does not exist
 
     Returns:
-        tod: AxisManager containing new hwp_angle data
+        tod: AxisManager with hwp_angle attribute
     """
 
     telescope = tod.obs_info.telescope
     if "lat" in telescope:
         return tod
 
-    hwp = getattr(tod, hwp_solution_attr)
+    hwp = tod['hwp_solution']
 
-    # sign
-    # Relative sign between observations will be estimated from hwp_metadata by multiple methods
-    # sign matrix contains absolute sign correction factor for each relative sign estimation method.
-    # sign matrix will be loaded from hwp_angle_model
-    # We will be able to add "scan" and "template"
+    # sign matrix contains absolute sign correction factor for each
+    # relative sign estimators.
     sign_matrix = {
         'satp1': {'pid':  1, 'offcenter': -1, },
         'satp3': {'pid': -1, 'offcenter':  1, },
     }
 
-    # check available sign estimation methods
-    available_signs = []
-
     methods = ['pid', 'offcenter']
-    for method in methods:
-        _sign = getattr(hwp, method + '_direction')
-        if _sign != 0:
-            available_signs.append(_sign*sign_matrix[telescope][method])
+    if on_sign_ambiguous in methods:
+        sign = hwp[on_sign_ambiguous + '_direction']
+        sign *= sign_matrix[telescope][on_sign_ambiguous]
+        if sign == 0:
+            raise ValueError('hwp rotation direction is ambiguous')
+    elif on_sign_ambiguous == 'fail':
+        available_signs = []
+        for method in methods:
+            _sign = hwp[method + '_direction']
+            if _sign != 0:
+                available_signs.append(_sign*sign_matrix[telescope][method])
 
-    # check agreements of available estimation methods
-    if len(available_signs) == 0:
-        print('WARNING: relative hwp rotation direction is ambiguous')
-        sign = 1.
-    if np.all(np.array(available_signs) == available_signs[0]):
-        sign = available_signs[0]
+        # check agreements of available estimation methods
+        if len(available_signs) == 0:
+            raise ValueError('hwp rotation direction is ambiguous')
+        if np.all(np.array(available_signs) == available_signs[0]):
+            sign = available_signs[0]
+        else:
+            raise ValueError('hwp rotation direction is ambiguous')
     else:
-        print('WARNING: relative hwp rotation direction is ambiguous')
-        sign = 1.
+        raise ValueError('Invalid on_sign_ambiguous')
 
-    # offset (degree). Convert to radian when we apply correction
+    # angle offset (degree).
     # Mechanically determined offsets
     encoder_offset = -1.66
     encoder_assembly_offset = {
         'satp1': {1: -90, 2: 90},
         'satp3': {1: 90, 2: -90},
     }
-    # offset of the optical axis of the achromatic hwp
-    # this needs to be calibrated optically, this will be loaded from hwp_angle_model
+    # angle offset of the optical axis of the achromatic hwp
+    # This will be loaded from hwp_angle_model metadata
     ahwp_offset = {
         'satp1': 49.1,
         'satp3': -2.29,
@@ -69,7 +74,10 @@ def apply_hwp_angle_model(tod, hwp_solution_attr='hwp_solution', hwp_angle_model
         ahwp_offset[telescope]
 
     # apply correction
-    tod.hwp_angle = np.mod(
-        sign*tod.hwp_angle + np.deg2rad(offset), 2*np.pi)
+    hwp_angle = np.mod(sign*hwp.hwp_angle + np.deg2rad(offset), 2*np.pi)
+    if 'hwp_angle' in tod._assignments.keys():
+        tod.hwp_angle = hwp_angle
+    else:
+        tod.wrap('hwp_angle', hwp_angle, [(0, "samps")])
 
     return tod
