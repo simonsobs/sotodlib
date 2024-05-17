9116470aca0b2b1cdeb9127dfd3b5283dfa9c900
David Nguyen <david.nguyen@yale.edu>
2024-05-16 22:41:21 -0400

    Fix preprocess_obs script for prefect (#859)
    * set tags arg to Optional[List[str]]
    
    * add changes from preprocess_tod
    
    * fix whitespace

--- sotodlib/site_pipeline/preprocess_obs.py
+++ sotodlib/site_pipeline/preprocess_obs.py
@@ -4,7 +4,7 @@ import time
 import numpy as np
 import argparse
 import traceback
-from typing import Optional
+from typing import Optional, List
 
 from sotodlib import core
 import sotodlib.site_pipeline.util as sp_util
@@ -106,7 +106,9 @@ def preprocess_obs(
     
     logger.info(f"Saving to database under {db_data}")
     if len(db.inspect(db_data)) == 0:
-        db.add_entry(db_data, dest_file)
+        h5_path = os.path.relpath(dest_file,
+                start=os.path.dirname(configs['archive']['index']))
+        db.add_entry(db_data, h5_path)
 
 def load_preprocess_obs(obs_id, configs="preprocess_obs_configs.yaml", context=None ):
     """ Loads the saved information from the preprocessing pipeline and runs the
@@ -185,7 +187,7 @@ def main(
     min_ctime: Optional[int] = None,
     max_ctime: Optional[int] = None,
     update_delay: Optional[int] = None,
-    tags: Optional[str] = None,
+    tags: Optional[List[str]] = None,
     planet_obs: bool = False,
  ):
     configs, context = _get_preprocess_context(configs)
@@ -209,10 +211,11 @@ def main(
         if tot_query=="":
             tot_query="1"
     
-    for i, tag in enumerate(tags):
-        tags[i] = tags[i].lower()
-        if '=' not in tags[i]:
-            tags[i] += '=1'
+    if not(tags is None):
+        for i, tag in enumerate(tags):
+            tags[i] = tags[i].lower()
+            if '=' not in tags[i]:
+                tags[i] += '=1'
 
     if planet_obs:
         obs_list = []
