a3c6a4dff060264d0b3eac566f42314f42934d43
tterasaki <tomoki.terasaki@phys.s.u-tokyo.ac.jp>
2024-06-27 08:24:09 +0000

    make the apodization function easier to use

--- sotodlib/tod_ops/apodize.py
+++ sotodlib/tod_ops/apodize.py
@@ -1,5 +1,12 @@
 import numpy as np
 
+def get_apodize_window(aman, apodize_samps=1600):
+    w = np.ones(aman.samps.count)
+    cosedge = np.cos(np.linspace(0, np.pi/2, apodize_samps))
+    w[-apodize_samps:] = cosedge
+    w[:apodize_samps] = np.flip(cosedge)
+    return w
+    
 def apodize_cosine(aman, signal='signal', apodize_samps=1600, in_place=True,
                    apo_axis='apodized'):
     """
@@ -12,10 +19,7 @@ def apodize_cosine(aman, signal='signal', apodize_samps=1600, in_place=True,
         in_place (bool): writes over signal with apodized version
         apo_axis (str): 
     """
-    w = np.ones(aman.samps.count)
-    cosedge = np.cos(np.linspace(0, np.pi/2, apodize_samps))
-    w[-apodize_samps:] = cosedge
-    w[:apodize_samps] = np.flip(cosedge)
+    w = get_apodize_window(aman, apodize_samps)
     if in_place:
         aman[signal] *= w
     else:
