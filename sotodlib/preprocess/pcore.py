5a813f4d19e24b1c32c6d3c84ed7fcaaa2e2dfa8
David Nguyen <david.nguyen@yale.edu>
2024-09-26 13:25:24 -0700

    fix run_plot variable

--- sotodlib/preprocess/pcore.py
+++ sotodlib/preprocess/pcore.py
@@ -434,6 +434,7 @@ class Pipeline(list):
             proc_aman = core.AxisManager( aman.dets, aman.samps)
             full = core.AxisManager( aman.dets, aman.samps)
             run_calc = True
+            run_plot = False
         else:
             if aman.dets.count != proc_aman.dets.count or not np.all(aman.dets.vals == proc_aman.dets.vals):
                 self.logger.warning("proc_aman has different detectors than aman. Cutting aman to match")
@@ -444,6 +445,8 @@ class Pipeline(list):
             run_calc = False
             if update_plot:
                 run_plot = True
+            else:
+                run_plot = False
         
         success = 'end'
         for step, process in enumerate(self):
