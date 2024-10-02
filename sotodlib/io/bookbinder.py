58c7ecc8b1ddc9bc7e5e0b023b2d6685caa4efc6
jlashner <jlashner@gmail.com>
2024-10-02 15:50:07 -0700

    Change non-res name to 'untracked'

--- sotodlib/io/bookbinder.py
+++ sotodlib/io/bookbinder.py
@@ -625,7 +625,7 @@ class SmurfStreamProcessor:
         in_offset_idxs = np.arange(len(self.times)) - offsets[self.frame_idxs]
 
         # Mask to use to separate out non-resonator signal from resonator signal
-        res_chans = np.array(['NONE' not in r for r in self.readout_ids], dtype=bool)
+        tracked_chans = np.array(['NONE' not in r for r in self.readout_ids], dtype=bool)
 
         # Handle file writers
         writer = None
@@ -743,8 +743,8 @@ class SmurfStreamProcessor:
             oframe['flag_smurfgaps'] = core.G3VectorBool(~filled)
 
             ts = core.G3VectorTime(ts * core.G3Units.s)
-            oframe['signal'] = so3g.G3SuperTimestream(self.readout_ids[res_chans], ts, data[res_chans, :])
-            oframe['non_res_signal'] = so3g.G3SuperTimestream(self.readout_ids[~res_chans], ts, data[~res_chans, :])
+            oframe['signal'] = so3g.G3SuperTimestream(self.readout_ids[tracked_chans], ts, data[tracked_chans, :])
+            oframe['untracked'] = so3g.G3SuperTimestream(self.readout_ids[~tracked_chans], ts, data[~tracked_chans, :])
             oframe['primary'] = so3g.G3SuperTimestream(self.primary_names, ts, primary)
             oframe['tes_biases'] = so3g.G3SuperTimestream(self.bias_names, ts, biases)
             oframe['stream_id'] = self.stream_id
