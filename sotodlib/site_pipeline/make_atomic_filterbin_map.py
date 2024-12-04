af69e5565bd22dc014e92cfb97df08ea52b3216a
chervias <aconcagua.chc@gmail.com>
2024-12-04 06:46:06 -0800

    Changing the format of the input parameters to the main function

--- sotodlib/site_pipeline/make_atomic_filterbin_map.py
+++ sotodlib/site_pipeline/make_atomic_filterbin_map.py
@@ -1,4 +1,5 @@
 from argparse import ArgumentParser
+from typing import Optional
 import time
 import warnings
 import os
@@ -18,113 +19,102 @@ from pixell import wcsutils, colors, memory, mpi
 from concurrent.futures import ProcessPoolExecutor, as_completed
 import sotodlib.site_pipeline.util as util
 
-defaults = {
-    "area": None,
-    "nside": None,
-    "query": "type == 'obs' and subtype == 'cmb'",
-    "odir": "./output",
-    "update_delay": None,
-    "comps": "TQU",
-    "nproc": 1,
-    "ntod": None,
-    "tods": None,
-    "nset": None,
-    "wafer": None,
-    "freq": None,
-    "center_at": None,
-    "dec_ref": -40.0,
-    "site": 'so_sat3',
-    "max_dets": None,  # not implemented yet
-    "verbose": 0,
-    "quiet": 0,
-    "tiled": 0,  # not implemented yet
-    "singlestream": False,
-    "only_hits": False,
-    "det_in_out": False,
-    "det_left_right": False,
-    "det_upper_lower": False,
-    "scan_left_right": False,
-    "window": 0.0,  # not implemented yet
-    "dtype_tod": 'float32',
-    "dtype_map": 'float64',
-    "atomic_db": "atomic_maps.db",
-    "fixed_time": None,
-    "min_dur": None,
-    "hk_data_path": None,
-    }
-
-
 def get_parser(parser=None):
     if parser is None:
         parser = ArgumentParser()
-    parser.add_argument("--config_file", type=str, default=None,
-                        help="Path to mapmaker config.yaml file")
     parser.add_argument("--context", type=str, help='Path to context file')
     parser.add_argument("--preprocess_config", type=str,
                         help='Path to config file to run the\
                         preprocessing pipeline')
     parser.add_argument("--area",
-                        help='WCS kernel for rectangular pixels')
+                        help='WCS kernel for rectangular pixels',
+                        default=None)
     parser.add_argument("--nside",
-                        help='Nside for HEALPIX pixels')
+                        help='Nside for HEALPIX pixels',
+                       default=None)
     parser.add_argument("--query",
                         help='Query, can be a file (list of obs_id)\
                         or selection string (will select only CMB scans\
-                        by default)')
+                        by default)',
+                        default="type == 'obs' and subtype == 'cmb'")
     parser.add_argument("--odir",
-                        help='Output directory')
-    parser.add_argument('--update_delay', type=int,
+                        help='Output directory',
+                        default='./output')
+    parser.add_argument('--update_delay', type=float,
                         help="Number of days (unit is days) in the past\
-                        to start observation list")
+                        to start observation list",
+                        default=None)
     parser.add_argument("--nproc", type=int,
                         help='Number of procs in\
-                        the multiprocessing pool')
+                        the multiprocessing pool',
+                        default=1)
     parser.add_argument("--comps", type=str,
-                        help="Components to map (TQU by default)")
+                        help="Components to map (TQU by default)",
+                        default='TQU')
     parser.add_argument("--singlestream", action="store_true",
                         help="Map without demodulation (e.g. with\
-                        a static HWP)")
+                        a static HWP)",
+                        default=False)
     parser.add_argument("--only_hits", action="store_true",
-                        help='Only create a hits map')
+                        help='Only create a hits map',
+                        default=False)
 
     # detector position splits (fixed in time)
-    parser.add_argument("--det_in_out", action="store_true")
-    parser.add_argument("--det_left_right", action="store_true")
-    parser.add_argument("--det_upper_lower", action="store_true")
+    parser.add_argument("--det_in_out", action="store_true", 
+                        default=False)
+    parser.add_argument("--det_left_right", action="store_true",
+                        default=False)
+    parser.add_argument("--det_upper_lower", action="store_true",
+                        default=False)
 
     # time samples splits
-    parser.add_argument("--scan_left_right", action="store_true")
+    parser.add_argument("--scan_left_right", action="store_true",
+                        default=False)
 
-    parser.add_argument("--ntod", type=int, )
-    parser.add_argument("--tods", type=str, )
-    parser.add_argument("--nset", type=int, )
+    parser.add_argument("--ntod", type=int,
+                        default=None)
+    parser.add_argument("--tods", type=str,
+                        default=None)
+    parser.add_argument("--nset", type=int,
+                        default=None)
     parser.add_argument("--wafer", type=str,
-                        help="Detector set to map with")
+                        help="Detector set to map with",
+                        default=None)
     parser.add_argument("--freq", type=str,
-                        help="Frequency band to map with")
+                        help="Frequency band to map with",
+                       default=None)
     parser.add_argument("--dec_ref", type=float,
                         help="Decl. at which we will calculate the\
-                        reference R.A.")
-    parser.add_argument("--center_at", type=str)
-    parser.add_argument("--max_dets", type=int, )
-    parser.add_argument("--fixed_ftime", type=int, )
-    parser.add_argument("--min_dur", type=int, )
-    parser.add_argument("--site", type=str, )
-    parser.add_argument("--verbose", action="count", )
-    parser.add_argument("--quiet", action="count", )
-    parser.add_argument("--window", type=float, )
-    parser.add_argument("--dtype_tod", type=str)
-    parser.add_argument("--dtype_map", type=str)
+                        reference R.A.",
+                        default=-40.0)
+    parser.add_argument("--center_at", type=str,
+                        default=None)
+    parser.add_argument("--max_dets", type=int,
+                        default=None)
+    parser.add_argument("--fixed_time", type=int,
+                        default=None)
+    parser.add_argument("--min_dur", type=int,
+                        default=None)
+    parser.add_argument("--site", type=str,
+                        default='so_sat3')
+    parser.add_argument("--verbose", action="count",
+                        default=0)
+    parser.add_argument("--quiet", action="count",
+                        default=0)
+    parser.add_argument("--window", type=float,
+                        default=None)
+    parser.add_argument("--dtype_tod", type=str,
+                        default='float32')
+    parser.add_argument("--dtype_map", type=str,
+                        default='float64')
     parser.add_argument("--atomic_db", type=str,
                         help='name of the atomic map database, will be\
-                        saved where this script is being run')
+                        saved where this script is being run',
+                        default=None)
     parser.add_argument("--hk_data_path",
-                        help='Path to housekeeping data')
+                        help='Path to housekeeping data',
+                        default=None)
     return parser
-
-
-def _get_config(config_file):
-    return yaml.safe_load(open(config_file, 'r'))
 
 
 class DataMissing(Exception):
@@ -226,7 +216,41 @@ def future_write_to_log(e, errlog):
     f.close()
 
 
-def main(config_file=None, defaults=defaults, **args):
+def main(
+    context: str,
+    preprocess_config: str,
+    area: Optional[str] = None,
+    nside: Optional[int] = None,
+    query: Optional[str] = "type == 'obs' and subtype == 'cmb'",
+    odir: Optional[str] = "./output",
+    update_delay: Optional[float] = None,
+    site: Optional[str] = 'so_sat3',
+    hk_data_path: Optional[str] = None,
+    nproc: Optional[int] = 1,
+    atomic_db: Optional[str] = None,
+    comps: Optional[str] = 'TQU',
+    singlestream: Optional[bool] = False,
+    only_hits: Optional[bool] = False,
+    det_in_out: Optional[bool] = False,
+    det_left_right: Optional[bool] = False,
+    det_upper_lower: Optional[bool] = False,
+    scan_left_right: Optional[bool] = False,
+    ntod: Optional[int] = None,
+    tods: Optional[str] = None,
+    nset: Optional[int] = None,
+    wafer: Optional[str] = None,
+    freq: Optional[str] = None,
+    dec_ref: Optional[float] = -40.0,
+    center_at: Optional[str] = None,
+    max_dets: Optional[int] = None,
+    fixed_time: Optional[int] = None,
+    min_dur: Optional[int] = None,
+    verbose: Optional[int] = 0,
+    quiet: Optional[int] = 0,
+    window: Optional[float] = None,
+    dtype_tod: Optional[str] = 'float32',
+    dtype_map: Optional[str] = 'float64'):
+
     # Set up logging.
     L = logging.getLogger(__name__)
     L.setLevel(logging.INFO)
@@ -237,57 +261,41 @@ def main(config_file=None, defaults=defaults, **args):
     ch.addFilter(LogInfoFilter())
     L.addHandler(ch)
 
-    cfg = dict(defaults)
-    # Update the default dict with values provided from a config.yaml file
-    if config_file is not None:
-        cfg_from_file = _get_config(config_file)
-        cfg.update({k: v for k, v in cfg_from_file.items() if v is not None})
-    else:
-        L.error("No config file provided, assuming default values")
-    # Merge flags from config file and defaults with any passed through CLI
-    cfg.update({k: v for k, v in args.items() if v is not None})
-    # Certain fields are required. Check if they are all supplied here
-    required_fields = ['context', 'preprocess_config']
-    for req in required_fields:
-        if req not in cfg.keys():
-            raise KeyError("{} is a required argument. Please supply it in a\
-            config file or via the command line".format(req))
-    args = cfg
     warnings.simplefilter('ignore')
     comm = mpi.FAKE_WORLD  # Fake communicator since we won't use MPI
-    verbose = args['verbose'] - args['quiet']
-    if args['area'] is not None:
-        shape, wcs = enmap.read_map_geometry(args['area'])
+    verbose = verbose - quiet
+    if area is not None:
+        shape, wcs = enmap.read_map_geometry(area)
         wcs = wcsutils.WCS(wcs.to_header())
-    elif args['nside'] is not None:
+    elif nside is not None:
         pass  # here I will map in healpix
     else:
         L.error('Neither rectangular area or nside specified, exiting.')
         exit(1)
 
     noise_model = mapmaking.NmatWhite()
-    putils.mkdir(args['odir'])
+    putils.mkdir(odir)
 
     recenter = None
-    if args['center_at']:
-        recenter = mapmaking.parse_recentering(args['center_at'])
-    preprocess_config = yaml.safe_load(open(args['preprocess_config'], 'r'))
+    if center_at:
+        recenter = mapmaking.parse_recentering(center_at)
+    preprocess_config = yaml.safe_load(open(preprocess_config, 'r'))
     errlog = os.path.join(os.path.dirname(
         preprocess_config['archive']['index']), 'errlog.txt')
 
     multiprocessing.set_start_method('spawn')
-    if (args['update_delay'] is not None):
-        min_ctime = int(time.time()) - args['update_delay']*86400
-        args['query'] += f" and timestamp>={min_ctime}"
+    if (update_delay is not None):
+        min_ctime = int(time.time()) - update_delay*86400
+        query += f" and timestamp>={min_ctime}"
 
-    context = Context(args['context'])
+    context_obj = Context(context)
     # obslists is a dict, obskeys is a list, periods is an array, only rank 0
     # will do this and broadcast to others.
     try:
         obslists, obskeys, periods, obs_infos = mapmaking.build_obslists(
-            context, args['query'], nset=args['nset'], wafer=args['wafer'],
-            freq=args['freq'], ntod=args['ntod'], tods=args['tods'],
-            fixed_time=args['fixed_time'], mindur=args['min_dur'])
+            context_obj, query, nset=nset, wafer=wafer,
+            freq=freq, ntod=ntod, tods=tods,
+            fixed_time=fixed_time, mindur=min_dur)
     except mapmaking.NoTODFound as err:
         L.exception(err)
         exit(1)
@@ -295,16 +303,16 @@ def main(config_file=None, defaults=defaults, **args):
     cwd = os.getcwd()
 
     split_labels = []
-    if args['det_in_out']:
+    if det_in_out:
         split_labels.append('det_in')
         split_labels.append('det_out')
-    if args['det_left_right']:
+    if det_left_right:
         split_labels.append('det_left')
         split_labels.append('det_right')
-    if args['det_upper_lower']:
+    if det_upper_lower:
         split_labels.append('det_upper')
         split_labels.append('det_lower')
-    if args['scan_left_right']:
+    if scan_left_right:
         split_labels.append('scan_left')
         split_labels.append('scan_right')
     if not split_labels:
@@ -312,9 +320,9 @@ def main(config_file=None, defaults=defaults, **args):
 
     # We open the data base for checking if we have maps already,
     # if we do we will not run them again.
-    if os.path.isfile(args['atomic_db']) and not args['only_hits']:
+    if os.path.isfile(atomic_db) and not only_hits:
         # open the connector, in reading mode only
-        conn = sqlite3.connect('./'+args['atomic_db'])
+        conn = sqlite3.connect('./'+atomic_db)
         cursor = conn.cursor()
         keys_to_remove = []
         # Now we have obslists and splits ready, we look through the database
@@ -346,11 +354,12 @@ def main(config_file=None, defaults=defaults, **args):
     obslists_arr = [item for key, item in obslists.items()]
     tod_list = []  # this list will receive the outputs from read_tods
     L.info('Starting with read_tods')
-    with ProcessPoolExecutor(args['nproc']) as exe:
+
+    with ProcessPoolExecutor(nproc) as exe:
         futures = [exe.submit(
-            read_tods, args['context'], obslist, dtype_tod=args['dtype_tod'],
-            only_hits=args['only_hits'], l2_data=args['hk_data_path'],
-            site=args['site'], dec_ref=args['dec_ref'])
+            read_tods, context, obslist, dtype_tod=dtype_tod,
+            only_hits=only_hits, l2_data=hk_data_path,
+            site=site, dec_ref=dec_ref)
                    for obslist in obslists_arr]
         for future in as_completed(futures):
             try:
@@ -363,7 +372,7 @@ def main(config_file=None, defaults=defaults, **args):
     L.info('Done with read_tods')
 
     my_tods = [bb.my_tods for bb in tod_list]
-    if args['area'] is not None:
+    if area is not None:
         subgeoms = []
         for obs in my_tods:
             if recenter is None:
@@ -383,8 +392,8 @@ def main(config_file=None, defaults=defaults, **args):
         t = putils.floor(periods[pid, 0])
         t5 = ("%05d" % t)[:5]
         prefix = "%s/%s/atomic_%010d_%s_%s" % (
-            args['odir'], t5, t, detset, band)
-        if args['area'] is not None:
+            odir, t5, t, detset, band)
+        if area is not None:
             subshape, subwcs = subgeoms[oi]
 
         tag = "%5d/%d" % (oi+1, len(obskeys))
@@ -394,7 +403,7 @@ def main(config_file=None, defaults=defaults, **args):
         # We will write an individual file,
         # another script will loop over those files
         # and write into sqlite data base
-        if not args['only_hits']:
+        if not only_hits:
             info = []
             for split_label in split_labels:
                 info.append(bunch.Bunch(
@@ -412,25 +421,26 @@ def main(config_file=None, defaults=defaults, **args):
                     azimuth=obs_infos[obslist[0][3]].az_center,
                     pwv=float(pwv_atomic)))
         # inputs that are unique per atomic map go into run_list
-        if args['area'] is not None:
+        if area is not None:
             run_list.append([obslist, subshape, subwcs, info, prefix, t])
-        elif args['nside'] is not None:
+        elif nside is not None:
             run_list.append([obslist, None, None, info, prefix, t])
     # Done with creating run_list
-    with ProcessPoolExecutor(args['nproc']) as exe:
+
+    with ProcessPoolExecutor(nproc) as exe:
         futures = [exe.submit(
-            mapmaking.make_demod_map, args['context'], r[0],
+            mapmaking.make_demod_map, context, r[0],
             noise_model, r[3], preprocess_config, r[4],
-            shape=r[1], wcs=r[2], nside=args['nside'],
+            shape=r[1], wcs=r[2], nside=nside,
             comm=comm, t0=r[5], tag=tag,
             recenter=recenter,
-            dtype_map=args['dtype_map'],
-            dtype_tod=args['dtype_tod'],
-            comps=args['comps'],
+            dtype_map=dtype_map,
+            dtype_tod=dtype_tod,
+            comps=comps,
             verbose=verbose,
             split_labels=split_labels,
-            singlestream=args['singlestream'],
-            site=args['site']) for r in run_list]
+            singlestream=singlestream,
+            site=site) for r in run_list]
         for future in as_completed(futures):
             L.info('New future as_completed result')
             try:
