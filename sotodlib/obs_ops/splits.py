bba246346374d12069f7b3e01cb74bdd7f6032e8
Max Silva-Feaver <maximiliano.silva-feaver@yale.edu>
2024-12-12 17:03:21 -0800

    make preproc splits optional.

--- sotodlib/obs_ops/splits.py
+++ sotodlib/obs_ops/splits.py
@@ -86,20 +86,10 @@ def get_split_flags(aman, proc_aman=None, split_cfg=None):
         ``<split_name>_threshold`` fields contain the threshold used for the split.
         Other fields conatain info for obs-level splits.
     '''
-    if proc_aman is None:
-        try:
-            proc_aman = aman.preprocess
-        except:
-            raise ValueError('proc_aman is None and no preprocess field in aman provide valid preprocess metadata')
-
-    if (not 't2p' in proc_aman) | (not 'hwpss_stats' in proc_aman):
-        raise ValueError('t2p or hwpss_stats not in proc_aman must run after those steps in the pipeline.')
-
     # Set default set of splits
     default_cfg = {'high_gain': 0.115, 'high_noise': 3.5e-5, 'high_tau': 1.5e-3,
-                   'det_A': 'A', 'pol_angle': 35, 'det_top': 'B', 'high_leakage': 1e-3,
-                   'high_2f': 1.5e-3, 'right_focal_plane': 0, 'top_focal_plane': 0,
-                   'central_pixels': 0.071 }
+                   'det_A': 'A', 'pol_angle': 35, 'det_top': 'B', 'right_focal_plane': 0,
+                   'top_focal_plane': 0, 'central_pixels': 0.071 }
     if split_cfg is None:
         split_cfg = default_cfg
 
@@ -109,7 +99,6 @@ def get_split_flags(aman, proc_aman=None, split_cfg=None):
     for k in default_cfg.keys():
         if not k in split_cfg:
             split_cfg[k] = default_cfg[k]
-        split_aman.wrap(f'{k}_threshold', split_cfg[k])
 
     # Gain split
     fm.wrap_dets('high_gain', aman.det_cal.phase_to_pW > split_cfg['high_gain'])
@@ -132,16 +121,6 @@ def get_split_flags(aman, proc_aman=None, split_cfg=None):
     # det top/bottom split
     fm.wrap_dets('det_type_top', aman.det_info.wafer.crossover > split_cfg['det_top'])
     fm.wrap_dets('det_type_bottom', aman.det_info.wafer.crossover <= split_cfg['det_top'])
-    # T2P Leakage split
-    fm.wrap_dets('high_leakage', np.sqrt(proc_aman.t2p.lamQ**2 + proc_aman.t2p.lamU**2) > split_cfg['high_leakage'])
-    fm.wrap_dets('low_leakage', np.sqrt(proc_aman.t2p.lamQ**2 + proc_aman.t2p.lamU**2) <= split_cfg['high_leakage'])
-    split_aman.wrap('leakage_avg', np.nanmean(np.sqrt(proc_aman.t2p.lamQ**2 + proc_aman.t2p.lamU**2)),
-                    [(0, 'dets')])
-    # High 2f amplitude split
-    a2 = aman.det_cal.phase_to_pW*np.sqrt(proc_aman.hwpss_stats.coeffs[:,2]**2 + proc_aman.hwpss_stats.coeffs[:,3]**2)
-    fm.wrap_dets('high_2f', a2 > split_cfg['high_2f'])
-    fm.wrap_dets('low_2f', a2 <= split_cfg['high_2f'])
-    split_aman.wrap('2f_avg', np.nanmean(a2), [(0, 'dets')])
     # Right/left focal plane split
     fm.wrap_dets('det_right', aman.focal_plane.xi > split_cfg['right_focal_plane'])
     fm.wrap_dets('det_left', aman.focal_plane.xi <= split_cfg['right_focal_plane'])
@@ -152,10 +131,41 @@ def get_split_flags(aman, proc_aman=None, split_cfg=None):
     r =  np.sqrt(aman.focal_plane.xi**2 + aman.focal_plane.eta**2)
     fm.wrap_dets('det_in', r < split_cfg['central_pixels'])
     fm.wrap_dets('det_out', r >= split_cfg['central_pixels'])
+
+    # Preproc dependent splits
+    if proc_aman is None:
+        try:
+            proc_aman = aman.preprocess
+        except:
+            print('Preprocess information not present, cannot generate preprocess dependent splits.')
+            for k in split_cfg.keys():
+                split_aman.wrap(f'{k}_threshold', split_cfg[k])
+            split_aman.wrap('cuts', fm)
+            return split_aman
+
+    if 't2p' in proc_aman:
+        # T2P Leakage split
+        if not 'high_leakage' in split_cfg:
+            split_cfg['high_leakage'] = 1e-3
+        fm.wrap_dets('high_leakage', np.sqrt(proc_aman.t2p.lamQ**2 + proc_aman.t2p.lamU**2) > split_cfg['high_leakage'])
+        fm.wrap_dets('low_leakage', np.sqrt(proc_aman.t2p.lamQ**2 + proc_aman.t2p.lamU**2) <= split_cfg['high_leakage'])
+        split_aman.wrap('leakage_avg', np.nanmean(np.sqrt(proc_aman.t2p.lamQ**2 + proc_aman.t2p.lamU**2)),
+                    [(0, 'dets')])
+    if 'hwpss_stats' in proc_aman:
+        # High 2f amplitude split
+        if not 'high_2f' in split_cfg:
+            split_cfg['high_2f'] = 1.5e-3
+        a2 = aman.det_cal.phase_to_pW*np.sqrt(proc_aman.hwpss_stats.coeffs[:,2]**2 + proc_aman.hwpss_stats.coeffs[:,3]**2)
+        fm.wrap_dets('high_2f', a2 > split_cfg['high_2f'])
+        fm.wrap_dets('low_2f', a2 <= split_cfg['high_2f'])
+        split_aman.wrap('2f_avg', np.nanmean(a2), [(0, 'dets')])
     # Left/right subscans
     if 'turnaround_flags' in proc_aman:
         fm.wrap('scan_left', proc_aman.turnaround_flags.left_scan)
         fm.wrap('scan_right', proc_aman.turnaround_flags.right_scan)
+
+    for k in split_cfg.keys():
+        split_aman.wrap(f'{k}_threshold', split_cfg[k])
 
     split_aman.wrap('cuts', fm)
 
