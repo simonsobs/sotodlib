6f50de8ecb6599b1e18aef7d704a625d5547a7d2
Sigurd Kirkevold Næss <sigurdkn@astro.uio.no>
2025-01-10 08:15:49 -0800

    Fix variable name

--- sotodlib/coords/helpers.py
+++ sotodlib/coords/helpers.py
@@ -348,9 +348,9 @@ def get_footprint(tod, wcs_kernel, dets=None, timestamps=None, boresight=None,
         focal_plane=fp0, count=16)
     fp1 = so3g.proj.FocalPlane.from_xieta(xieta1[0], xieta1[1])
 
-    asm = so3g.proj.Assembly.attach(sight, fp1)
-    output = np.zeros((xieta1.shape[1], n_samp, 4))
-    proj = so3g.proj.Projectionist.for_geom((1,1), wcs_kernel)
+    asm    = so3g.proj.Assembly.attach(sight, fp1)
+    planar = np.zeros((xieta1.shape[1], n_samp, 4))
+    proj   = so3g.proj.Projectionist.for_geom((1,1), wcs_kernel)
     if rot:
         # Works whether rot is a quat or a vector of them.
         asm.Q = rot * asm.Q
