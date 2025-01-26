e73d63856747d8e0cd88b6fc0479194e99e53d6a
Ted Kisner <tskisner.public@gmail.com>
2025-01-26 13:43:22 -0800

    Avoid numpy fancy indexing, which introduces a copy.

--- sotodlib/toast/io/save_book.py
+++ sotodlib/toast/io/save_book.py
@@ -411,8 +411,11 @@ def export_obs_data(
 
         # Fill the data
         for iob, ob in enumerate(wafer_obs):
-            dts_temp[wafer_indices[iob], :] = ob.detdata[det_data][:, slc] * uscale
-            fts_temp[wafer_indices[iob], :] = ob.detdata[det_flags][:, slc]
+            for idet, det in enumerate(det_names):
+                dts_temp[wafer_indices[iob][idet], :] = ob.detdata[det_data][
+                    det, slc
+                ] * uscale
+                fts_temp[wafer_indices[iob][idet], :] = ob.detdata[det_flags][det, slc]
 
         dts.data = dts_temp
         # dts.options(enable=1)
