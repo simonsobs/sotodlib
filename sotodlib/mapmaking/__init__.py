22eb4cdd287a041b116f8873acc5684eb01b8b57
Carlos <aconcagua.chc@gmail.com>
2025-04-07 15:26:37 -0400

    I forgot to add the base __init__

--- sotodlib/mapmaking/__init__.py
+++ sotodlib/mapmaking/__init__.py
@@ -1,6 +1,5 @@
-from .utils import *
-from .ml_mapmaker import *
-from .demod_mapmaker import *
+from .utils import utils
+from .mapmakers import ml_mapmaker, demod_mapmaker
 from .noise_model import *
 from .pointing_matrix import *
 from .obs_grouping import *
