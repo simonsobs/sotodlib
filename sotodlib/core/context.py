8a52c3184fef1819a65b66f186398287de5c6cd5
Carlos <aconcagua.chc@gmail.com>
2025-06-11 12:06:14 -0400

    improvement to context manager

--- sotodlib/core/context.py
+++ sotodlib/core/context.py
@@ -550,8 +550,7 @@ def _read_cfg(filename=None, envvar=None, default=None):
         return False, filename, odict()
     with open(filename, 'r') as file:
         yaml_file = yaml.safe_load(file)
-        file.close()
-        return True, filename, yaml_file
+    return True, filename, yaml_file
 
 
 def obsloader_template(db, obs_id, dets=None, prefix=None, samples=None,
