c6b7f8db09c8dadde22fb7959a67cccad8a50842
ykyohei <38639108+ykyohei@users.noreply.github.com>
2025-07-14 16:10:44 -0400

    refactor

--- sotodlib/site_pipeline/make_hwp_solutions.py
+++ sotodlib/site_pipeline/make_hwp_solutions.py
@@ -11,6 +11,7 @@ import logging
 import yaml
 import traceback
 import sqlite3
+from typing import Optional, Union, Callable
 
 from sotodlib import core
 from sotodlib.hwp.g3thwp import G3tHWP
@@ -95,74 +96,6 @@ def get_parser(parser=None):
     return parser
 
 
-def setup_dir(args):
-    if args.verbose == 0:
-        logger.setLevel(logging.ERROR)
-    elif args.verbose == 1:
-        logger.setLevel(logging.WARNING)
-    elif args.verbose == 2:
-        logger.setLevel(logging.INFO)
-    elif args.verbose == 3:
-        logger.setLevel(logging.DEBUG)
-
-    configs = yaml.safe_load(open(args.HWPconfig, "r"))
-    logger.info("Starting make_hwp_solutions")
-
-    # Specify output directory
-    if args.solution_output_dir is None:
-        args.solution_output_dir = configs["solution_output_dir"]
-    if args.encoder_output_dir is None:
-        args.encoder_output_dir = configs["encoder_output_dir"]
-
-    if not os.path.exists(args.solution_output_dir):
-        logger.info(f"Making output directory {args.solution_output_dir}")
-        os.mkdir(args.solution_output_dir)
-    if not os.path.exists(args.encoder_output_dir):
-        logger.info(f"Making output directory {args.encoder_output_dir}")
-        os.mkdir(args.encoder_output_dir)
-
-
-def get_obs_to_run(args):
-    """ Return list of obs_id to run
-    """
-    ctx = core.Context(args.context)
-
-    # load observation data
-    if args.obs_id is not None:
-        tot_query = ' or '.join([f"(obs_id=='{o}')" for o in args.obs_id])
-    else:
-        tot_query = "and "
-        if args.min_ctime is not None:
-            tot_query += f"start_time>={args.min_ctime} and "
-        if args.max_ctime is not None:
-            tot_query += f"start_time<={args.max_ctime} and "
-        if args.query is not None:
-            tot_query += args.query + " and "
-        tot_query = tot_query[4:-4]
-        if tot_query == "":
-            tot_query = "1"
-
-    logger.debug(f"sending query to obsdb: {tot_query}")
-    obs_list = ctx.obsdb.query(tot_query)
-    obs_list = [obs['obs_id'] for obs in obs_list]
-    run_list = []
-
-    if len(obs_list) == 0:
-        logger.warning(f"no observations returned from query: {args.query}")
-        return []
-
-    if args.overwrite:
-        return obs_list
-
-    db_solution = make_db(os.path.join(args.solution_output_dir, 'hwp_angle.sqlite'))
-    completed = db_solution.get_entries(['dataset'])['dataset']
-
-    for obs_id in obs_list:
-        if obs_id not in completed:
-            run_list.append(obs_id)
-    return run_list
-
-
 def make_db(db_filename):
     """
     Make sqlite database
@@ -237,7 +170,8 @@ def save(aman, db, h5_filename, output_dir, obs_id, overwrite, compression, enco
     return True
 
 
-def get_solution(obs_id, encoder_completed, args):
+def get_solution(obs_id, encoder_completed, context, HWPconfig,
+                 encoder_output_dir, off_site=False):
     """ calculate hwp angle solution.
     Load encoder L3 data if it exists.
     If this is run off site, load L2 data, otherwise skip it.
@@ -257,9 +191,9 @@ def get_solution(obs_id, encoder_completed, args):
     logger.info(f"Calculating Angles for {obs_id}")
     # split h5 file by first 5 digits of unixtime
     unix = obs_id.split('_')[1][:5]
-    h5_encoder = os.path.join(args.encoder_output_dir, f'hwp_encoder_{unix}.h5')
-    ctx = core.Context(args.context)
-    g3thwp = G3tHWP(args.HWPconfig)
+    h5_encoder = os.path.join(encoder_output_dir, f'hwp_encoder_{unix}.h5')
+    ctx = core.Context(context)
+    g3thwp = G3tHWP(HWPconfig)
 
     # make angle solutions
     tod = ctx.get_obs(obs_id, dets=[])
@@ -268,7 +202,7 @@ def get_solution(obs_id, encoder_completed, args):
         g3thwp.set_data(tod, h5_filename=h5_encoder)
         aman_encoder = None
     # Do nothing if L3 data does not exists off-site
-    elif args.off_site:
+    elif off_site:
         logger.info(f"Skip {obs_id} beause L3 data do not exist off site")
         return obs_id, None, None
     # Load L2 data at site
@@ -279,18 +213,91 @@ def get_solution(obs_id, encoder_completed, args):
     return obs_id, aman_encoder, aman_solution
 
 
-def _main(executor, as_completed_callable, args):
+def _main(
+    executor: Union["MPICommExecutor", "ProcessPoolExecutor"],
+    as_completed_callable: Callable,
+    context: str,
+    HWPconfig: str,
+    solution_output_dir: Optional[str] = None,
+    encoder_output_dir: Optional[str] = None,
+    verbose: Optional[int] = 2,
+    overwrite: Optional[bool] = False,
+    query: Optional[str] = None,
+    min_ctime: Optional[float] = None,
+    max_ctime: Optional[float] = None,
+    obs_id: Optional[str] = None,
+    off_site: Optional[bool] = False,
+    nprocs: Optional[int] = 1,
+):
 
-    setup_dir(args)
-    run_list = get_obs_to_run(args)
-    db_solution = make_db(os.path.join(args.solution_output_dir, 'hwp_angle.sqlite'))
-    db_encoder = make_db(os.path.join(args.encoder_output_dir, 'hwp_encoder.sqlite'))
+    logger.info(f"Using context {context} and HWPconfig {HWPconfig}")
+
+    configs = yaml.safe_load(open(HWPconfig, "r"))
+    logger.info("Starting make_hwp_solutions")
+
+    # Specify output directory
+    if solution_output_dir is None:
+        solution_output_dir = configs["solution_output_dir"]
+    if encoder_output_dir is None:
+        encoder_output_dir = configs["encoder_output_dir"]
+
+    if not os.path.exists(solution_output_dir):
+        logger.info(f"Making output directory {solution_output_dir}")
+        os.mkdir(solution_output_dir)
+    if not os.path.exists(encoder_output_dir):
+        logger.info(f"Making output directory {encoder_output_dir}")
+        os.mkdir(encoder_output_dir)
+
+    # Set verbose
+    if verbose == 0:
+        logger.setLevel(logging.ERROR)
+    elif verbose == 1:
+        logger.setLevel(logging.WARNING)
+    elif verbose == 2:
+        logger.setLevel(logging.INFO)
+    elif verbose == 3:
+        logger.setLevel(logging.DEBUG)
+
+    db_solution = make_db(os.path.join(solution_output_dir, 'hwp_angle.sqlite'))
+    db_encoder = make_db(os.path.join(encoder_output_dir, 'hwp_encoder.sqlite'))
     encoder_completed = db_encoder.get_entries(['dataset'])['dataset']
+
+    # load observation data
+    if obs_id is not None:
+        tot_query = ' or '.join([f"(obs_id=='{o}')" for o in obs_id])
+    else:
+        tot_query = "and "
+        if min_ctime is not None:
+            tot_query += f"start_time>={min_ctime} and "
+        if max_ctime is not None:
+            tot_query += f"start_time<={max_ctime} and "
+        if query is not None:
+            tot_query += query + " and "
+        tot_query = tot_query[4:-4]
+        if tot_query == "":
+            tot_query = "1"
+
+    logger.debug(f"Sending query to obsdb: {tot_query}")
+    ctx = core.Context(context)
+    obs_list = ctx.obsdb.query(tot_query)
+    obs_list = [obs['obs_id'] for obs in obs_list]
+    run_list = []
+
+    if len(obs_list) == 0:
+        logger.warning(f"No observations returned from query: {query}")
+
+    completed = db_solution.get_entries(['dataset'])['dataset']
+
+    for obs_id in obs_list:
+        if overwrite or obs_id not in completed:
+            run_list.append(obs_id)
 
     # write solutions
     futures = []
     for obs_id in run_list:
-        futures.append(executor.submit(get_solution, obs_id, encoder_completed, args))
+        futures.append(executor.submit(get_solution, obs_id, encoder_completed,
+                                       context, HWPconfig, encoder_output_dir,
+                                       off_site))
 
     for future in as_completed_callable(futures):
         try:
@@ -302,15 +309,15 @@ def _main(executor, as_completed_callable, args):
             if aman_encoder is not None:
                 logger.info(f"Saving hwp_encoder: {obs_id}")
                 success = save(aman_encoder, db_encoder, h5_encoder,
-                               args.encoder_output_dir,
-                               obs_id, args.overwrite, 'gzip')
+                               encoder_output_dir,
+                               obs_id, overwrite, 'gzip')
                 if not success:
                     logger.error(f"Failed to save hwp_encoder: {obs_id}, skip saving hwp_angle")
                     continue
             if aman_solution is not None:
                 logger.info(f"Saving hwp_angle: {obs_id}")
                 success = save(aman_solution, db_solution, h5_solution,
-                               args.solution_output_dir, obs_id, args.overwrite,
+                               solution_output_dir, obs_id, overwrite,
                                compression='gzip', encodings=encodings)
                 if not success:
                     logger.error(f"Failed to save hwp_angle: {obs_id}")
@@ -322,7 +329,7 @@ def _main(executor, as_completed_callable, args):
 def main(args):
     rank, executor, as_completed_callable = get_exec_env(nprocs=args.nprocs)
     if rank == 0:
-        _main(executor, as_completed_callable, args)
+        _main(executor, as_completed_callable, **vars(args))
 
 
 if __name__ == '__main__':
