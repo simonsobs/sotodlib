95d3e65a3f1ffab50c3c12d2e1dd24fad6598aea
Carlos <aconcagua.chc@gmail.com>
2025-07-30 18:46:26 -0400

    fixing the test demod_mapmaker so that it has a fake wobble_params

--- tests/test_demod_mapmaker.py
+++ tests/test_demod_mapmaker.py
@@ -66,25 +66,29 @@ def unweight_map(rhs, div):
     return rhs * idiv
 
 def get_tod(Q_stream, U_stream):
-        tod = quick_tod(10, 10000)
-        tod.wrap('flags', core.FlagManager.for_tod(tod))
-        tod.wrap('weather', np.full(1, 'toco'))
-        tod.wrap('site', np.full(1, 'so_sat1'))
-        tod.flags.wrap('glitch_flags', so3g.proj.RangesMatrix.zeros(tod.shape), [(0, 'dets'), (1, 'samps')])
+    tod = quick_tod(10, 10000)
+    tod.wrap('flags', core.FlagManager.for_tod(tod))
+    tod.wrap('weather', np.full(1, 'toco'))
+    tod.wrap('site', np.full(1, 'so_sat1'))
+    tod.flags.wrap('glitch_flags', so3g.proj.RangesMatrix.zeros(tod.shape), [(0, 'dets'), (1, 'samps')])
+    wobble_params = core.AxisManager(core.IndexAxis('dets'))
+    for k in ['amp','phase']:
+        wobble_params.wrap(k, np.zeros(tod.shape[0]), [(0,'dets')])
+    tod.wrap('wobble_params', wobble_params)
 
-        fp = so3g.proj.FocalPlane.from_xieta(
-            tod.dets.vals, tod.focal_plane.xi, tod.focal_plane.eta, tod.focal_plane.gamma)
-        csl = so3g.proj.CelestialSightLine.az_el(
-            tod.timestamps, tod.boresight.az, tod.boresight.el, roll=tod.boresight.roll,
-            site='so_sat1', weather='toco')
+    fp = so3g.proj.FocalPlane.from_xieta(
+        tod.dets.vals, tod.focal_plane.xi, tod.focal_plane.eta, tod.focal_plane.gamma)
+    csl = so3g.proj.CelestialSightLine.az_el(
+        tod.timestamps, tod.boresight.az, tod.boresight.el, roll=tod.boresight.roll,
+        site='so_sat1', weather='toco')
 
-        for k in ['dsT', 'demodQ', 'demodU']:
-            tod.wrap_new(k, shape=('dets', 'samps'), dtype='float32')
+    for k in ['dsT', 'demodQ', 'demodU']:
+        tod.wrap_new(k, shape=('dets', 'samps'), dtype='float32')
 
-        for i, det in enumerate(tod.dets.vals):
-            q_total = csl.Q * fp[det]
-            ra, dec, alpha = so3g.proj.quat.decompose_lonlat(q_total)
-            GAMMA = alpha - 2 * tod.focal_plane.gamma[i]
-            tod.demodQ[i] = Q_stream * np.cos(2 * GAMMA) + U_stream * np.sin(2 * GAMMA)
-            tod.demodU[i] = U_stream * np.cos(2 * GAMMA) - Q_stream * np.sin(2 * GAMMA)
-        return tod
+    for i, det in enumerate(tod.dets.vals):
+        q_total = csl.Q * fp[det]
+        ra, dec, alpha = so3g.proj.quat.decompose_lonlat(q_total)
+        GAMMA = alpha - 2 * tod.focal_plane.gamma[i]
+        tod.demodQ[i] = Q_stream * np.cos(2 * GAMMA) + U_stream * np.sin(2 * GAMMA)
+        tod.demodU[i] = U_stream * np.cos(2 * GAMMA) - Q_stream * np.sin(2 * GAMMA)
+    return tod
