77898a5456287144344c52eaf8b647f00232ecad
John Orlowski-Scherer <jackorlowskischerer@gmail.com>
2025-08-12 15:38:09 -0400

    feat: init:

---
+++ sotodlib/mapcat/mapcat/__init__.py
@@ -0,0 +1,6 @@
+"""
+A python client for accessing data in mapcat. You can make requests to the service and
+recieve answers as a list of pydantic models.
+
+Also includes mocking functionality for downstream services to use in their tests.
+"""
