6416b9ba4c176b320101b6b9505282a5dd0a21ca
John Orlowski-Scherer <jackorlowskischerer@gmail.com>
2025-08-12 15:59:29 -0400

    fix: change arrays to wafers

--- sotodlib/mapcat/alembic/versions/78293df04081_create_depth_1_map_table.py
+++ sotodlib/mapcat/alembic/versions/78293df04081_create_depth_1_map_table.py
@@ -27,7 +27,7 @@ def upgrade() -> None:
         sa.Column("map_name", sa.String, nullable=False),
         sa.Column("map_path", sa.String, nullable=False),
         sa.Column("tube_slot", sa.String, nullable=False),
-        sa.Column("arrays", sa.Integer, nullable=False),
+        sa.Column("wafers", sa.Integer, nullable=False),
         sa.Column("frequency", sa.String, nullable=False),
         sa.Column(
             "ctime", sa.Float, nullable=False
