f7b3621dbd881925d4f51b0b070ca0337791436d
John Orlowski-Scherer <jackorlowskischerer@gmail.com>
2025-08-12 16:24:15 -0400

    feat: init database with Depth 1 map table

--- sotodlib/mapcat/mapcat/database.py
+++ sotodlib/mapcat/mapcat/database.py
@@ -10,3 +10,86 @@ from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_asyn
 from sqlmodel import JSON, Column, Field, SQLModel
 
 from .settings import settings
+
+
+class DepthOneMap(BaseModel):
+    """
+    A Depth 1 map.
+
+    Attributes
+    ----------
+    id : int
+        Unique source identifier. Internal to SO
+    map_name : str
+        Name of depth 1 map
+    map_path : str
+        Non-localized path to str
+    tube_slot : str
+        OT for map
+    wafers : str
+        Standardized names of wafers used in this map
+    frequency : str
+        Frequency channel of map
+    ctime :
+        Central unix time of map
+    """
+
+    id: int
+    map_name: str
+    map_path: str
+    tube_slot: str
+    wafers: int
+    frequency: str
+    ctime: float
+
+    def __repr__(self):
+        return f"DepthOneMap(id={self.id}, map_name={self.map_name}, map_path={self.map_path}, tube_slot={self.tube_slot}, wafers={self.wafers}, frequency={self.frequency}, ctime={self.ctime})"  # pragma: no cover
+
+
+class DepthOneMapTable(DepthOneMap, SQLModel, table=True):
+    """
+    A depth-1 map. This is the table model
+    providing SQLModel functionality. You can export a base model, for example
+    for responding to a query with using the `to_model` method.
+
+    Attributes
+    ----------
+    id : int
+        Unique source identifiers. Internal to SO
+    """
+
+    __tablename__ = "depth_one_maps"
+
+    id: int = Field(primary_key=True)
+    map_name: str = Field(index=True, nullable=False)
+    frequency: str = Field(index=True, nullable=False)
+
+    def to_model(self) -> DepthOneMap:
+        """
+        Return an Extragalactic source from table.
+
+        Returns
+        -------
+        ExtragalaticSource : DepthOneMap
+            Source corresponding to this id.
+        """
+        return DepthOneMap(
+            id=self.id,
+            map_name=self.map_name,
+            map_path=self.map_path,
+            tube_slot=self.tube_slot,
+            wafers=self.wafers,
+            frequency=self.frequency,
+            ctime=self.ctime,
+        )
+
+
+ALL_TABLES = [DepthOneMapTable]
+
+async_engine = create_async_engine(settings.database_url, echo=True, future=True)
+
+
+async def get_async_session() -> AsyncSession:
+    async_session = async_sessionmaker(bind=async_engine, expire_on_commit=False)
+    async with async_session() as session:
+        yield session
