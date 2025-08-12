49c88b5e9ea951c34d9e57f493188ee64ae9e7bb
John Orlowski-Scherer <jackorlowskischerer@gmail.com>
2025-08-12 15:24:08 -0400

    feat init tod tracking table

--- sotodlib/mapcat/alembic/versions/78293df04081_create_depth_1_map_table.py
+++ sotodlib/mapcat/alembic/versions/78293df04081_create_depth_1_map_table.py
@@ -24,9 +24,9 @@ def upgrade() -> None:
     op.create_table(
         "depth_one_maps",
         sa.Column("id", sa.Integer, primary_key=True),
-        sa.Column("name", sa.String, nullable=False),
+        sa.Column("map_name", sa.String, nullable=False),
         sa.Column("map_path", sa.String, nullable=False),
-        sa.Column("ot", sa.String, nullable=False),
+        sa.Column("tube_slot", sa.String, nullable=False),
         sa.Column("arrays", sa.Integer, nullable=False),
         sa.Column("frequency", sa.String, nullable=False),
         sa.Column(
@@ -37,7 +37,7 @@ def upgrade() -> None:
     op.create_table(
         "time_domain_processing",
         sa.Column(
-            "name",
+            "map_name",
             sa.String,
             sa.ForeignKey("depth_one_maps.name", nullable=False, primary_key=True),
         ),
@@ -50,7 +50,57 @@ def upgrade() -> None:
         sa.Column("processing_status", sa.String, nullable=False),
     )
 
+    op.create_table(
+        "depth_1_pointing_offsets",
+        sa.Column(
+            "map_name",
+            sa.String,
+            sa.ForeignKey("depth_one_maps.map_name", nullable=False, primary_key=True),
+        ),
+        sa.Column("ra_offset", sa.Float),
+        sa.Column("dec_offset", sa.Float),
+    )
+
+    op.create_table(
+        "tod_depth_one",
+        sa.Column("id", sa.Integer, nullable=False, primary_key=True),
+        sa.Column(
+            "map_name",
+            sa.String,
+            sa.ForeignKey("depth_one_maps.map_name"),
+        ),
+        sa.Column("pwv", sa.Float),
+        sa.Column(
+            "ctime", sa.Float, nullable=False
+        ),  # TODO: should this be sa.Datetime?
+        sa.Column(
+            "start_time", sa.Float, nullable=False
+        ),  # TODO: should this be sa.Datetime?
+        sa.Column(
+            "stop_time", sa.Float, nullable=False
+        ),  # TODO: should this be sa.Datetime?
+        sa.Column("nsamples", sa.Integer),
+        sa.Column("telescope", sa.String),
+        sa.Column("telescope_flavor", sa.String),
+        sa.Column("tube_slot", sa.String),
+        sa.Column("tube_flavor", sa.String),
+        sa.Column("frequency", sa.String),
+        sa.Column("type", sa.String),
+        sa.Column("subtype", sa.String),
+        sa.Column("wafer_count", sa.Integer),
+        sa.Column("duration", sa.Float),
+        sa.Column("az_center", sa.Float),
+        sa.Column("az_throw", sa.Float),
+        sa.Column("el_center", sa.Float),
+        sa.Column("el_throw", sa.Float),
+        sa.Column("roll_center", sa.Float),
+        sa.Column("roll_throw", sa.Float),
+        sa.Column("wafer_slots_list", sa.String),
+        sa.Column("stream_ids_list", sa.String),
+    )
+
 
 def downgrade() -> None:
     op.drop_table("depth_one_maps")
     op.drop_table("time_domain_processing")
+    op.drop_table("depth_1_pointing_offsets")
