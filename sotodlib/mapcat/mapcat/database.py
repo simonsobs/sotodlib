8c2928ca4835ae3f753021f8d702d0888f8bea4c
John Orlowski-Scherer <jackorlowskischerer@gmail.com>
2025-08-14 14:36:18 -0400

    feat: allow processing start and end to be None

--- sotodlib/mapcat/mapcat/database.py
+++ sotodlib/mapcat/mapcat/database.py
@@ -50,12 +50,25 @@ class DepthOneMapTable(DepthOneMap, SQLModel, table=True):
     """
     A depth-1 map. This is the table model
     providing SQLModel functionality. You can export a base model, for example
-    for responding to a query with using the `to_model` method.
+    for responding to a query with using the `to_model` method. Note some attributes
+    are inherited from DepthOneMap.
 
     Attributes
     ----------
     id : int
         Unique source identifiers. Internal to SO
+    map_name : str
+        Name of depth 1 map
+    map_path : str
+        Non-localized path to str
+    tube_slot : str
+        OT for map
+    wafers : str
+        Standardized names of wafers used in this map
+    frequency : str
+        Frequency channel of map
+    ctime : float
+        Central unix time of map
     """
 
     __tablename__ = "depth_one_maps"
@@ -86,7 +99,78 @@ class DepthOneMapTable(DepthOneMap, SQLModel, table=True):
         )
 
 
-ALL_TABLES = [DepthOneMapTable]
+class ProcessingStatus(BaseModel):
+    """
+    Processing status of a depth-1 map
+
+    Attributes
+    ----------
+    map_name : str
+        Name of depth 1 map being tracked. Primary Key, foreign into DepthOneMap
+    processing_start : float
+        Time processing started.
+    processing_end : float
+        Time processing Eneded
+    processing_status : str
+        Status of processing
+    """
+
+    map_name: str
+    processing_start: float | None
+    processing_end: float | None
+    processing_status: str
+
+    def __repr__(self):
+        # TODO: This should probably change based on process status once we nail that down
+        return f"ProcessingStatus(map_name={self.map_name}, processing_start={self.processing_start}, processing_end={self.processing_end}, processing_status={self.processing_status})"  # pragma: no cover
+
+
+class ProcessingStatusTable(ProcessingStatus, SQLModel, table=True):
+    """
+    Table for tracking processing status of depth-1 maps
+    providing SQLModel functionality. You can export a base model, for example
+    for responding to a query with using the `to_model` method. Note some attributes
+    are inherited from ProcessingStatus.
+
+    Attributes
+    ----------
+    map_name : str
+        Name of depth 1 map being tracked. Primary Key, foreign into DepthOneMap
+    processing_start : float | None
+        Time processing started. None if not started.
+    processing_end : float | None
+        Time processing ended. None if not ended.
+    processing_status : str
+        Status of processing
+    """
+
+    __tablename__ = "processing_status"
+
+    map_name: str = Field(
+        index=True, nullable=False, foreign_key="depth_one_maps.map_name"
+    )
+    processing_start: float = Field(nullable=True)
+    processing_end: float = Field(nullable=True)
+    processing_status: str = Field(index=True, nullable=False)
+
+    def to_model(self) -> ProcessingStatus:
+        """
+        Return an Extragalactic source from table.
+
+        Returns
+        -------
+        ProcessingStatus : ProcessingStatus
+            Processing status corresponding to this depth-1 map
+        """
+        return ProcessingStatus(
+            map_name=self.map_name,
+            processing_start=self.processing_start,
+            processing_end=self.processing_end,
+            processing_status=self.cProcessingStatus,
+        )
+
+
+ALL_TABLES = [DepthOneMapTable, ProcessingStatusTable]
 
 async_engine = create_async_engine(settings.database_url, echo=True, future=True)
 
