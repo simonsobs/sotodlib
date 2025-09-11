d59c08bc61e9d60329687b8df690154af1eefb4e
Michael McCrackan <mmccrack@login28.chn.perlmutter.nersc.gov>
2025-09-11 14:46:52 -0700

    add a valid range matrix to proc_aman

--- sotodlib/preprocess/preprocess_util.py
+++ sotodlib/preprocess/preprocess_util.py
@@ -11,6 +11,7 @@ import inspect
 from sotodlib.hwp import hwp_angle_model
 from sotodlib.coords import demod as demod_mm
 from sotodlib.tod_ops import t2pleakage
+from sotodlib.core.flagman import has_any_cuts
 
 from .. import core
 
@@ -318,15 +319,15 @@ def load_preprocess_det_select(obs_id, configs, context=None,
     configs, context = get_preprocess_context(configs, context)
     pipe = Pipeline(configs["process_pipe"], logger=logger)
 
-    meta = context.get_meta(obs_id, dets=dets, meta=meta)
+    if meta is None:
+        meta = context.get_meta(obs_id, dets=dets)
     logger.info("Restricting detectors on all processes")
     keep_all = np.ones(meta.dets.count,dtype=bool)
     for process in pipe[:]:
         keep = process.select(meta, in_place=False)
         if isinstance(keep, np.ndarray):
             keep_all &= keep
-    meta.restrict("dets", meta.dets.vals[keep_all])
-    return meta
+    return meta.dets.vals[keep_all]
 
 
 def load_and_preprocess(obs_id, configs, context=None, dets=None, meta=None,
@@ -363,8 +364,14 @@ def load_and_preprocess(obs_id, configs, context=None, dets=None, meta=None,
         logger = init_logger("preprocess")
 
     configs, context = get_preprocess_context(configs, context)
-    meta = load_preprocess_det_select(obs_id, configs=configs, context=context,
-                                      dets=dets, meta=meta, logger=logger)
+    meta = context.get_meta(obs_id, dets=dets, meta=meta)
+    if 'valid' in meta.preprocess:
+        keep = has_any_cuts(meta.preprocess.valid)
+        meta.restrict("dets", keep)
+    else:
+        det_vals = load_preprocess_det_select(obs_id, configs=configs, context=context,
+                                              dets=dets, meta=meta, logger=logger)
+        meta.restrict("dets", [d for d in meta.dets.vals if d in det_vals])
 
     if meta.dets.count == 0:
         logger.info(f"No detectors left after cuts in obs {obs_id}")
@@ -444,11 +451,14 @@ def multilayer_load_and_preprocess(obs_id, configs_init, configs_proc,
             pipe_proc = Pipeline(configs_proc["process_pipe"], logger=logger)
 
             logger.info("Restricting detectors on all proc pipeline processes")
-            keep_all = np.ones(meta_proc.dets.count, dtype=bool)
-            for process in pipe_proc[:]:
-                keep = process.select(meta_proc, in_place=False)
-                if isinstance(keep, np.ndarray):
-                    keep_all &= keep
+            if 'valid' in meta_proc.preprocess:
+                keep_all = has_any_cuts(meta_proc.preprocess.valid)
+            else:
+                keep_all = np.ones(meta_proc.dets.count, dtype=bool)
+                for process in pipe_proc[:]:
+                    keep = process.select(meta_proc, in_place=False)
+                    if isinstance(keep, np.ndarray):
+                        keep_all &= keep
             meta_proc.restrict("dets", meta_proc.dets.vals[keep_all])
             meta_init.restrict('dets', meta_proc.dets.vals)
 
@@ -461,8 +471,9 @@ def multilayer_load_and_preprocess(obs_id, configs_init, configs_proc,
             logger.info("Running dependent pipeline")
             proc_aman = context_proc.get_meta(obs_id, meta=aman)
 
+            if 'valid' in aman.preprocess:
+                aman.preprocess.move('valid', None)
             aman.preprocess.merge(proc_aman.preprocess)
-
             pipe_proc.run(aman, aman.preprocess, select=False)
 
             return aman
@@ -579,6 +590,8 @@ def multilayer_load_and_preprocess_sim(obs_id, configs_init, configs_proc,
 
             logger.info("Running dependent pipeline")
             proc_aman = context_proc.get_meta(obs_id, meta=aman)
+            if 'valid' in aman.preprocess:
+                aman.preprocess.move('valid', None)
             aman.preprocess.merge(proc_aman.preprocess)
             pipe_proc.run(aman, aman.preprocess, sim=True)
 
@@ -943,6 +956,7 @@ def preproc_or_load_group(obs_id, configs_init, dets, configs_proc=None,
                 try:
                     outputs_proc = save_group(obs_id, configs_proc, dets, context_proc, subdir='temp_proc')
                     init_fields = aman.preprocess._fields.copy()
+                    init_fields.pop('valid', None)
                     logger.info(f"Generating new dependent preproc db entry for {obs_id} {dets}")
                     # pipeline for init config
                     pipe_init = Pipeline(configs_init["process_pipe"], plot_dir=configs_init["plot_dir"], logger=logger)
@@ -979,6 +993,7 @@ def preproc_or_load_group(obs_id, configs_init, dets, configs_proc=None,
                 logger.info(f"Saving data to {outputs_proc['temp_file']}:{outputs_proc['db_data']['dataset']}")
                 proc_aman.save(outputs_proc['temp_file'], outputs_proc['db_data']['dataset'], overwrite)
 
+                aman.preprocess.move('valid', None)
                 aman.preprocess.merge(proc_aman)
 
                 return error, [obs_id, dets], outputs_proc, aman
@@ -1013,6 +1028,7 @@ def preproc_or_load_group(obs_id, configs_init, dets, configs_proc=None,
             try:
                 outputs_proc = save_group(obs_id, configs_proc, dets, context_proc, subdir='temp_proc')
                 init_fields = aman.preprocess._fields.copy()
+                init_fields.pop('valid', None)
                 logger.info(f"Generating new dependent preproc db entry for {obs_id} {dets}")
                 # pipeline for processing config
                 pipe_proc = Pipeline(configs_proc["process_pipe"], plot_dir=configs_proc["plot_dir"], logger=logger)
@@ -1027,7 +1043,7 @@ def preproc_or_load_group(obs_id, configs_init, dets, configs_proc=None,
 
                 # remove fields found in aman.preprocess from proc_aman
                 for fld_init in init_fields:
-                    if fld_init in proc_aman:
+                    if fld_init in proc_aman and fld_init:
                         proc_aman.move(fld_init, None)
 
                 proc_aman.wrap('pcfg_ref', aman_cfgs_ref)
@@ -1045,6 +1061,7 @@ def preproc_or_load_group(obs_id, configs_init, dets, configs_proc=None,
             logger.info(f"Saving data to {outputs_proc['temp_file']}:{outputs_proc['db_data']['dataset']}")
             proc_aman.save(outputs_proc['temp_file'], outputs_proc['db_data']['dataset'], overwrite)
 
+            aman.preprocess.move('valid', None)
             aman.preprocess.merge(proc_aman)
 
             return error, outputs_init, outputs_proc, aman
