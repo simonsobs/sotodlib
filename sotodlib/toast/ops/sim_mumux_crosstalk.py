Fixed factors of 2pi, dPhidT calculation

sotodlib/toast/ops/sim_mumux_crosstalk.py
@@ -81,6 +81,8 @@ P_SAT = {
 R_BOLO = 0.008
 # Readout noise fraction
 R_FRAC = 0.5
+# Shunt Resistance [Ohm]
+R_SHUNT = 400e-6
 
 
 @trait_docs
@@ -152,7 +154,7 @@ class SimMuMUXCrosstalk(Operator):
         output_signal = input_signal / dPhi0dT
         return output_signal
 
-    def _draw_Phi0(self, obs, focalplane, detectors, vmin=0.3, vmax=1.3):
+    def _draw_Phi0(self, obs, focalplane, detectors, vmin=0.0, vmax=1.0):
         """ Draw initial SQUID phases from a flat distribution
         """
         Phi0 = {}
@@ -173,6 +175,8 @@ class SimMuMUXCrosstalk(Operator):
                 counter=(counter1, counter2),
             )[0]
             v = vmin + x * (vmax - vmin)
+            # Convert from Phi0 to Phi
+            v *= 2 * np.pi
             Phi0[det] = v
 
         return Phi0
@@ -209,9 +213,12 @@ class SimMuMUXCrosstalk(Operator):
             P_atm = efficiency * bandpass.optical_loading(det, median_signal)  # W
             P_opt += P_atm - P_atm_ref
             dPdT = bandpass.kcmb2w(det)  # K_CMB -> W
-            dIdP = 1 / np.sqrt((P_sat - P_opt) * R_FRAC * R_BOLO)  # W -> A
+            R_TES = R_FRAC * R_BOLO
+            I_TES = np.sqrt((P_sat - P_opt) / R_TES)
+            dIdP = -1 / (I_TES * (R_TES - R_SHUNT))  # W -> A
             dPhi0dI = 1 / 9e-6  # A -> [rad]
-            dPhi0dT[det] = dPdT * dIdP * dPhi0dI  # K_CMB -> [rad]
+            dPhidPhi0 = 2 * np.pi
+            dPhi0dT[det] = dPdT * dIdP * dPhi0dI * dPhidPhi0  # K_CMB -> [rad]
 
         return dPhi0dT
 
@@ -290,10 +297,11 @@ class SimMuMUXCrosstalk(Operator):
                         crosstalk += chi * np.sin(
                             source_squid_phase - target_squid_phase
                         )
-                    else:
-                        # Otherwise flag
-                        temp_obs.detdata[self.det_flags][det_source] |= self.det_flag_mask
-                        temp_obs.detdata[self.det_flags][det_target] |= self.det_flag_mask
+                    #else:
+                    #    # Otherwise flag
+                    #    print('Flagging!')
+                    #    temp_obs.detdata[self.det_flags][det_source] |= self.det_flag_mask
+                    #    temp_obs.detdata[self.det_flags][det_target] |= self.det_flag_mask
 
                 # Translate crosstalk into temperature units and scale to
                 # match input data