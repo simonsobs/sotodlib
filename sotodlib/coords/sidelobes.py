5fb42ea00efb71e5667a11fd2efb7ae65ccd8f98
chervias <aconcagua.chc@gmail.com>
2025-11-18 04:14:39 -0800

    Adding docstring for sidelobes

--- sotodlib/coords/sidelobes.py
+++ sotodlib/coords/sidelobes.py
@@ -3,10 +3,36 @@ from pixell import enmap, utils, coordsys
 from sotodlib import coords, mapmaking
 import so3g
 
-def Simplecut(ndets, nsamps):
-    return so3g.proj.RangesMatrix.zeros((ndets,nsamps))
-
 def sidelobe_cut(obs, args, sidelobe_cutters, object_list=None):
+    """
+    Function to calculate cuts (RangesMatrix) for Sun and Moon
+    from a binary sidelobe mask with an arbitrary shape.
+    The masks MUST BE full sky and relatively low resolution since
+    the map is loaded as a non-tiled enmap.
+
+    Parameters
+    ----------
+    obs : sotodlib.core.AxisManager
+        An observation axis manager.
+    args : dict
+        An argument dictionary, which must contains the path to the
+        masks in mask_sun and mask_moon. This is only used for these
+        paths.
+    sidelobe_cutters : dict
+        A dict holding SidelobeCutter for each sun and moon. In the
+        first run this should be empty and will be filled. Subsequent
+        runs will use these precalculated objects.
+    object_list : list, optional
+        A list of the objects we want to mask. If None, sun and moon
+        will be run.
+
+    Returns
+    -------
+    cutss : list
+        A list where the elements are the cuts for the objects requested.
+        Each will be a RangesMatrix with shape (ndets,nsamps).
+    """
+
 	if object_list is None: object_list = ["sun", "moon"]
 	cutss = []
 	for name in object_list:
@@ -19,12 +45,15 @@ def sidelobe_cut(obs, args, sidelobe_cutters, object_list=None):
 		cutss.append(cuts)
 	return cutss
 
+def Simplecut(ndets, nsamps):
+    return so3g.proj.RangesMatrix.zeros((ndets,nsamps))
+
 class SidelobeCutter:
     def __init__(self, mask, objname, dtype=np.float32, rise_tol=1*utils.degree):
         self.mask = mask
-        self.lmap = enmap.read_map(mask).astype(np.double)
+        self.lmap = enmap.read_map(mask).astype(np.double) #from_map will complain if the map is not double
         self.objname= objname
-        self.sys_sotodlib  = "%s,up=hor" % np.char.capitalize([objname])[0]
+        self.sys_sotodlib  = "%s,up=hor" % np.char.capitalize([objname])[0] # this is to capitalize the first letter
         self.sys_pixell = "hor,on=%s" % objname
         self.rise_tol = rise_tol
         self.dtype = dtype
@@ -60,7 +89,7 @@ class SidelobeCutter:
             # explicitly instead of relying on gpu_mm to catch things itself
             #L.print("Error cutting %s sidelobes for %s: Pointing overflow. Skipping cut" % (self.objname, id), level=2, color=colors.red)
             return Simplecut(obs.signal.shape[0], obs.signal.shape[1])
-        cuts = so3g.proj.RangesMatrix.from_mask(tod.astype(int))
+        cuts = so3g.proj.RangesMatrix.from_mask(tod.astype(int)) #here the mask is expected to be int
         return cuts
 
 def forward(tod, map, pmat):
