simonsobs/sotodlib

commit 9779c4866a1941a19ff2644805ae96bbf147a7fd

Fix for discarding too short obs for obs_grouping (#1462)
* Fix for discarding too short obs for obs_grouping

* some more fixes

* addressing Matthews comments

* more matthews comments

sotodlib/site_pipeline/make_atomic_filterbin_map.py
@@ -137,7 +137,7 @@ class Cfg:
         center_at: Optional[str] = None,
         max_dets: Optional[int] = None,
         fixed_time: Optional[int] = None,
-        min_dur: Optional[int] = None,
+        min_dur: Optional[int] = 300,
         verbose: int = 0,
         quiet: int = 0,
         window: Optional[float] = None,
@@ -322,7 +322,7 @@ def main(
         errlog.append( os.path.join(os.path.dirname(
             preproc_local['archive']['index']), 'errlog.txt') )
 
-
+    args.query += f" and duration>{args.min_dur}"
     if (args.update_delay is not None):
         min_ctime = int(time.time()) - args.update_delay*86400
         args.query += f" and timestamp>={min_ctime}"
@@ -337,7 +337,7 @@ def main(
         obslists, obskeys, periods, obs_infos = mapmaking.build_obslists(
             context_obj, args.query, nset=args.nset, wafer=args.wafer,
             freq=args.freq, ntod=args.ntod, tods=args.tods,
-            fixed_time=args.fixed_time, mindur=args.min_dur)
+            fixed_time=args.fixed_time, min_dur=args.min_dur)
     except mapmaking.NoTODFound as err:
         L.exception(err)
         exit(0)

sotodlib/mapmaking/obs_grouping.py
@@ -29,7 +29,7 @@ class NoTODFound(Exception):
         self.msg = msg
 
 def build_obslists(context, query, mode=None, nset=None, wafer=None,
-        freq=None, ntod=None, tods=None, fixed_time=None, mindur=None, per_tube=False ):
+        freq=None, ntod=None, tods=None, fixed_time=None, min_dur=None, per_tube=False ):
     """ 
     Return an obslists dictionary (described in the submodule docstring), 
     along with all ancillary data necessary for the mapmaker
@@ -61,7 +61,7 @@ def build_obslists(context, query, mode=None, nset=None, wafer=None,
     fixed_time : int or None, optional
             Optional, if mode=='fixed_interval', this is the fixed time in
             seconds
-    mindur : int or None, optional
+    min_dur : int, optional
             Optional, minimum duration of an observation to be included in 
             the mapping. If not defined it will be 120 seconds
            
@@ -95,9 +95,9 @@ def build_obslists(context, query, mode=None, nset=None, wafer=None,
     
     if mode is None or mode == 'per_obs':
         # We simply need to make and obslists dict with each key being one obs
-        periods = find_scan_periods_perobs(obs_infos)
+        periods = find_scan_periods_perobs(obs_infos, min_dur=min_dur)
     elif mode=='depth_1':
-        periods   = find_scan_periods(obs_infos, ttol=12*3600)
+        periods   = find_scan_periods(obs_infos, ttol=12*3600, min_dur=min_dur)
         periods   = split_periods(periods, 24*3600)
     elif mode=='fixed_interval':
         if fixed_time is not None:
@@ -113,21 +113,24 @@ def build_obslists(context, query, mode=None, nset=None, wafer=None,
     # We will make one map per period-detset-band if per_tube=False
     # or per period-band if per_tube=True
     obslists = build_period_obslists(obs_infos, periods, context, nset=nset,
-                                     wafer=wafer, freq=freq, per_tube=per_tube)
+                                     wafer=wafer, freq=freq, per_tube=per_tube,
+                                    min_dur=min_dur)
     obskeys  = sorted(obslists.keys())
     return obslists, obskeys, periods, obs_infos
 
 
-def find_scan_periods(obs_info, ttol=60, atol=2*utils.degree, mindur=120):
+def find_scan_periods(obs_info, ttol=60, atol=2*utils.degree, min_dur=None):
     """Given a list of obs info, return the set of contiguous scanning periods in the form
     [:,{ctime_from,ctime_to}]."""
     atol = atol/utils.degree
     info = np.array([obs_info[a] for a in ["az_center", "el_center", 
                                            "az_throw", "timestamp", "duration"]]).T
+    if min_dur is None:
+        min_dur = 120
     # Get rid of nan entries
     bad  = np.any(~np.isfinite(info),1)
     # get rid of too short tods, since those don't have reliable az bounds
-    bad |= info[:,-1] < mindur
+    bad |= info[:,-1] < min_dur
     info = info[~bad]
     t1   = info[:,-2] # timestamp is start of tod, unlike in enki
     info = info[np.argsort(t1)]
@@ -155,15 +158,17 @@ def find_scan_periods(obs_info, ttol=60, atol=2*utils.degree, mindur=120):
     periods = np.array([t1s, t2s]).T
     return periods
 
-def find_scan_periods_perobs(obs_info, mindur=120):
+def find_scan_periods_perobs(obs_info, min_dur=None):
     """Given a list of obs info, return the periods per obs, i.e. start and 
     stop of each scan. This is a simplified version of find_scan_periods
     [:,{ctime_from,ctime_to}]."""
     info = np.array([obs_info[a] for a in ["timestamp", "duration"]]).T
+    if min_dur is None:
+        min_dur = 120
     # Get rid of nan entries
     bad  = np.any(~np.isfinite(info),1)
     # get rid of too short tods, since those don't have reliable az bounds
-    bad |= info[:,-1] < mindur
+    bad |= info[:,-1] < min_dur
     info = info[~bad]
     t1   = info[:,-2] # timestamp is start of tod, unlike in enki
     info = info[np.argsort(t1)]
@@ -208,7 +213,7 @@ def split_periods(periods, maxdur):
     return np.array([t1,t2]).T
 
 def build_period_obslists(obs_info, periods, context, nset=None, 
-                          wafer=None, freq=None, per_tube=False):
+                          wafer=None, freq=None, per_tube=False, min_dur=None):
     """
     For each period for each detset-band, make a list of (id,detset,band)
     for the ids that fall inside that period. Returns the obslist dictionary
@@ -217,12 +222,16 @@ def build_period_obslists(obs_info, periods, context, nset=None,
     If per_tube=True, then we loop over all available wafers (which will
     override nset and wafer).
     """
+    if min_dur is None:
+        min_dur = 120
     obslists = {}
     # 1. Figure out which period each obs belongs to
     ctimes_mid = obs_info.timestamp + obs_info.duration/2
     pids       = np.searchsorted(periods[:,0], ctimes_mid)-1
     # 2. Build our lists. Not sure how to do this without looping
     for i, row in enumerate(obs_info):
+        if row.duration < min_dur:
+            continue # we skip obs that are too short
         if wafer is not None and not per_tube:
             wafer_list = [wafer]
         else:

commit 543ea7cb7b2874bb58eaf10d9b6e06c300bd5e1e

fixing a mistake I introduced in #1462

sotodlib/site_pipeline/make_atomic_filterbin_map.py
@@ -310,8 +310,9 @@ def main(
         errlog.append( os.path.join(os.path.dirname(
             preproc_local['archive']['index']), 'errlog.txt') )
 
-    args.query += f" and duration>{args.min_dur}"
     if (args.update_delay is not None):
+        # this is only to be used for running automatic maps on prefect
+        args.query += f" and duration>{args.min_dur}"
         min_ctime = int(time.time()) - args.update_delay*86400
         args.query += f" and timestamp>={min_ctime}"
 