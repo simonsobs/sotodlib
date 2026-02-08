679f6d24fd3c487b25e4ca1bcd3428bc25e1450d
Saianeesh Keshav Haridas <accounts@saianeesh.com>
2026-02-08 10:50:30 -0800

    feat: add option for optics tubes to float and moved some fitter options to config file

--- sotodlib/site_pipeline/solve_pointing_model.py
+++ sotodlib/site_pipeline/solve_pointing_model.py
@@ -66,6 +66,7 @@ def load_per_obs_data(config, t0, tf):
     obs_ufm_centers = np.zeros([len(filelist), 7, 3]) * np.nan
     weights_ufm = np.zeros([len(filelist), 7])
     obs_index = []
+    ot_list = []
 
     for i, ffp in enumerate(filelist):
         this_OT = rxs[ffp].optics_tubes[0]
@@ -73,16 +74,18 @@ def load_per_obs_data(config, t0, tf):
             index = ufms.index(this_OT.focal_planes[u].stream_id)
             obs_ufm_centers[i, index, :3] = this_OT.focal_planes[u].center_transformed
             weights_ufm[i, index] = np.nansum(this_OT.focal_planes[u].weights)
-        obs_index.append(np.repeat(i, 7))
+            ot_list.append(this_OT.name)
+            obs_index.append(i)
            
     weights_ufm = weights_ufm / 1720.0
     weights_ufm[weights_ufm < config.get("weight_cutoff")] = 0.0
     initial_weights_mask = np.where(weights_ufm == 0)
     obs_ufm_centers[initial_weights_mask] = np.nan
-    obs_index = np.concatenate(obs_index)
+    obs_index = np.array(obs_index)
+    ot_list = np.array(ot_list)
     #obs_index[initial_weights_mask] = np.nan
 
-    return filelist, obs_ufm_centers, weights_ufm, obs_index
+    return filelist, obs_ufm_centers, weights_ufm, obs_index, ot_list
 
 def load_nom_focal_plane_full(config, ufm):
     which_template = config.get("use_as_template", "ffp")
@@ -161,7 +164,7 @@ def load_per_detector_data(config, t0, tf, no_downsample_set=False, return_all_d
         if config.get("use_these_files") is not None:
             filelist = [filelist[i] for i in config.get("use_these_files")]
 
-    weights_dets, obs_dets_fits, stream_id_list, obs_index = [], [], [], []
+    weights_dets, obs_dets_fits, stream_id_list, ot_list, obs_index = [], [], [], [], []
     #which_ufm = config.get("which_ufm", None)
     which_data = config.get("use_as_data")
     which_weights = config.get("use_as_weights", None)
@@ -177,12 +180,14 @@ def load_per_detector_data(config, t0, tf, no_downsample_set=False, return_all_d
             weights_dets.append(weights)
             obs_dets_fits.append(data)
             obs_index.append(np.repeat(i, len(ufm.weights)))
+            ot_list.append([this_OT.name] * len(data))
             
     nom_data = [load_nom_focal_plane_full(config, s) for s in stream_id_list]
     all_det_ids, all_nom_det_array = map(np.concatenate, zip(*nom_data))
     weights_dets = np.concatenate(weights_dets)
     obs_dets_fits = np.concatenate(obs_dets_fits, axis=0)
     obs_index = np.concatenate(obs_index)
+    ot_list = np.concatenate(ot_list)
 
     weights_dets[weights_dets < config.get("weight_cutoff")] = 0.0
     obs_dets_fits[np.where(weights_dets == 0)] = np.nan
@@ -197,6 +202,7 @@ def load_per_detector_data(config, t0, tf, no_downsample_set=False, return_all_d
             all_nom_det_array[mask],
             all_det_ids[mask],
             obs_index[mask],
+            ot_list[mask],
         )
 
     else:
@@ -209,6 +215,7 @@ def load_per_detector_data(config, t0, tf, no_downsample_set=False, return_all_d
         all_nom_det_array = all_nom_det_array[mask]
         all_det_ids = all_det_ids[mask]
         obs_index = obs_index[mask]
+        ot_list = ot_list[mask]
         if even_obs_size:
             mask = create_size_mask(obs_index)
             obs_dets_fits = obs_dets_fits[mask]
@@ -216,6 +223,7 @@ def load_per_detector_data(config, t0, tf, no_downsample_set=False, return_all_d
             all_nom_det_array = all_nom_det_array[mask]
             all_det_ids = all_det_ids[mask]
             obs_index = obs_index[mask]
+            ot_list = ot_list[mask]
         if cull_dets is not None:
             for _ in range(2 if cull_twice else 1):
                 mask = create_culling_mask(obs_index, cull_dets)
@@ -224,13 +232,15 @@ def load_per_detector_data(config, t0, tf, no_downsample_set=False, return_all_d
                 all_nom_det_array = all_nom_det_array[mask]
                 all_det_ids = all_det_ids[mask]
                 obs_index = obs_index[mask]
+                ot_list = ot_list[mask]
         return (
             filelist, 
             obs_dets_fits,
             weights_dets,
             all_nom_det_array,
             all_det_ids,
-            obs_index
+            obs_index,
+            ot_list,
         )
 
 def load_obs_boresight(config, filelist):
@@ -286,14 +296,26 @@ def _init_fit_params(config, epochs):
     pm_version = config.get("pm_version")
     initial_params = config.get("initial_params", {})
     fixed_params = config.get("fixed_params",[])
+    float_ots = config.get("float_ots", False)
     init_params = pm.param_defaults[pm_version]
     init_params.update(initial_params)
 
     # Add independant params
     orig_pars = np.array(list(init_params.keys()))
     par_list = orig_pars.copy()
+    added_ots = []
     for epoch in epochs: 
         indep_list = epoch["indep_list"]
+        if float_ots:
+            for ot in np.unique(epoch["solver_aman"].ot_list):
+                ot_float_pars = [f"{n}_{ot}" for n in ["xioff", "etaoff", "rot", "xiscale", "etascale"]]
+                indep_list += ot_float_pars 
+                if ot not in added_ots:
+                    orig_pars = np.concatenate((orig_pars, ot_float_pars))
+                    par_list = np.concatenate((par_list, ot_float_pars))
+                    for p, d in zip(ot_float_pars, [0, 0, 0, 1, 1]):
+                        init_params[p] = d
+                added_ots += [ot]
         if np.sum(np.isin(indep_list, par_list)) != len(indep_list):
             raise ValueError(f"Invalid independant parameters in epoch {epoch['name']}")
         indep_list = [f"{n}_{epoch['name']}" for n in indep_list]
@@ -333,6 +355,30 @@ def objective_model_func_lmfit(
     elif xieta_model == "template":
         xi_mod, eta_mod = model_template_xieta(params, pm_version, solver_aman)
         xi_ref, eta_ref, _ = solver_aman.nominal_xieta_locs
+
+    # For simplicity we do the floating of OTs to just the model
+    # We are recomputing information that should just be cached here...
+    # TODO: Fix that
+    ot_float_pars = ["xioff", "etaoff", "rot", "xiscale", "etascale"]
+    ot_float_defaults = np.array([0, 0, 0, 1, 1])
+    for ot in np.unique(solver_aman.ot_list):
+        ot_pars = [params.get(f"{n}_{ot}", d) for n, d in zip(ot_float_pars, ot_float_defaults)]
+        if np.array_equal(ot_pars, ot_float_defaults):
+            continue
+        msk = (solver_aman.ot_list == ot)
+        xi_mod[msk] += ot_pars[0]
+        eta_mod[msk] += ot_pars[1]
+        theta = ot_pars[2]
+        R = np.array([[np.cos(theta), -np.sin(theta)],
+              [np.sin(theta), np.cos(theta)]])
+        xi_cent = np.mean(xi_mod[msk])
+        eta_cent = np.mean(eta_mod[msk])
+        xi_mod[msk], eta_mod[msk] = R@np.vstack((xi_mod[msk] - xi_cent, eta_mod[msk] - eta_cent))
+        xi_mod[msk] += xi_cent
+        eta_mod[msk] += eta_cent
+        xi_mod[msk] *= ot_pars[3]
+        eta_mod[msk] *= ot_pars[4]
+
     dist = np.sqrt((xi_ref - xi_mod) ** 2 + (eta_ref - eta_mod) ** 2)
     #print(np.nansum(dist))
     weights_array = solver_aman.weights if weights else np.ones(len(dist))
@@ -343,6 +389,7 @@ def objective_model_func_lmfit_joint(
 ):
     params = params.valuesdict()
     chisq = 0
+    t1 = time.time()
     for epoch in epochs:
         chisq += objective_model_func_lmfit({par.split(f"_{epoch['name']}")[0]:params[par] for par in epoch["params"]}, pm_version, epoch["solver_aman"], xieta_model, weights)
     return chisq
@@ -446,7 +493,7 @@ def analyze_PM_with_all_dets(config, t0, tf, params):
     
     (filelist, obs_dets_fits,
      weights_dets, all_nom_det_array,
-     all_det_ids, obs_index,
+     all_det_ids, obs_index, ot_list
     ) = load_per_detector_data(config, t0, tf, return_all_dets=True)
     
     ancil, roll_c = load_obs_boresight_per_detector(config, filelist, obs_index)
@@ -469,6 +516,7 @@ def analyze_PM_with_all_dets(config, t0, tf, params):
     )
     full_aman.wrap("weights", weights_dets, [(0, "samps")])
     full_aman.wrap("obs_index", obs_index)
+    full_aman.wrap("ot_list", ot_list)
     full_aman.wrap("roll_c", roll_c, [(0, "samps")])
     full_aman.wrap("det_ids", all_det_ids,  [(0, "samps")])
     full_aman.wrap("radial", 
@@ -759,6 +807,7 @@ def main(config_path: str):
                 all_nom_det_array,
                 all_det_ids,
                 obs_index,
+                ot_list,
             ) = load_per_detector_data(config, t0, tf, no_downsample_set=True)
             ancil, roll_c = load_obs_boresight_per_detector(config, filelist, obs_index)
             
@@ -775,6 +824,7 @@ def main(config_path: str):
             )
             fitcheck_aman.wrap("weights", weights_dets, [(0, "samps")])
             fitcheck_aman.wrap("obs_index", obs_index, [(0, "samps")])
+            fitcheck_aman.wrap("ot_list", ot_list, [(0, "samps")])
             logger.info("Loaded %s fit check data points", len(weights_dets))
 
             #Now make axis manager that has down sampled data for computation
@@ -786,6 +836,7 @@ def main(config_path: str):
                 all_nom_det_array,
                 all_det_ids,
                 obs_index,
+                ot_list,
             ) = load_per_detector_data(config, t0, tf)
             logger.info("Loaded %s data points", len(weights_dets))
             ancil, roll_c = load_obs_boresight_per_detector(config, filelist, obs_index)
@@ -810,6 +861,7 @@ def main(config_path: str):
             )
             solver_aman.wrap("weights", weights_dets, [(0, "samps")])
             solver_aman.wrap("obs_index", obs_index)
+            solver_aman.wrap("ot_list", ot_list)
             epoch["solver_aman"] = solver_aman
             epoch["fitcheck_aman"] = fitcheck_aman 
             logger.info("Built axis manager for epoch %s", epoch["name"])
@@ -824,7 +876,7 @@ def main(config_path: str):
 
         for epoch in epochs:
             t0, tf = epoch["begin_timerange"], epoch["end_timerange"]
-            filelist, obs_ufm_centers, weights_ufm, obs_index = load_per_obs_data(config, t0, tf)
+            filelist, obs_ufm_centers, weights_ufm, obs_index, ot_list = load_per_obs_data(config, t0, tf)
             logger.info("Loaded per-obs FFP data from %s: ", config.get("per_obs_fps"))
             logger.info("Including data from these obs:")
             logger.info(filelist)
@@ -856,6 +908,7 @@ def main(config_path: str):
             )
             solver_aman.wrap("weights", weights_ufm.reshape(-1), [(0, "samps")])
             solver_aman.wrap("obs_index", obs_index)
+            solver_aman.wrap("ot_list", ot_list)
             # Make weights/data cuts
             epoch["solver_aman"] = solver_aman
             logger.info("Built axis manager for epoch %s", epoch["name"])
@@ -873,9 +926,11 @@ def main(config_path: str):
     model_solved_params = minimize(
         objective_model_func_lmfit_joint,
         fit_params,
-        method="nelder",
+        method="lbfgsb", # "nelder",
         nan_policy="omit",
         args=(pm_version, epochs, xieta_model, use_weights),
+        workers=-1,
+        options={'maxcor': 10}
     )
     logger.info("Ran 1st Minimization")
 
@@ -997,9 +1052,11 @@ def main(config_path: str):
         model_solved_params = minimize(
             objective_model_func_lmfit_joint,
             fit_params,
-            method="nelder",
+            method="lbfgsb",
             nan_policy="omit",
             args=(pm_version, epochs, xieta_model, use_weights),
+            workers=-1,
+            options={'maxcor': 10}
         )
 
         test_params = _round_params(model_solved_params.params.valuesdict(), 8)
