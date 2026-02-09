10bf4314457a5b281c7f8d8ae6d9c15b02ae2af1
Katie Harrington <kharrington@anl.gov>
2026-02-09 19:57:55 +0000

    if we have streamed again after an incomplete observation, close it. Finally closes #1061

--- sotodlib/io/imprinter.py
+++ sotodlib/io/imprinter.py
@@ -1170,6 +1170,32 @@ class Imprinter:
         )
         return q
 
+    def _attempt_incomplete_fix(self, incomplete_obs):
+        """
+        For a list of (likely) incomplete level 2 observations, see if a new level2
+        observation has been streamed from the same slot and, if so, force the 
+        completion of the previous observation.
+        """
+        g3session, SMURF = self.get_g3tsmurf_session(return_archive=True)
+        for obs in incomplete_obs:
+            if obs.stop is not None:
+                ## edge case wont fix
+                continue
+            next_obs = g3session.query(G3tObservations).filter(
+                G3tObservations.stream_id == obs.stream_id,
+                G3tObservations.timestamp > obs.timestamp,
+            ).order_by(G3tObservations.timestamp).first()
+            if next_obs is not None:
+                ## we've streamed again on this readout slot.
+                ## older obs should be closed
+                self.logger.warning(
+                    f"{obs} is incomplete but we have streamed {next_obs} "
+                    "from the same slot. Attempting to set stop time"
+                )
+                SMURF.update_observation_files(
+                    obs, g3session, force_stop=True
+                )    
+
     @loop_over_tubes
     def update_bookdb_from_g3tsmurf(
         self,
@@ -1206,8 +1232,8 @@ class Imprinter:
             if True, return the list of observation sets instead of registering
             books. Useful as a debugging tool
         incomplete_timeouts: tuple
-            hours for raising a (warning, error) if incomplete observations are
-            found in the g3tsmurf database
+            hours passed for (attempting to complete observation, raising a error) 
+            if incomplete observations are found in the g3tsmurf database
         """
         if not self.build_det:
             return
@@ -1245,7 +1271,21 @@ class Imprinter:
         # check for incomplete observations in time range
         q_incomplete = self._find_incomplete(min_ctime, max_ctime, streams)
 
-        # if we have incomplete observations in our stream_id list we cannot
+        ## if incomplete observations are more than 3 hours in the past, try and
+        ## set these to complete by seeing if we've streamed from that slot again
+        if q_incomplete.count() > 0:
+            incomplete_obs = q_incomplete.all()
+            new_ctime = min([obs.timestamp for obs in incomplete_obs])
+            if max_ctime - new_ctime > 3600*incomplete_timeouts[0]:
+                self.logger.warning(
+                    f"Found {q_incomplete.count()} incomplete observations "
+                    f"more than {incomplete_timeouts[0]} hours in the past."
+                    " Will attempt to complete these observations"
+                )
+                self._attempt_incomplete_fix(incomplete_obs)
+                q_incomplete = self._find_incomplete(min_ctime, max_ctime, streams)
+            
+        # if we still have incomplete observations in our stream_id list we cannot
         # bookbind any observations overlapping the incomplete ones.
         if q_incomplete.count() > 0:
             new_ctime = min([obs.timestamp for obs in q_incomplete.all()])
