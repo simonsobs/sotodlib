simonsobs/sotodlib

commit 992dcf34e91e0f0a3043737da5458fbf3418b800

Toast data and metadata improvements: (#1522)
- Support loading raw data as int32 DAQ units.

- When loading metadata, ensure that byte arrays are converted
  to strings as the starting point.  These get automatically
  converted to / from bytestrings during HDF5 save / load.

- The HKManager now excludes several large fields by default
  (WG / HWP encoders and ACU udp stream).

- HKManager converts all byte strings to unicode on load, and
  then separately converts to / from bytestrings during HDF5
  save / load.

- Small fix to always use "so3g compatibility mode" when
  converting boresight Az/El to RA/DEC on load.

sotodlib/toast/ops/load_context.py
@@ -24,8 +24,7 @@ from toast.traits import (
     Float,
 )
 from toast.ops.operator import Operator
-from toast.ops.pipeline import Pipeline
-from toast.utils import Logger
+from toast.utils import Logger, replace_byte_arrays
 from toast.dist import distribute_discrete
 from toast.observation import default_values as defaults
 
@@ -135,13 +134,9 @@ class LoadContext(Operator):
         help="Path to DB for site housekeeping",
     )
 
-    hk_site_fields = List(
-        list(), help="Restrict loading to only these site fields"
-    )
+    hk_site_fields = List(list(), help="Restrict loading to only these site fields")
 
-    hk_site_aliases = Dict(
-        dict(), help="Optional convenience aliases for site fields"
-    )
+    hk_site_aliases = Dict(dict(), help="Optional convenience aliases for site fields")
 
     hk_platform_root = Unicode(
         None,
@@ -330,6 +325,10 @@ class LoadContext(Operator):
 
     bandwidth = Float(0.2, help="Fractional bandwith used in analytic bandpass")
 
+    daq_units = Bool(
+        False, help="If True, convert raw data to original int32 DAQ units"
+    )
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
@@ -354,6 +353,10 @@ class LoadContext(Operator):
             if self.context_file is not None:
                 msg = "Only one of the context or context_file should be specified"
                 raise RuntimeError(msg)
+
+        if self.daq_units and self.preprocess_config is not None:
+            msg = "Cannot convert raw signal to DAQ units if passing data through preprocessing"
+            raise RuntimeError(msg)
 
         # Build our detector selection dictionary.  Merge our explicit traits
         # with any pre-existing detector selection.
@@ -635,8 +638,14 @@ class LoadContext(Operator):
             # Read and communicate data
             self._load_data(ob, have_pointing, preproc_conf)
 
+            # Now that all metadata has been loaded, ensure that all byte strings
+            # are converted to unicode arrays.
+            ob._internal = replace_byte_arrays(ob._internal)
+
             # Optionally load housekeeping data
             if self.hk_site_root is not None or self.hk_platform_root is not None:
+                hk_timer = Timer()
+                hk_timer.start()
                 ob.hk = HKManager(
                     ob.comm.comm_group,
                     ob.shared[self.times].data,
@@ -648,6 +657,11 @@ class LoadContext(Operator):
                     plat_db=self.hk_platform_db,
                     plat_fields=self.hk_platform_fields,
                     plat_aliases=self.hk_platform_aliases,
+                )
+                log.debug_rank(
+                    f"LoadContext {obs_name} load House Keeping in",
+                    comm=comm.comm_group,
+                    timer=hk_timer,
                 )
 
             # Compute the boresight pointing and observatory position
@@ -1052,9 +1066,14 @@ class LoadContext(Operator):
                 dtype=np.float64,
             )
         if ax_det_signal is not None:
-            ob.detdata.create(
-                self.det_data, dtype=np.float64, units=self.det_data_units
-            )
+            if self.daq_units:
+                ob.detdata.create(
+                    self.det_data, dtype=np.int32, units=u.dimensionless_unscaled
+                )
+            else:
+                ob.detdata.create(
+                    self.det_data, dtype=np.float64, units=self.det_data_units
+                )
             ob.detdata.create(self.det_flags, dtype=np.uint8)
 
         if meta is not None:
@@ -1118,6 +1137,7 @@ class LoadContext(Operator):
             ignore_preprocess_archive=self.ignore_preprocess_archive,
             context=self.context,
             context_file=self.context_file,
+            daq_units=self.daq_units,
         )
 
         log.debug_rank(
@@ -1315,6 +1335,7 @@ class LoadContext(Operator):
                     is_flag=(mask is not None),
                     flag_invert=do_invert,
                     flag_mask=mask,
+                    daq_units=self.daq_units,
                 )
 
         # Original wafer data no longer needed.  AxisManager does not seem to

sotodlib/toast/ops/load_context_utils.py
@@ -15,6 +15,7 @@ from toast.timing import function_timer, Timer
 from toast.utils import Logger
 from toast.dist import distribute_uniform
 from toast.observation import default_values as defaults
+from toast.utils import replace_byte_arrays, byte_array_to_unicode
 
 from ...coords import pointing_model
 from ...core import Context, AxisManager
@@ -44,6 +45,7 @@ def read_and_preprocess_wafers(
     ignore_preprocess_archive=False,
     context=None,
     context_file=None,
+    daq_units=False,
 ):
     """Read the wafer data.
 
@@ -63,6 +65,7 @@ def read_and_preprocess_wafers(
         ignore_preprocess_db (bool): Ignore the 'archive' field in the preprocessing configuration.
         context (Context):  The pre-existing Context or None.
         context_file (str):  The context file to open or None.
+        daq_units (bool):  If True, convert raw signal back to DAQ units.
 
     Returns:
         (dict):  The AxisManager data for each wafer on this process.
@@ -75,6 +78,8 @@ def read_and_preprocess_wafers(
         rank = 0
     else:
         rank = gcomm.rank
+
+    rescale_to_daq = 2**15 / np.pi
 
     results = dict()
     for wf, reader in wafer_readers.items():
@@ -125,19 +130,17 @@ def read_and_preprocess_wafers(
             # Apply pointing model, UNLESS we are applying a preprocess config
             # on load and that config includes the pointing model
             if "pointing_model" in axtod:
-                if (
-                    preconfig is None
-                    or "pointing_model" not in [i['name'] for i in preconfig["process_pipe"]]
-                ):
+                if preconfig is None or "pointing_model" not in [
+                    i["name"] for i in preconfig["process_pipe"]
+                ]:
                     pointing_model.apply_pointing_model(axtod)
 
             # If the axis manager has a HWP angle solution, apply it.
             if "hwp_solution" in axtod:
                 # Did we already apply it in the preprocessing?
-                if (
-                    preconfig is None
-                    or "hwp_angle_model" not in [i['name'] for i in preconfig["process_pipe"]]
-                ):
+                if preconfig is None or "hwp_angle_model" not in [
+                    i["name"] for i in preconfig["process_pipe"]
+                ]:
                     axtod = apply_hwp_angle_model(axtod)
                     timer.stop()
                     elapsed = timer.seconds()
@@ -158,6 +161,9 @@ def read_and_preprocess_wafers(
                 msg = f"LoadContext {obs_name} apply preproc to {wf}"
                 msg += f" in {elapsed} seconds"
                 log.debug(msg)
+            if daq_units:
+                if "signal" in axtod:
+                    axtod["signal"] *= rescale_to_daq
             results[wf] = axtod
     return results
 
@@ -219,14 +225,16 @@ def parse_metadata(axman, obs_meta, fp_cols, path_sep, det_axis, obs_base, fp_ba
             field_axes = axman._assignments[key]
             if len(field_axes) == 0:
                 # This data is not associated with an axis.
-                om[key] = axman[key]
+                om[key] = replace_byte_arrays(axman[key])
             elif len(field_axes) == 1 and field_axes[0] == det_axis:
                 # This is a detector property
                 if fp_key in fp_cols:
                     msg = f"Context meta key '{fp_key}' is duplicated in nested"
                     msg += " AxisManagers"
                     raise RuntimeError(msg)
-                fp_cols[fp_key] = Column(name=fp_key, data=np.array(axman[key]))
+                fp_cols[fp_key] = Column(
+                    name=fp_key, data=np.array(byte_array_to_unicode(axman[key]))
+                )
     if obs_base is not None and len(om) == 0:
         # There were no meta data keys- delete this dict
         del obs_meta[obs_base]
@@ -376,6 +384,7 @@ def distribute_detector_data(
     is_flag=False,
     flag_invert=False,
     flag_mask=None,
+    daq_units=False,
 ):
     """Communicate detector data from the reading processes to the destinations.
 
@@ -406,6 +415,7 @@ def distribute_detector_data(
         is_flag (bool):  If True, this field is a flag.
         flag_invert (bool):  If True, invert the meaning of the flag values.
         flag_mask (np.uint8):  The flag mask (or None).
+        daq_units (bool):  If True, AxisManager signal is in DAQ units.
 
     Returns:
         None
@@ -569,7 +579,17 @@ def distribute_detector_data(
                         )
                     else:
                         for idet in range(n_send_det):
-                            obs.detdata[field][idet + recv_dets[0], :] = sdata_2d[idet]
+                            if daq_units:
+                                nan_mask = np.isnan(sdata_2d[idet])
+                                if np.count_nonzero(nan_mask) > 0:
+                                    det_flags[idet] = 1
+                                obs.detdata[field][idet + recv_dets[0], :] = sdata_2d[
+                                    idet
+                                ].astype(np.int32, casting="unsafe")
+                            else:
+                                obs.detdata[field][idet + recv_dets[0], :] = sdata_2d[
+                                    idet
+                                ]
                     # Update per-detector flags
                     dflags = {
                         obs.local_detectors[recv_dets[0] + x]: defaults.det_mask_invalid
@@ -615,7 +635,15 @@ def distribute_detector_data(
             else:
                 # Just assign
                 for idet in range(n_recv_det):
-                    obs.detdata[field][idet + recv_dets[0], :] = recv_2d[idet]
+                    if daq_units:
+                        nan_mask = np.isnan(recv_data[idet])
+                        if np.count_nonzero(nan_mask) > 0:
+                            det_flags[idet] = 1
+                        obs.detdata[field][idet + recv_dets[0], :] = recv_data[
+                            idet
+                        ].astype(np.int32, casting="unsafe")
+                    else:
+                        obs.detdata[field][idet + recv_dets[0], :] = recv_data[idet]
             # Update per-detector flags
             dflags = {
                 obs.local_detectors[recv_dets[0] + x]: defaults.det_mask_invalid
@@ -638,15 +666,17 @@ def distribute_detector_data(
     del send_data
 
     # Every process checks its local data for NaN values.  If any are found, a warning
-    # is printed and the detector is cut.
-    dflags = dict()
-    for det in obs.local_detectors:
-        nnan = np.count_nonzero(np.isnan(obs.detdata[field][det]))
-        if nnan > 0:
-            msg = f"{obs.name}:{det} has {nnan} NaN values.  Cutting."
-            log.warning(msg)
-            dflags[det] = defaults.det_mask_invalid
-    obs.update_local_detector_flags(dflags)
+    # is printed and the detector is cut.  If we are using DAQ units, those detectors
+    # with NaNs were already cut during the float to int32 conversion above.
+    if not daq_units:
+        dflags = dict()
+        for det in obs.local_detectors:
+            nnan = np.count_nonzero(np.isnan(obs.detdata[field][det]))
+            if nnan > 0:
+                msg = f"{obs.name}:{det} has {nnan} NaN values.  Cutting."
+                log.warning(msg)
+                dflags[det] = defaults.det_mask_invalid
+        obs.update_local_detector_flags(dflags)
 
 
 @function_timer
@@ -750,6 +780,7 @@ def compute_boresight_pointing(
         obs.shared[times_key].data[slc],
         bore_azel,
         use_qpoint=True,
+        so3g_compat_mode=True,
     )
 
     # Gather all samples to rank zero and set the shared object elements.

sotodlib/toast/hkmanager.py
@@ -2,13 +2,17 @@
 # Full license can be found in the top level "LICENSE" file.
 
 import copy
+import re
 from collections.abc import MutableMapping
 
 import numpy as np
 from scipy.interpolate import CubicSpline, PchipInterpolator
 
-from toast.utils import Logger
-from toast.io.hdf_utils import load_meta_object, save_meta_object
+from toast.utils import Logger, replace_byte_arrays, array_equal
+from toast.io.hdf_utils import (
+    load_meta_object,
+    save_meta_object,
+)
 
 from ..io import hkdb
 
@@ -49,6 +53,17 @@ class HKManager(MutableMapping):
 
     """
 
+    # Exclude the massive HWP and WG housekeeping data by default, since
+    # this information is only used to build the HWP angle and WG calibration
+    # metadata.  Loading these fields dramatically increases the load time
+    # for normal use.  These can still be forcibly loaded by specifying them
+    # in the `plat_fields` list.
+    exclude_by_default = [
+        r"^hwp-.*",
+        r"^wg-.*",
+        r"^acu.acu_udp_stream.*",
+    ]
+
     def __init__(
         self,
         comm=None,
@@ -83,7 +98,7 @@ class HKManager(MutableMapping):
                 # Load data on just one process per group
                 self._load_hk_data(site_root, site_db, site_fields, site_aliases)
 
-        # Load the plot HK data
+        # Load the platform HK data
         if plat_root is not None:
             if plat_db is None:
                 raise RuntimeError("If plat_root is specified, plat_db is required")
@@ -99,6 +114,7 @@ class HKManager(MutableMapping):
     def _load_hk_data(self, root, db, fields, aliases):
         """Helper function to do the actual loading."""
         log = Logger.get()
+        exclude_pat = [re.compile(x) for x in self.exclude_by_default]
         conf = hkdb.HkConfig.from_dict(
             {
                 "hk_root": root,
@@ -120,7 +136,17 @@ class HKManager(MutableMapping):
         )
         all_fields = hkdb.get_feed_list(test_spec)
         if fields is None or len(fields) == 0:
-            selected = list(all_fields)
+            # The user is requesting "all" fields.  We will prune this list to exclude
+            # fields related to the HWP and wire grid encoders, which are excessively
+            # large and rarely used for downstream analysis.
+            selected = list()
+            for fld in all_fields:
+                keep = True
+                for pat in exclude_pat:
+                    if pat.match(fld) is not None:
+                        keep = False
+                if keep:
+                    selected.append(fld)
         else:
             selected = fields
         # Load the data
@@ -133,7 +159,10 @@ class HKManager(MutableMapping):
             hkdb=None,
         )
         result = hkdb.load_hk(lspec, show_pb=False)
-        self._internal.update(result.data)
+
+        converted = replace_byte_arrays(result.data)
+
+        self._internal.update(converted)
         if aliases is not None:
             for k, v in aliases.items():
                 if v not in self._internal:
@@ -159,12 +188,24 @@ class HKManager(MutableMapping):
         itrp = CubicSpline(times, vals, extrapolate=True)
         return itrp(self._stamps)
 
-    def memory_use(self):
+    def memory_use(self, threshold=0):
+        """Return the total size of all fields and a string with the breakdown.
+
+        Args:
+            threshold (int):  Only include in the string objects larger than this.
+
+        Returns:
+            (tuple):  The (total size, info string)
+
+        """
         bytes = 0
+        fstr = ""
         for k, v in self._internal.items():
-            bytes += v[0].nbytes
-            bytes += v[1].nbytes
-        return bytes
+            fbytes = v[0].nbytes + v[1].nbytes
+            bytes += fbytes
+            if fbytes > threshold:
+                fstr += f"{k}: {fbytes} bytes\n"
+        return bytes, fstr
 
     # Mapping methods
 
@@ -204,14 +245,14 @@ class HKManager(MutableMapping):
             log.verbose(f"  keys {self._internal} != {other._internal}")
             return False
         for k, v in self._internal.items():
-            try:
-                if not np.allclose(v, other._internal[k]):
-                    log.verbose(f"  data array {v} != {other._internal[k]}")
-                    return False
-            except Exception:
-                if self._internal[k] != other._internal[k]:
-                    log.verbose(f"  data value {v} != {other._internal[k]}")
-                    return False
+            times = v[0]
+            vals = v[1]
+            other_times = other._internal[k][0]
+            other_vals = other._internal[k][1]
+            if not array_equal(times, other_times, log_prefix=f"HKManager {k} times"):
+                return False
+            if not array_equal(vals, other_vals, log_prefix=f"HKManager {k} values"):
+                return False
         if self._aliases != other._aliases:
             log.verbose(f"  aliases {self._aliases} != {other._aliases}")
             return False

tests/test_toast_context.py
@@ -4,6 +4,7 @@
 
 import os
 import copy
+import shutil
 
 import numpy as np
 import astropy.units as u
@@ -70,6 +71,12 @@ class ToastContextTest(TestCase):
             return
         world, procs, rank = toast.get_world()
 
+        vol_path = os.path.join(self.outdir, "test_hk")
+        if rank == 0 and os.path.isdir(vol_path):
+            shutil.rmtree(vol_path)
+        if world is not None:
+            world.barrier()
+
         data = simulation_test_data(
             world,
             telescope_name="SAT1",
@@ -109,7 +116,6 @@ class ToastContextTest(TestCase):
         )
         demod_data = demod.apply(data)
 
-        vol_path = os.path.join(self.outdir, "test_hk")
         toast.ops.SaveHDF5(
             volume=vol_path,
             detdata=[
@@ -127,10 +133,10 @@ class ToastContextTest(TestCase):
         toast.ops.LoadHDF5(volume=vol_path).apply(new_data)
 
         for old_ob, new_ob in zip(demod_data.obs, new_data.obs):
-            if old_ob != new_ob:
+            if not old_ob.__eq__(new_ob, approx=True):
                 print(f"OLD: {old_ob} not equal to")
                 print(f"NEW: {new_ob}", flush=True)
-                #self.assertTrue(False)
+                self.assertTrue(False)
 
         close_data_and_comm(new_data)
         close_data_and_comm(demod_data)

commit 06eac0d516f171dd21721fa325d97b4c7b1f9ded

Fix a typo which introduced a bug in PR #1522.
Found through use of some new integration tests in `sopipetest`,
PR will be opened soon with those.

sotodlib/toast/ops/load_context.py
@@ -1,6 +1,7 @@
 # Copyright (c) 2022-2024 Simons Observatory.
 # Full license can be found in the top level "LICENSE" file.
 
+import copy
 import re
 import sqlite3
 import yaml
@@ -596,7 +597,7 @@ class LoadContext(Operator):
 
             # If we have an observation with a specific wafer, modify the detector
             # selection dictionary to include only that wafer.
-            obs_dets_select = dets_select
+            obs_dets_select = copy.deepcopy(dets_select)
             if obs_props[obindx]["wafer"] != "all":
                 if obs_dets_select is None:
                     obs_dets_select = dict()
@@ -722,6 +723,8 @@ class LoadContext(Operator):
         # hence one reader).
         if rank == 0:
             # Load metadata
+            msg = f"LoadContext {obs_name} metadata using dets_select={dets_select}"
+            log.debug(msg)
             ctx = open_context(context=self.context, context_file=self.context_file)
             meta = ctx.get_meta(session_name, dets=dets_select)
             if self.context_file is not None:
@@ -755,7 +758,11 @@ class LoadContext(Operator):
                     band = fp_cols[
                         f"det_info{self.ax_pathsep}wafer{self.ax_pathsep}bandpass"
                     ].data
-                freq = [float(b[1:]) for b in band]
+
+                fpat = re.compile(r"^f[\d]+$")
+                freq = [
+                    float(b[1:]) if fpat.match(b) is not None else 0.0 for b in band
+                ]
                 bandcenter = np.array(freq) * u.GHz
                 bandwidth = bandcenter * self.bandwidth
                 fp_cols["bandcenter"] = Column(name="bandcenter", data=bandcenter)

sotodlib/toast/ops/load_context_utils.py
@@ -636,14 +636,14 @@ def distribute_detector_data(
                 # Just assign
                 for idet in range(n_recv_det):
                     if daq_units:
-                        nan_mask = np.isnan(recv_data[idet])
+                        nan_mask = np.isnan(recv_2d[idet])
                         if np.count_nonzero(nan_mask) > 0:
                             det_flags[idet] = 1
-                        obs.detdata[field][idet + recv_dets[0], :] = recv_data[
+                        obs.detdata[field][idet + recv_dets[0], :] = recv_2d[
                             idet
                         ].astype(np.int32, casting="unsafe")
                     else:
-                        obs.detdata[field][idet + recv_dets[0], :] = recv_data[idet]
+                        obs.detdata[field][idet + recv_dets[0], :] = recv_2d[idet]
             # Update per-detector flags
             dflags = {
                 obs.local_detectors[recv_dets[0] + x]: defaults.det_mask_invalid