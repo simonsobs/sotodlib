4290e3badcd616db8c9d46da44b016403ab62132
Matthew Hasselfield <mhasself@users.noreply.github.com>
2026-02-16 02:58:55 -0800

    Revert "Remove HkExtract class, which is a visitor from the future"
    This reverts commit 913f34c9060c40cf91188e61036d7000b6a210e3.

--- sotodlib/io/ancil/utils.py
+++ sotodlib/io/ancil/utils.py
@@ -380,6 +380,235 @@ class LowResTable(AncilEngine):
         raise NotImplementedError()
 
 
+class HkExtract(AncilEngine):
+    """Helper class for storing extracts from HK data.
+
+    """
+
+    def _clear_dataset(self, filename, dataset):
+        with h5py.File(filename, 'a') as hin:
+            if dataset in hin:
+                del hin[dataset]
+
+    def _get_datasets(self, time_range):
+        """Return a list of all (file, dataset) pairs that overlap with
+        time_range, along with the overlap.
+
+        """
+        ns = int(self.cfg.archive_block_seconds)
+        nb = int(self.cfg.dataset_block_seconds)
+        assert (ns % nb == 0)
+        t0 = int((time_range[0] // ns) * ns)
+        t1 = time_range[1]
+        rows = []
+        while t0 < t1:
+            fn = self.cfg.filename_pattern.format(
+                timestamp=t0,
+                dataset_name=self.cfg.dataset_name)
+            fn = os.path.join(self._base_dir, fn)
+            sub_t1 = min(t0 + ns, t1)
+            while t0 < sub_t1:
+                ds = f't{t0}'
+                if t0 + nb > time_range[0]:
+                    rows.append((#max(t0, time_range[0]), min(t1, t0 + nb), fn, ds))
+                        t0, t0 + nb, fn, ds))
+                t0 += nb
+        return rows
+
+    def check_base(self):
+        """Check the base data -- determine output directory, count files
+        therein, take note of extra files, etc.
+
+        """
+        info = {
+            'output_dir': self._base_dir,
+            'files_found': 0,
+        }
+        time_range = _get_time_range(self.cfg.dataset_time_range)
+        ds_count = {}
+        for t0, t1, filename, _ in self._get_datasets(time_range):
+            if filename in ds_count:
+                if ds_count[filename] == -1:
+                    continue
+            else:
+                if os.path.exists(filename):
+                    ds_count[filename] = 0
+                else:
+                    ds_count[filename] = -1
+        info['files_found'] = len([v for v in ds_count.values() if v == 0])
+        return info
+
+    def update_base(self, time_range=None, reset=False):
+        """Update data by grabbing for HK archive.  Only data in time_range
+        will be loaded, and even then only for times not confidently
+        covered by the archive.  If reset=True then active data in the
+        archive for this time_range is discarded, and fully replaced with
+        newly loaded results.
+
+        Each file has groups containing data for a specific time
+        interval (probably ~1 day).  That group also specifies the
+        complete_until time.  If complete_until is lower than the end
+        time for the group, it is eligible to be updated.  To
+        invalidate existing data for time_range, and definitely
+        replace it, pass reset=True.
+
+        You have to assume that "coverage" is a contiguous
+        super-interval.  Once you get data at t, the coverage is
+        extend to t.  If there's a gap, you can fill it using *reset*,
+        which should not pay attention to coverage.
+
+        Coverage interval is then a ~global archive value; or
+        per-file?  If you do per-dataset, that's perhaps inefficient
+        but fine.
+
+        So -- for each dataset, check coverage time and read data
+        starting from the end of that.
+
+        """
+        # Validate archive time range...
+        now = time.time()
+        _tr = self.cfg.dataset_time_range
+        _tr = ((0  if _tr[0] is None else _tr[0]),
+               (now if _tr[1] is None else _tr[1]))
+        time_range = _get_time_range(time_range, _tr)
+        time_range = intersect_ranges(time_range, _tr)
+
+        field_list = list(self.cfg.aliases.keys())
+        tbuf = 120.
+
+        cfg = hkdb.HkConfig.from_yaml(self.cfg.hkdb_config)
+        cfg.aliases = self.cfg.aliases
+        db = hkdb.HkDb(cfg)
+
+        def insert_vector(origd, newd, time_range):
+            if newd is not None:
+                s = (time_range[0] <= newd[0]) * (newd[0] < time_range[1])
+                if origd[0] is None:
+                    return (newd[0][s], newd[1][s])
+            else:
+                if origd[0] is None:
+                    return (None, None)
+                newd = [[], []]
+                s = slice(0, 0)
+            left = (origd[0] < time_range[0])
+            right = (origd[0] >= time_range[1])
+            return tuple(np.hstack((o[left], n[s], o[right]))
+                         for o, n in zip(origd, newd))
+
+        for dst0, dst1, filename, dataset in self._get_datasets(time_range):
+            # If reset, then we will load and replace any data in t0,
+            # t1.  coverage may be modified if it's <= t1.
+
+            # If not reset, then (t0, t1) is further restricted to
+            # start at coverage; and that sets the new data insertion
+            # window.  coverage may only be increased in this case.
+            logger.info(f'{dst0} {dst1} {filename} {dataset}')
+
+            vects = {k: (None, None) for k in field_list}
+            complete_to = dst0
+            if os.path.exists(filename):
+                with h5py.File(filename, 'r') as hin:
+                    if dataset in hin:
+                        dset = hin[dataset]
+                        if 'complete_to' in dset.attrs:
+                            complete_to = dset.attrs['complete_to']
+                        for k in field_list:
+                            if k not in dset:
+                                continue
+                            try:
+                                vects[k] = (dset[k]['timestamp'][()],
+                                          dset[k]['value'][()])
+                            except Exception as e:
+                                logger.error(f'Invalid data in {filename} : {dataset}.')
+                                raise e
+
+            t0 = max(time_range[0], dst0, complete_to)
+            t1 = min(time_range[1], dst1)
+            if t0 >= t1:
+                logger.info('No updates; archive complete for requested range.')
+                continue
+
+            # Load.
+            lspec = hkdb.LoadSpec(
+                cfg=cfg, start=t0 - tbuf, end=t1 + tbuf,
+                fields=field_list,
+                hkdb=db)
+
+            logger.info(f' ... loading hk {t0}-{t1}')
+            result = hkdb.load_hk(lspec)
+            # Merge the data.
+            output = {}
+            new_complete_to = []
+            for k in field_list:
+                newd = getattr(result, k, None)
+                if newd is not None:
+                    new_complete_to.append(max(newd[0]))
+                else:
+                    new_complete_to.append(min(t0, complete_to))
+                output[k] = insert_vector(vects[k], newd, (t0, t1))
+                if output[k][0] is not None:
+                    dtype = self.cfg.dtypes.get(k)
+                    if dtype is None:
+                        dtype = self.cfg.default_dtype
+                    if dtype is not None:
+                        output[k] = (output[k][0].astype('float64'),
+                                     output[k][1].astype(dtype))
+            new_complete_to = min(min(new_complete_to), t1)
+
+            if t0 > complete_to:
+                new_complete_to = complete_to
+
+            # Write it out.
+            logger.info(' ... writing %s : %s' % (filename, dataset))
+            with h5py.File(filename, 'a') as hin:
+                if dataset in hin:
+                    del hin[dataset]
+                g = hin.create_group(dataset)
+                try:
+                    g.attrs['complete_to'] = float(new_complete_to)
+                    for k in field_list:
+                        if output[k][0] is not None:
+                            g1 = g.create_group(k)
+                            g1.create_dataset('timestamp', data=output[k][0])
+                            g1.create_dataset('value', data=output[k][1])
+
+                except Exception as e:
+                    logger.error(f'Problem storing {dataset} in {filename}, removing')
+                    raise e
+
+    def _load(self, time_range):
+        field_list = list(self.cfg.aliases.keys())
+        output = {k: [[], []] for k in field_list}
+
+        for _, _, filename, dataset in self._get_datasets(time_range):
+            if not os.path.exists(filename):
+                continue
+            with h5py.File(filename, 'r') as hin:
+                if dataset not in hin:
+                    continue
+                ds = hin[dataset]
+                for k in field_list:
+                    if k not in ds:
+                        continue
+                    t, v = ds[k]['timestamp'][()], ds[k]['value'][()]
+                    s = (time_range[0] <= t) * (t < time_range[1])
+                    if s.any():
+                        output[k][0].append(t[s])
+                        output[k][1].append(v[s])
+
+        for k in output.keys():
+            if len(output[k][0]):
+                output[k] = [np.hstack(x) for x in output[k]]
+            else:
+                output[k] = [np.zeros(0, dtype='float64'),
+                             np.zeros(0, dtype='float32')]
+
+        return output
+
+    def getter(self, targets=None, **kwargs):
+        raise NotImplementedError()
+
+
 def get_example_obsdb(start, end=None, step=3600, filename=None):
     obsdb = ObsDb()
     obsdb.add_obs_columns(['timestamp float', 'start_time float', 'stop_time float'])
