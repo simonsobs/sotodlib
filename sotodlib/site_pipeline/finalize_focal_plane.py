9f89ecb53a33c66a2326fc704af391b4b67abca2
Tomoki Terasaki <59273369+tterasaki@users.noreply.github.com>
2026-04-24 02:10:18 +0900

    Pointing workflow (#772)
    Scripts in this PR are for processing incoming source observations (moon & planet). Default parameters are best used with SAT moon data.
    
    * sotodlib/site_pipeline/get_brightsrc_pointing_step1.py
      Produces xieta coordinates for detectors on the focal plane using map-based processing.
    
    * sotodlib/site_pipeline/get_brighsrc_pointing_step2.py
      Refines pointing solutions from step1 using TOD data processing with map-based solutions as inputs for starting guesses.
     
    * sotodlib/coords/brightsrd_processing.py
      Helper functions for the site_pipeline scripts.
    
    ---------
    
    Co-authored-by: Tomoki Terasaki <tterasaki@compute11.simonsobs.org>
    Co-authored-by: Tomoki Terasaki <tterasaki@compute12.simonsobs.org>
    Co-authored-by: Elle Shaw <elle.shaw@utexas.edu>

--- sotodlib/site_pipeline/finalize_focal_plane.py
+++ sotodlib/site_pipeline/finalize_focal_plane.py
@@ -6,6 +6,10 @@ from copy import deepcopy
 from importlib import import_module
 from typing import List, Optional
 
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
 import git
 import h5py
 import megham.transform as mt
@@ -403,7 +407,7 @@ def _mk_pointing_config(telescope_flavor, tube_slot, wafer_slot, config):
 def _restrict_inliers(aman, focal_plane):
     # TODO: Use gamma as well
     # Map to template
-    fp, _, template_msk = focal_plane.map_by_det_id(aman)
+    fp, _, _, template_msk = focal_plane.map_by_det_id(aman)
     fp = fp[:, :2]
     inliers = np.ones(len(fp), dtype=bool)
 
@@ -779,7 +783,7 @@ def main():
                 _restrict_inliers(aman, focal_plane)
 
                 # Mapping to template
-                fp, r2, template_msk = focal_plane.map_by_det_id(aman)
+                fp, r2, det_boresight, template_msk = focal_plane.map_by_det_id(aman)
                 focal_plane.template.add_wafer_info(aman, template_msk)
 
                 # Try an initial alignment and get weights
@@ -816,7 +820,7 @@ def main():
 
                 # Store weighted values
                 weights = np.column_stack((weights, r2))
-                focal_plane.add_fp(i, fp, weights, template_msk)
+                focal_plane.add_fp(i, fp, weights, det_boresight, template_msk)
 
                 n_obs += 1
 
