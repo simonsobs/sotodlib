c77e4b4a63d9d4d1cfb4b94e6914e21aae1f5195
Josh Borrow <josh@joshborrow.com>
2026-05-13 16:34:48 -0400

    Apply same profiling pattern from g3t_smurf to update_librarian

--- sotodlib/site_pipeline/update_librarian.py
+++ sotodlib/site_pipeline/update_librarian.py
@@ -1,5 +1,6 @@
 import argparse
 import datetime as dt
+from pathlib import Path
 from typing import Optional
 
 from sotodlib.io.imprinter import Imprinter, BOUND, UPLOADED
@@ -7,7 +8,37 @@ from sotodlib.site_pipeline.utils.logging import init_logger
 
 logger = init_logger(__name__, "update_librarian: ")
 
-def main(config: str):
+def main(config: Optional[str] = None, profile: bool=False,
+         profile_output: Optional[Path]=None):
+    """
+    Arguments
+    ---------
+    config: string
+        configuration file for imprinter
+    profile: bool
+        if True, will run the script with pyinstrument and output to profile_output
+    profile_output: str
+        if profile is True, the file name of the directory
+        to output the pyinstrument profiling results to
+    """
+
+    if profile:
+        import pyinstrument
+        filename = f"update_librarian_{dt.datetime.now().strftime('%Y%m%d_%H%M%S')}.html"
+        output_filename = profile_output / filename if profile_output is not None else filename
+        profiler = pyinstrument.Profiler()
+        profiler.start()
+    
+    try:
+        core(config=config)
+    finally:
+        if profile:
+            profiler.stop()
+            if profile_output is not None:
+                with open(output_filename, "w") as f:
+                    f.write(profiler.output_html())
+
+def core(config: str):
     """
     Update the book plan database with new data from the g3tsmurf database.
 
@@ -42,14 +73,15 @@ def main(config: str):
         raise failed_list[0][1]
 
 
-
-
 def get_parser(parser=None):
     if parser is None:
         parser = argparse.ArgumentParser()
     parser.add_argument(
         '--config', type=str, help="imprinter configuration file"
     )
+    parser.add_argument("--profile", help="Run with pyinstrument profiling", action="store_true")
+    parser.add_argument("--profile-output", help="Directory to output pyinstrument profiling results to, if --profile is set", 
+                        type=Path)
     return parser
 
 
