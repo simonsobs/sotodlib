6e1ae0e5464a71ba3eba953679256b725dea67d4
Theodore Kisner <tskisner.public@gmail.com>
2026-06-15 09:16:15 -0700

    Move hdf5 atomic tests into a dedicated file

--- tests/test_hdf5_atomic.py
+++ tests/test_hdf5_atomic.py
@@ -4,8 +4,6 @@
 """
 
 import os
-import multiprocessing as mp
-import time
 
 import unittest
 from unittest import TestCase
@@ -16,6 +14,8 @@ import h5py
 from sotodlib.core.util import H5ContextManager
 
 from ._helpers import create_outdir, mpi_multi
+
+from .test_hdf5_atomic_update import main as inode_main
 
 
 @unittest.skipIf(mpi_multi(), "Running with multiple MPI processes")
@@ -112,56 +112,9 @@ class HDF5AtomicTest(TestCase):
         # after it is renamed with the original name.  Meanwhile, the reading process
         # has an open handle to the original inode, which is unmodified and not
         # deleted until it is closed.
-
-        # Spawn a new process, not fork
-        ctx = mp.get_context("spawn")
-
-        outpath = os.path.join(self.outdir, "test_inode_replace.h5")
-        n_append = 4
-
-        def _modifier_func(path):
-            # Open the file in append mode and add some datasets
-            with H5ContextManager(path, mode="a") as hf:
-                for iapp in range(n_append):
-                    _ = self.append_dataset_file(hf)
-
-        # Create initial file
-        nds = self.create_fake_file(outpath)
-
-        # Reader opens file.  Test the direct method here instead of a context.
-        hf = H5ContextManager(outpath, mode="r").open()
-
-        # File should have original number of datasets
-        self.assertTrue(self.check_file(hf, nds))
-
-        # Imagine the reader process is an rsync, and the open file is a large
-        # HDF5 metadata file.  Meanwhile the file might be opened multiple times
-        # by writing processes that append little datasets.  Try to capture
-        # that here.
-
-        # While file is open for read, append to file from separate process
-        writer = ctx.Process(target=_modifier_func(outpath))
-        writer.start()
-        writer.join()
-
-        # And again...
-        writer = ctx.Process(target=_modifier_func(outpath))
-        writer.start()
-        writer.join()
-
-        # And again...
-        writer = ctx.Process(target=_modifier_func(outpath))
-        writer.start()
-        writer.join()
-
-        # Check that our open file handle is still the same data
-        self.assertTrue(self.check_file(hf, nds))
-
-        # The classmethod to close the handle will release the inode containing
-        # the original file version, whose name has since been redirected (3 times)
-        # to different inodes.
-        H5ContextManager.close(hf)
-
-        # Starting process re-opens the file and now the contents should be updated
-        with H5ContextManager(outpath, mode="r") as hf:
-            self.assertTrue(self.check_file(hf, nds + 3 * n_append))
+        opts = [
+            "--test_dir",
+            self.outdir,
+        ]
+        ret = inode_main(opts=opts)
+        self.assertEqual(ret, 0)
