99bf277fc75e291e6266a8586b9ad25307717ee1
Michael McCrackan <mikemccrackan@gmail.com>
2026-08-10 11:36:29 -0400

    allow flag_cut_select to take int (#1712)
    Co-authored-by: Michael McCrackan <mmccrack@login32.chn.perlmutter.nersc.gov>

--- sotodlib/core/flagman.py
+++ sotodlib/core/flagman.py
@@ -300,10 +300,10 @@ def flag_cut_select(flags, kind, invert=False):
 
     Args:
         flags (RangesMatrix): An instance of so3g.proj.RangesMatrix indicating flagged time ranges.
-        kind (str or float): One of the following:
+        kind (str or int/float): One of the following:
             - 'any': Select/cut detectors with any flagged samples.
             - 'all': Select/cut detectors with all samples flagged.
-            - float: A threshold ratio (0.0–1.0); selects/cuts detectors whose flagged ratio exceeds the threshold.
+            - int/float: A threshold ratio (0.0–1.0); selects/cuts detectors whose flagged ratio exceeds the threshold.
         intert (bool): default=False returns detectors to be excluded =True, detectors to be kept = False. If True, The logic is flipped.
 
     Returns:
@@ -320,7 +320,7 @@ def flag_cut_select(flags, kind, invert=False):
             return has_any_cuts(flags)
         elif kind == 'all':
             return has_all_cut(flags)
-        elif isinstance(kind, float):
+        elif isinstance(kind, (int, float)):
             return has_ratio_cuts(flags, ratio=kind)
         else:
             raise ValueError("kind must be 'any', 'all', or a float between 0.0 and 1.0")
@@ -329,7 +329,7 @@ def flag_cut_select(flags, kind, invert=False):
             return ~has_any_cuts(flags)
         elif kind == 'all':
             return ~has_all_cut(flags)
-        elif isinstance(kind, float):
+        elif isinstance(kind, (int, float)):
             return ~has_ratio_cuts(flags, ratio=kind)
         else:
             raise ValueError("kind must be 'any', 'all', or a float between 0.0 and 1.0")
